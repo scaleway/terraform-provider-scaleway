412ed4a5dd237a8f038f94cc9f8b536cc670243d
Raphael Randschau <nicolai86@me.com>
2018-01-14 18:03:11 -0800

    add retry support for rate limited requests
    closes #30

--- scaleway/config.go
+++ scaleway/config.go
@@ -1,8 +1,16 @@
 package scaleway
 
 import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 	"sort"
+	"time"
 
+	retryablehttp "github.com/hashicorp/go-retryablehttp"
 	sdk "github.com/nicolai86/scaleway-sdk"
 )
 
@@ -18,12 +26,50 @@ type Client struct {
 	scaleway *sdk.API
 }
 
+// client is a bridge between sdk.HTTPClient interface and retryablehttp.Client
+type client struct {
+	*retryablehttp.Client
+}
+
+func (c *client) Do(r *http.Request) (*http.Response, error) {
+	var body io.ReadSeeker
+	if r.Body != nil {
+		bs, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			return nil, err
+		}
+		body = bytes.NewReader(bs)
+	}
+	req, err := retryablehttp.NewRequest(r.Method, r.URL.String(), body)
+	for key, val := range r.Header {
+		req.Header.Set(key, val[0])
+	}
+	if err != nil {
+		return nil, err
+	}
+	return c.Client.Do(req)
+}
+
 // Client configures and returns a fully initialized Scaleway client
 func (c *Config) Client() (*Client, error) {
 	api, err := sdk.New(
 		c.Organization,
 		c.APIKey,
 		c.Region,
+		func(c *sdk.API) {
+			cl := retryablehttp.NewClient()
+			cl.RetryMax = 3
+			cl.RetryWaitMax = 2 * time.Minute
+			cl.Logger = log.New(os.Stderr, "", 0)
+			cl.RetryWaitMin = time.Minute
+			cl.CheckRetry = func(resp *http.Response, err error) (bool, error) {
+				if resp.StatusCode == http.StatusTooManyRequests {
+					return true, err
+				}
+				return retryablehttp.DefaultRetryPolicy(resp, err)
+			}
+			c.Client = &client{cl}
+		},
 	)
 	if err != nil {
 		return nil, err
