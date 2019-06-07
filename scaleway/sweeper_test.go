66e7753614bd1901f1982c165065c27a015b2d58
Quentin Brosse <quentin.brosse@icloud.com>
2019-06-07 15:29:23 +0200

    fix: shared deprecated client for region

--- scaleway/sweeper_test.go
+++ scaleway/sweeper_test.go
@@ -16,24 +16,32 @@ func TestMain(m *testing.M) {
 
 // sharedDeprecatedClientForRegion returns a scaleway deprecated client needed for the sweeper
 // functions for a given region {par1,ams1}
-func sharedDeprecatedClientForRegion(r string) (*api.API, error) {
-	if os.Getenv("SCALEWAY_ORGANIZATION") == "" {
-		return nil, fmt.Errorf("empty SCALEWAY_ORGANIZATION")
+func sharedDeprecatedClientForRegion(region string) (*api.API, error) {
+	projectId := os.Getenv("SCW_DEFAULT_PROJECT_ID")
+	if projectId == "" {
+		projectId = os.Getenv("SCALEWAY_ORGANIZATION")
+	}
+	if projectId == "" {
+		return nil, fmt.Errorf("empty SCW_DEFAULT_PROJECT_ID")
 	}
 
-	if os.Getenv("SCALEWAY_TOKEN") == "" {
-		return nil, fmt.Errorf("empty SCALEWAY_TOKEN")
+	secretKey := os.Getenv("SCW_SECRET_KEY")
+	if secretKey == "" {
+		secretKey = os.Getenv("SCALEWAY_TOKEN")
+	}
+	if secretKey == "" {
+		return nil, fmt.Errorf("empty SCW_SECRET_KEY")
 	}
 
-	region, err := utils.ParseRegion(r)
+	parsedRegion, err := utils.ParseRegion(region)
 	if err != nil {
 		return nil, err
 	}
 
 	conf := &Config{
-		DefaultProjectID: os.Getenv("SCALEWAY_ORGANIZATION"),
-		SecretKey:        os.Getenv("SCALEWAY_TOKEN"),
-		DefaultRegion:    region,
+		DefaultProjectID: projectId,
+		SecretKey:        secretKey,
+		DefaultRegion:    parsedRegion,
 	}
 
 	// configures a default client for the region, using the above env vars
