65ada5f009024d2abd54c56fbff2fb19b1d39959
Olivier CANO <kindermoumoute@gmail.com>
2019-06-17 15:38:39 +0200

    WIP

--- scaleway/resource_compute_instance_server.go
+++ scaleway/resource_compute_instance_server.go
@@ -1,8 +1,13 @@
 package scaleway
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/validation"
 	"github.com/scaleway/scaleway-sdk-go/api/instance/v1"
+	"github.com/scaleway/scaleway-sdk-go/namegenerator"
 )
 
 func resourceScalewayComputeInstanceServer() *schema.Resource {
@@ -55,11 +60,6 @@ func resourceScalewayComputeInstanceServer() *schema.Resource {
 				Default:     false,
 				Description: "determines if IPv6 is enabled for the server",
 			},
-			"dynamic_ip_required": {
-				Type:        schema.TypeBool,
-				Optional:    true,
-				Description: "determines if a public IP address should be allocated for the server",
-			},
 			"private_ip": {
 				Type:        schema.TypeString,
 				Computed:    true,
@@ -70,11 +70,19 @@ func resourceScalewayComputeInstanceServer() *schema.Resource {
 				Computed:    true,
 				Description: "the public IPv4 address of the server",
 			},
-			"state": {
+			"action": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				Computed:    true,
-				Description: "the server state (running, stopped)",
+				Default:     instance.ServerActionPoweron,
+				Description: "the server action (poweron, poweroff)",
+				ValidateFunc: validation.StringInSlice([]string{
+					string(instance.ServerActionPoweron),
+					string(instance.ServerActionStopInPlace),
+					string(instance.ServerActionPoweroff),
+					string(instance.ServerActionTerminate),
+					string(instance.ServerActionBackup), // allowed but not recommended
+					string(instance.ServerActionReboot), // allowed but not recommended
+				}, false),
 			},
 			"user_data": {
 				Type: schema.TypeMap,
@@ -84,6 +92,8 @@ func resourceScalewayComputeInstanceServer() *schema.Resource {
 				Optional:    true,
 				Description: "The user data associated with the server", // TODO: document reserved keys (`cloud-init`)
 			},
+			"zone":       zoneSchema(),
+			"project_id": projectIDSchema(),
 		},
 	}
 }
@@ -91,8 +101,68 @@ func resourceScalewayComputeInstanceServer() *schema.Resource {
 func resourceScalewayComputeInstanceServerCreate(d *schema.ResourceData, m interface{}) error {
 	meta := m.(*Meta)
 	instanceApi := instance.NewAPI(meta.scwClient)
-	instanceApi.CreateServer(&instance.CreateServerRequest{})
-	instanceApi.CreateSecurityGroupRule(&instance.CreateSecurityGroupRuleRequest{})
-	instanceApi.CreateSecurityGroup(&instance.CreateSecurityGroupRequest{})
-	return nil
+
+	zone, err := getZone(d, meta)
+	if err != nil {
+		return err
+	}
+
+	req := &instance.CreateServerRequest{
+		Zone:           zone,
+		Organization:   d.Get("project_id").(string),
+		CommercialType: d.Get("commercial_type").(string),
+		EnableIPv6:     d.Get("enable_ipv6").(bool),
+		SecurityGroup:  d.Get("security_group_id").(string),
+	}
+
+	name, ok := d.GetOk("name")
+	if !ok {
+		name = namesgenerator.GetRandomName()
+	}
+	req.Name = name.(string)
+
+	if raw, ok := d.GetOk("tags"); ok {
+		for _, tag := range raw.([]interface{}) {
+			req.Tags = append(req.Tags, tag.(string))
+		}
+	}
+
+	if vs, ok := d.GetOk("volumes"); ok {
+		req.Volumes = make(map[string]*instance.VolumeTemplate)
+
+		for i, v := range vs.([]interface{}) {
+			req.Volumes[strconv.Itoa(i)] = &instance.VolumeTemplate{
+				ID:   v.(string),
+				Name: namesgenerator.GetRandomName(),
+			}
+		}
+	}
+
+	res, err := instanceApi.CreateServer(req)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(res.Server.ID)
+
+	// todo: add userdata
+
+	_, err = instanceApi.ServerAction(&instance.ServerActionRequest{
+		Zone:     zone,
+		ServerID: res.Server.ID,
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = instanceApi.WaitForServer(&instance.WaitForServerRequest{
+		Zone:     zone,
+		ServerID: res.Server.ID,
+		Timeout:  time.Minute * 2,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil // todo: read
 }
