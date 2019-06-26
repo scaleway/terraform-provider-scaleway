b623a315eac2546f1efad74a9abea91533dae41f
Quentin Brosse <quentin.brosse@icloud.com>
2019-06-26 17:17:13 +0200

    doc: deprecate scaleway_user_data

--- scaleway/resource_user_data.go
+++ scaleway/resource_user_data.go
@@ -10,6 +10,9 @@ import (
 
 func resourceScalewayUserData() *schema.Resource {
 	return &schema.Resource{
+		DeprecationMessage: `This resource is deprecated and will be removed in the next major version.
+ Please use scaleway_compute_instance_server instead.`,
+
 		Create: resourceScalewayUserDataCreate,
 		Read:   resourceScalewayUserDataRead,
 		Update: resourceScalewayUserDataUpdate,
