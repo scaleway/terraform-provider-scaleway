c6464b99d9f8e0eebc90aa1b4b6e11d895e6a900
Patrik Cyvoct <pcyvoct@scaleway.com>
2019-10-09 18:09:07 +0200

    add needed vendors
    Signed-off-by: Patrik Cyvoct <pcyvoct@scaleway.com>

---
+++ vendor/github.com/scaleway/scaleway-sdk-go/api/k8s/v1beta3/k8s_helpers.go
@@ -0,0 +1,49 @@
+package k8s
+
+import (
+	"time"
+
+	"github.com/scaleway/scaleway-sdk-go/internal/async"
+	"github.com/scaleway/scaleway-sdk-go/internal/errors"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+)
+
+// WaitForClusterRequest is used by WaitForCluster method.
+type WaitForClusterRequest struct {
+	ClusterID string
+	Region    scw.Region
+	Status    ClusterStatus
+	Timeout   time.Duration
+}
+
+// WaitForCluster waits for the cluster to be in a "terminal state" before returning.
+func (s *API) WaitForCluster(req *WaitForClusterRequest) (*Cluster, error) {
+	terminalStatus := map[ClusterStatus]struct{}{
+		ClusterStatusReady:   {},
+		ClusterStatusError:   {},
+		ClusterStatusWarning: {},
+		ClusterStatusLocked:  {},
+		ClusterStatusDeleted: {},
+	}
+
+	cluster, err := async.WaitSync(&async.WaitSyncConfig{
+		Get: func() (interface{}, error, bool) {
+			cluster, err := s.GetCluster(&GetClusterRequest{
+				ClusterID: req.ClusterID,
+				Region:    req.Region,
+			})
+			if err != nil {
+				return nil, err, false
+			}
+
+			_, isTerminal := terminalStatus[cluster.Status]
+			return cluster, nil, isTerminal
+		},
+		Timeout:          req.Timeout,
+		IntervalStrategy: async.LinearIntervalStrategy(5 * time.Second),
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "waiting for cluster failed")
+	}
+	return cluster.(*Cluster), nil
+}
