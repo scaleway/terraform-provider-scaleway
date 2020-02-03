6f88c26032179c9579e42c707e94b3f1f5a8f49b
Loïc Bourgois <lbourgois@scaleway.com>
2020-02-03 14:45:38 +0100

    improve tests

--- scaleway/resource_instance_security_group_rules_test.go
+++ scaleway/resource_instance_security_group_rules_test.go
@@ -2,6 +2,8 @@ package scaleway
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/scaleway/scaleway-sdk-go/api/instance/v1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
 	"testing"
 )
 
@@ -22,6 +24,30 @@ func TestAccScalewayInstanceSecurityGroupRules(t *testing.T) {
 				Config: conf1,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckScalewayInstanceSecurityGroupExists("scaleway_instance_security_group.sg01"),
+					testAccCheckScalewayInstanceSecurityGroupRuleMatch("scaleway_instance_security_group.sg01", 0, &instance.SecurityGroupRule{
+						Direction:    instance.SecurityGroupRuleDirectionInbound,
+						IPRange:      expandIPNet("0.0.0.0/0"),
+						DestPortFrom: scw.Uint32Ptr(80),
+						DestPortTo:   nil,
+						Protocol:     instance.SecurityGroupRuleProtocolTCP,
+						Action:       instance.SecurityGroupRuleActionAccept,
+					}),
+
+				),
+			},
+			{
+				Config: conf2,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayInstanceSecurityGroupExists("scaleway_instance_security_group.sg01"),
+					testAccCheckScalewayInstanceSecurityGroupRuleMatch("scaleway_instance_security_group.sg01", 0, &instance.SecurityGroupRule{
+						Direction:    instance.SecurityGroupRuleDirectionInbound,
+						IPRange:      expandIPNet("0.0.0.0/0"),
+						DestPortFrom: scw.Uint32Ptr(80),
+						DestPortTo:   nil,
+						Protocol:     instance.SecurityGroupRuleProtocolTCP,
+						Action:       instance.SecurityGroupRuleActionAccept,
+					}),
+					resource.TestCheckResourceAttrPair("scaleway_instance_security_group.sg01", "id", "scaleway_instance_security_group_rules.sgrs01", "security_group_id"),
 				),
 			},
 		},
@@ -30,6 +56,24 @@ func TestAccScalewayInstanceSecurityGroupRules(t *testing.T) {
 
 var conf1 = `
 resource scaleway_instance_server s01 { 
+	image = "ubuntu_bionic"
+	type = "DEV1-S"
+	security_group_id = scaleway_instance_security_group.sg01.id
+}
+
+resource scaleway_instance_security_group sg01 {
+	inbound_rule {
+		action = "accept"
+		port = 80
+		ip_range = "0.0.0.0/0"
+	}
+}
+`
+
+var conf2 = `
+resource scaleway_instance_server s01 { 
+	image = "ubuntu_bionic"
+	type = "DEV1-S"
 	security_group_id = scaleway_instance_security_group.sg01.id
 }
 
@@ -40,7 +84,9 @@ resource scaleway_instance_security_group sg01 {
 resource scaleway_instance_security_group_rules sgrs01 {
 	security_group_id = scaleway_instance_security_group.sg01.id
 	inbound_rule {
-		address = scaleway_instance_server.s01.private_ip
+		action = "accept"
+		port = 80
+		ip_range = "0.0.0.0/0"
 	}
 }
 `
