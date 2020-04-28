63c928f5134b419f7f02a4796cf49a28244caafb
Jerome Quere <jquere@scaleway.com>
2020-04-28 20:03:22 +0200

    test(instance): fix test for scaleway_instance_security_group_rules

--- scaleway/resource_instance_security_group_rules_test.go
+++ scaleway/resource_instance_security_group_rules_test.go
@@ -1,145 +1,198 @@
 package scaleway
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/scaleway/scaleway-sdk-go/api/instance/v1"
-	"github.com/scaleway/scaleway-sdk-go/scw"
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
 func TestAccScalewayInstanceSecurityGroupRules(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckScalewayInstanceSecurityGroupDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: `
-					resource scaleway_instance_security_group sg01 {
-						external_rules = true
-					}
-					resource scaleway_instance_security_group_rules sgrs01 {
-						security_group_id = scaleway_instance_security_group.sg01.id
-					}
-				`,
-			},
-			{
-				Config: `
-					resource scaleway_instance_security_group sg01 {
-						inbound_rule {
-							action = "accept"
-							port = 80
-							ip_range = "0.0.0.0/0"
-						}
-					}
-				`,
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckScalewayInstanceSecurityGroupExists("scaleway_instance_security_group.sg01"),
-					testAccCheckScalewayInstanceSecurityGroupRuleMatch("scaleway_instance_security_group.sg01", 0, &instance.SecurityGroupRule{
-						Direction:    instance.SecurityGroupRuleDirectionInbound,
-						IPRange:      expandIPNet("0.0.0.0/0"),
-						DestPortFrom: scw.Uint32Ptr(80),
-						DestPortTo:   nil,
-						Protocol:     instance.SecurityGroupRuleProtocolTCP,
-						Action:       instance.SecurityGroupRuleActionAccept,
-					}),
-				),
-			},
-			{
-				Config: `
-					resource scaleway_instance_security_group sg01 {
-						external_rules = true
-					}
-					
-					resource scaleway_instance_security_group_rules sgrs01 {
-						security_group_id = scaleway_instance_security_group.sg01.id
-						inbound_rule {
-							action = "accept"
-							port = 80
-							ip_range = "0.0.0.0/0"
-						}
-					}
-				`,
-				// Necessary because of security_group_id.ForceNew == true.
-				ExpectNonEmptyPlan: true,
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckScalewayInstanceSecurityGroupExists("scaleway_instance_security_group.sg01"),
-					testAccCheckScalewayInstanceSecurityGroupRuleMatch("scaleway_instance_security_group.sg01", 0, &instance.SecurityGroupRule{
-						Direction:    instance.SecurityGroupRuleDirectionInbound,
-						IPRange:      expandIPNet("0.0.0.0/0"),
-						DestPortFrom: scw.Uint32Ptr(80),
-						DestPortTo:   nil,
-						Protocol:     instance.SecurityGroupRuleProtocolTCP,
-						Action:       instance.SecurityGroupRuleActionAccept,
-					}),
-					resource.TestCheckResourceAttrPair("scaleway_instance_security_group.sg01", "id", "scaleway_instance_security_group_rules.sgrs01", "security_group_id"),
-				),
-			},
-			{
-				Config: `
-					resource scaleway_instance_security_group sg01 {
-						external_rules = true
-					}
-					
-					resource scaleway_instance_security_group_rules sgrs01 {
-						security_group_id = scaleway_instance_security_group.sg01.id
-						inbound_rule {
-							action = "accept"
-							port = 80
-							ip_range = "0.0.0.0/0"
-						}
-					}
-				`,
-				ResourceName: "scaleway_instance_security_group_rules.sgrs01",
-				ImportState:  true,
-				// Necessary because of security_group_id.ForceNew == true.
-				ImportStateVerify: false,
-				// Necessary because of security_group_id.ForceNew == true.
-				ExpectNonEmptyPlan: true,
-			},
-			{
-				Config: `
-					resource scaleway_instance_security_group sg01 {
-						external_rules = true
-					}
 
-					resource scaleway_instance_security_group_rules sgrs01 {
-						security_group_id = scaleway_instance_security_group.sg01.id
-						inbound_rule {
-							action = "accept"
-							port = 80
-							ip_range = "0.0.0.0/0"
+	t.Run("Basic", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			PreCheck:     func() { testAccPreCheck(t) },
+			Providers:    testAccProviders,
+			CheckDestroy: testAccCheckScalewayInstanceSecurityGroupDestroy,
+			Steps: []resource.TestStep{
+				{
+					// Simple empty configuration
+					Config: `
+						resource scaleway_instance_security_group sg01 {
+							external_rules = true
 						}
-						outbound_rule {
-							action = "accept"
-							port = 443
-							ip_range = "0.0.0.0/0"
+						resource scaleway_instance_security_group_rules sgrs01 {
+							security_group_id = scaleway_instance_security_group.sg01.id
 						}
-					}
-				`,
-				// Necessary because of security_group_id.ForceNew == true.
-				ExpectNonEmptyPlan: true,
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckScalewayInstanceSecurityGroupExists("scaleway_instance_security_group.sg01"),
-					testAccCheckScalewayInstanceSecurityGroupRuleMatch("scaleway_instance_security_group.sg01", 0, &instance.SecurityGroupRule{
-						Direction:    instance.SecurityGroupRuleDirectionInbound,
-						IPRange:      expandIPNet("0.0.0.0/0"),
-						DestPortFrom: scw.Uint32Ptr(80),
-						DestPortTo:   nil,
-						Protocol:     instance.SecurityGroupRuleProtocolTCP,
-						Action:       instance.SecurityGroupRuleActionAccept,
-					}),
-					testAccCheckScalewayInstanceSecurityGroupRuleMatch("scaleway_instance_security_group.sg01", 0, &instance.SecurityGroupRule{
-						Direction:    instance.SecurityGroupRuleDirectionOutbound,
-						IPRange:      expandIPNet("0.0.0.0/0"),
-						DestPortFrom: scw.Uint32Ptr(443),
-						DestPortTo:   nil,
-						Protocol:     instance.SecurityGroupRuleProtocolTCP,
-						Action:       instance.SecurityGroupRuleActionAccept,
-					}),
-					resource.TestCheckResourceAttrPair("scaleway_instance_security_group.sg01", "id", "scaleway_instance_security_group_rules.sgrs01", "security_group_id"),
-				),
+					`,
+					Check: resource.ComposeTestCheckFunc(
+						testAccCheckScalewayInstanceSecurityGroupExists("scaleway_instance_security_group.sg01"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.#", "0"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.#", "0"),
+					),
+				},
+				{
+					// We test that we can add some rules and they stay in correct orders
+					Config: `
+							resource scaleway_instance_security_group sg01 {
+								external_rules = true
+							}
+							resource scaleway_instance_security_group_rules sgrs01 {
+								security_group_id = scaleway_instance_security_group.sg01.id
+								inbound_rule {
+									action = "accept"
+									port = 80
+									ip_range = "0.0.0.0/0"
+								}
+								inbound_rule {
+									action = "drop"
+									port = 443
+									ip_range = "0.0.0.0/0"
+								}
+								outbound_rule {
+									action = "accept"
+									port = 80
+									ip_range = "0.0.0.0/0"
+								}
+								outbound_rule {
+									action = "drop"
+									port = 443
+									ip_range = "0.0.0.0/0"
+								}
+							}
+						`,
+					Check: resource.ComposeTestCheckFunc(
+						testAccCheckScalewayInstanceSecurityGroupExists("scaleway_instance_security_group.sg01"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.#", "2"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.0.action", "accept"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.0.port", "80"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.0.ip_range", "0.0.0.0/0"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.1.action", "drop"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.1.port", "443"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.1.ip_range", "0.0.0.0/0"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.#", "2"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.0.action", "accept"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.0.port", "80"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.0.ip_range", "0.0.0.0/0"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.1.action", "drop"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.1.port", "443"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.1.ip_range", "0.0.0.0/0"),
+					),
+				},
+				{
+					// We test that we can remove some rules
+					Config: `
+						resource scaleway_instance_security_group sg01 {
+							external_rules = true
+						}
+				
+						resource scaleway_instance_security_group_rules sgrs01 {
+							security_group_id = scaleway_instance_security_group.sg01.id
+								inbound_rule {
+									action = "drop"
+									port = 443
+									ip_range = "0.0.0.0/0"
+								}
+								outbound_rule {
+									action = "drop"
+									port = 443
+									ip_range = "0.0.0.0/0"
+								}
+						}
+					`,
+					Check: resource.ComposeTestCheckFunc(
+						testAccCheckScalewayInstanceSecurityGroupExists("scaleway_instance_security_group.sg01"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.#", "1"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.0.action", "drop"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.0.port", "443"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.0.ip_range", "0.0.0.0/0"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.#", "1"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.0.action", "drop"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.0.port", "443"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.0.ip_range", "0.0.0.0/0"),
+					),
+				},
+				{
+					// We test that we can remove all rules
+					Config: `
+						resource scaleway_instance_security_group sg01 {
+							external_rules = true
+						}
+				
+						resource scaleway_instance_security_group_rules sgrs01 {
+							security_group_id = scaleway_instance_security_group.sg01.id
+						}
+					`,
+					Check: resource.ComposeTestCheckFunc(
+						testAccCheckScalewayInstanceSecurityGroupExists("scaleway_instance_security_group.sg01"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.#", "0"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.#", "0"),
+					),
+				},
 			},
-		},
+		})
+	})
+
+	t.Run("Import", func(t *testing.T) {
+		config := `
+			resource scaleway_instance_security_group sg01 {
+				external_rules = true
+			}
+			resource scaleway_instance_security_group_rules sgrs01 {
+				security_group_id = scaleway_instance_security_group.sg01.id
+				inbound_rule {
+					action = "accept"
+					port = 80
+					ip_range = "0.0.0.0/0"
+				}
+				inbound_rule {
+					action = "drop"
+					port = 443
+					ip_range = "0.0.0.0/0"
+				}
+				outbound_rule {
+					action = "accept"
+					port = 80
+					ip_range = "0.0.0.0/0"
+				}
+				outbound_rule {
+					action = "drop"
+					port = 443
+					ip_range = "0.0.0.0/0"
+				}
+			}
+		`
+
+		resource.Test(t, resource.TestCase{
+			PreCheck:     func() { testAccPreCheck(t) },
+			Providers:    testAccProviders,
+			CheckDestroy: testAccCheckScalewayInstanceSecurityGroupDestroy,
+			Steps: []resource.TestStep{
+				{
+					Config: config,
+				},
+				{
+					ImportState:  true,
+					ResourceName: "scaleway_instance_security_group_rules.sgrs01",
+					Config:       config,
+					Check: resource.ComposeTestCheckFunc(
+						testAccCheckScalewayInstanceSecurityGroupExists("scaleway_instance_security_group.sg01"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.#", "2"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.0.action", "accept"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.0.port", "80"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.0.ip_range", "0.0.0.0/0"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.1.action", "drop"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.1.port", "443"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "inbound_rule.1.ip_range", "0.0.0.0/0"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.#", "2"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.0.action", "accept"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.0.port", "80"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.0.ip_range", "0.0.0.0/0"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.1.action", "drop"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.1.port", "443"),
+						resource.TestCheckResourceAttr("scaleway_instance_security_group_rules.sgrs01", "outbound_rule.1.ip_range", "0.0.0.0/0"),
+					),
+				},
+			},
+		})
 	})
 }
