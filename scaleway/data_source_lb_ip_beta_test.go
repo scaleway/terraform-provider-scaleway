6ba45a6085da5901df86c68e16b35fa1ff0087f1
Patrik Cyvoct <pcyvoct@scaleway.com>
2020-06-02 08:11:54 +0200

    feat(lb): add LB IP data source
    Signed-off-by: Patrik Cyvoct <pcyvoct@scaleway.com>

---
+++ scaleway/data_source_lb_ip_beta_test.go
@@ -0,0 +1,36 @@
+package scaleway
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccScalewayDataSourceLbIPBeta_Basic(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckScalewayLbIPBetaDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					resource "scaleway_lb_ip_beta" "test" {
+					}
+					
+					data "scaleway_lb_ip_beta" "test" {
+						ip_address = "${scaleway_lb_ip_beta.test.ip_address}"
+					}
+					
+					data "scaleway_lb_ip_beta" "test2" {
+						ip_id = "${scaleway_lb_ip_beta.test.id}"
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayLbIPBetaExists("data.scaleway_lb_ip_beta.test"),
+					resource.TestCheckResourceAttrPair("data.scaleway_lb_ip_beta.test", "ip_address", "scaleway_lb_ip_beta.test", "ip_address"),
+					resource.TestCheckResourceAttrPair("data.scaleway_lb_ip_beta.test2", "ip_address", "scaleway_lb_ip_beta.test", "ip_address"),
+				),
+			},
+		},
+	})
+}
