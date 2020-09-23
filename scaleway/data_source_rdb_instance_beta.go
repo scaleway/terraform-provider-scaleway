e1e46458b0bf9a2911ec997f8744f5890fa6d3c9
Rémy Léone <rleone@scaleway.com>
2020-09-23 17:25:56 +0200

    Fix

--- scaleway/data_source_rdb_instance_beta.go
+++ scaleway/data_source_rdb_instance_beta.go
@@ -9,100 +9,52 @@ import (
 )
 
 func dataSourceScalewayRDBInstanceBeta() *schema.Resource {
-	return &schema.Resource{
-		Read: func(data *schema.ResourceData, i interface{}) error {
-			api, region, err := rdbAPIWithRegion(data, i)
-			if err != nil {
-				return err
-			}
+	// Generate datasource schema from resource
+	dsSchema := datasourceSchemaFromResourceSchema(resourceScalewayRdbInstanceBeta().Schema)
 
-			var instance *rdb.Instance
-			instanceID, ok := data.GetOk("instance_id")
-			if !ok {
-				if data.Get("instance_id") != "" {
-					instanceID = scw.StringPtr(expandID(data.Get("instance_id")))
-				}
-				res, err := api.ListInstances(&rdb.ListInstancesRequest{
-					Region: region,
-					Name:   String(data.Get("name").(string)),
-				})
-				if err != nil {
-					return err
-				}
-				if len(res.Instances) == 0 {
-					return fmt.Errorf("no instances found with the name %s", data.Get("name"))
-				}
-				if len(res.Instances) > 1 {
-					return fmt.Errorf("%d instances found with the same name %s", len(res.Instances), data.Get("name"))
-				}
-				instance = res.Instances[0]
-			} else {
-				res, err := api.GetInstance(&rdb.GetInstanceRequest{
-					Region:     region,
-					InstanceID: expandID(instanceID),
-				})
-				if err != nil {
-					return err
-				}
-				instance = res
-			}
-
-			data.SetId(datasourceNewRegionalizedID(instance.ID, region))
-			_ = data.Set("instance_id", instance.ID)
-			_ = data.Set("name", instance.Name)
-			_ = data.Set("tags", instance.Tags)
-
-			return nil
-		},
-
-		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:          schema.TypeString,
-				Optional:      true,
-				Description:   "The name of the rdb instance",
-				ConflictsWith: []string{"instance_id"},
-			},
-			"instance_id": {
-				Type:          schema.TypeString,
-				Optional:      true,
-				Description:   "The ID of the rdb instance",
-				ValidateFunc:  validationUUIDorUUIDWithLocality(),
-				ConflictsWith: []string{"name"},
-			},
-			"tags": {
-				Type:     schema.TypeList,
-				Optional: true,
-				Computed: true,
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
-				},
-				Description: "The tags associated with the rdb instance",
-			},
-			"node_type": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "The type of database instance you want to create",
-			},
-			"engine": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				ForceNew:    true,
-				Description: "Database's engine version id",
-			},
-			"is_ha_cluster": {
-				Type:        schema.TypeBool,
-				Optional:    true,
-				Default:     false,
-				Description: "Enable or disable high availability for the database instance",
-			},
-			"disable_backup": {
-				Type:        schema.TypeBool,
-				Optional:    true,
-				Default:     false,
-				Description: "Disable automated backup for the database instance",
-			},
-			"region":          regionSchema(),
-			"organization_id": organizationIDSchema(),
-		},
+	dsSchema["instance_id"] = &schema.Schema{
+		Type:          schema.TypeString,
+		Optional:      true,
+		Description:   "The ID of the RDB instance",
+		ConflictsWith: []string{"name"},
+		ValidateFunc:  validationUUIDorUUIDWithLocality(),
 	}
+
+	return &schema.Resource{
+		Read:   dataSourceScalewayRDBInstanceBetaRead,
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceScalewayRDBInstanceBetaRead(d *schema.ResourceData, m interface{}) error {
+	api, region, err := rdbAPIWithRegion(d, m)
+	if err != nil {
+		return err
+	}
+
+	instanceID, ok := d.GetOk("instance_id")
+	if !ok { // Get instance by region and name.
+		res, err := api.ListInstances(&rdb.ListInstancesRequest{
+			Region: region,
+			Name:   scw.StringPtr(d.Get("name").(string)),
+		})
+		if err != nil {
+			return err
+		}
+		if len(res.Instances) == 0 {
+			return fmt.Errorf("no instances found with the name %s", d.Get("name"))
+		}
+		if len(res.Instances) > 1 {
+			return fmt.Errorf("%d instances found with the same name %s", len(res.Instances), d.Get("name"))
+		}
+		instanceID = res.Instances[0].ID
+	}
+
+	regionalID := datasourceNewRegionalizedID(instanceID, region)
+	d.SetId(regionalID)
+	err = d.Set("instance_id", regionalID)
+	if err != nil {
+		return err
+	}
+	return resourceScalewayRdbInstanceBetaRead(d, m)
 }
