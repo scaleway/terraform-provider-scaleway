dd669150cb43de05de47a7f929f9af8a8dd6d30c
Rémy Léone <rleone@scaleway.com>
2020-09-29 10:55:48 +0200

    fix gosimple

--- scaleway/resource_instance_server_test.go
+++ scaleway/resource_instance_server_test.go
@@ -805,11 +805,11 @@ resource "scaleway_instance_volume" "base_volume%d" {
 	}
 
 	if withBlock {
-		additionalVolumeResources += fmt.Sprintf(`
+		additionalVolumeResources += `
 resource "scaleway_instance_volume" "base_block" {
   size_in_gb = 10
   type       = "b_ssd"
-}`)
+}`
 		additionalVolumeIDs = append(additionalVolumeIDs, `"${scaleway_instance_volume.base_block.id}"`)
 	}
 	return fmt.Sprintf(`
