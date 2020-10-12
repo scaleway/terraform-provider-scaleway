3f14e080f6c647a63b006b48aa0ac7666fc38565
Rémy Léone <rleone@scaleway.com>
2020-10-12 15:37:19 +0200

    chore(instance): migrate to context based workflow (#615)

--- scaleway/resource_instance_server.go
+++ scaleway/resource_instance_server.go
@@ -2,11 +2,13 @@ package scaleway
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"strconv"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/scaleway/scaleway-sdk-go/api/instance/v1"
@@ -16,10 +18,10 @@ import (
 
 func resourceScalewayInstanceServer() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceScalewayInstanceServerCreate,
-		Read:   resourceScalewayInstanceServerRead,
-		Update: resourceScalewayInstanceServerUpdate,
-		Delete: resourceScalewayInstanceServerDelete,
+		CreateContext: resourceScalewayInstanceServerCreate,
+		ReadContext:   resourceScalewayInstanceServerRead,
+		UpdateContext: resourceScalewayInstanceServerUpdate,
+		DeleteContext: resourceScalewayInstanceServerDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
@@ -218,10 +220,10 @@ func resourceScalewayInstanceServer() *schema.Resource {
 	}
 }
 
-func resourceScalewayInstanceServerCreate(d *schema.ResourceData, m interface{}) error {
+func resourceScalewayInstanceServerCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	instanceAPI, zone, err := instanceAPIWithZone(d, m)
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 
 	////
@@ -239,7 +241,7 @@ func resourceScalewayInstanceServerCreate(d *schema.ResourceData, m interface{})
 			ImageLabel:     image.ID,
 		})
 		if err != nil {
-			return fmt.Errorf("could not get image '%s': %s", image, err)
+			return diag.FromErr(fmt.Errorf("could not get image '%s': %s", image, err))
 		}
 		image = newZonedID(zone, imageUUID)
 	}
@@ -281,9 +283,9 @@ func resourceScalewayInstanceServerCreate(d *schema.ResourceData, m interface{})
 		}
 	}
 
-	res, err := instanceAPI.CreateServer(req)
+	res, err := instanceAPI.CreateServer(req, scw.WithContext(ctx))
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 
 	d.SetId(newZonedID(zone, res.Server.ID).String())
@@ -313,26 +315,26 @@ func resourceScalewayInstanceServerCreate(d *schema.ResourceData, m interface{})
 	if len(userDataRequests.UserData) > 0 {
 		err := instanceAPI.SetAllServerUserData(userDataRequests)
 		if err != nil {
-			return err
+			return diag.FromErr(err)
 		}
 	}
 
 	targetState, err := serverStateExpand(d.Get("state").(string))
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
-	err = reachState(instanceAPI, zone, res.Server.ID, targetState)
+	err = reachState(ctx, instanceAPI, zone, res.Server.ID, targetState)
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 
-	return resourceScalewayInstanceServerRead(d, m)
+	return resourceScalewayInstanceServerRead(ctx, d, m)
 }
 
-func resourceScalewayInstanceServerRead(d *schema.ResourceData, m interface{}) error {
+func resourceScalewayInstanceServerRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	instanceAPI, zone, ID, err := instanceAPIWithZoneAndID(m, d.Id())
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 
 	////
@@ -341,17 +343,17 @@ func resourceScalewayInstanceServerRead(d *schema.ResourceData, m interface{}) e
 	response, err := instanceAPI.GetServer(&instance.GetServerRequest{
 		Zone:     zone,
 		ServerID: ID,
-	})
+	}, scw.WithContext(ctx))
 	if err != nil {
 		if is404Error(err) {
 			d.SetId("")
 			return nil
 		}
-		return err
+		return diag.FromErr(err)
 	}
 	state, err := serverStateFlatten(response.Server.State)
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 
 	_ = d.Set("state", state)
@@ -404,7 +406,7 @@ func resourceScalewayInstanceServerRead(d *schema.ResourceData, m interface{}) e
 		_ = d.Set("ipv6_gateway", response.Server.IPv6.Gateway.String())
 		prefixLength, err := strconv.Atoi(response.Server.IPv6.Netmask)
 		if err != nil {
-			return err
+			return diag.FromErr(err)
 		}
 		_ = d.Set("ipv6_prefix_length", prefixLength)
 	} else {
@@ -443,13 +445,13 @@ func resourceScalewayInstanceServerRead(d *schema.ResourceData, m interface{}) e
 	allUserData, _ := instanceAPI.GetAllServerUserData(&instance.GetAllServerUserDataRequest{
 		Zone:     zone,
 		ServerID: ID,
-	})
+	}, scw.WithContext(ctx))
 
 	var userDataList []interface{}
 	for key, value := range allUserData.UserData {
 		userData, err := ioutil.ReadAll(value)
 		if err != nil {
-			return err
+			return diag.FromErr(err)
 		}
 		if key != "cloud-init" {
 			userDataList = append(userDataList, map[string]interface{}{
@@ -467,10 +469,10 @@ func resourceScalewayInstanceServerRead(d *schema.ResourceData, m interface{}) e
 	return nil
 }
 
-func resourceScalewayInstanceServerUpdate(d *schema.ResourceData, m interface{}) error {
+func resourceScalewayInstanceServerUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	instanceAPI, zone, ID, err := instanceAPIWithZoneAndID(m, d.Id())
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 
 	// This variable will be set to true if any state change requires a server reboot.
@@ -515,9 +517,9 @@ func resourceScalewayInstanceServerUpdate(d *schema.ResourceData, m interface{})
 
 		for i, volumeID := range raw.([]interface{}) {
 			// We make sure volume is detached so we can attach it to the server.
-			err = detachVolume(instanceAPI, zone, expandZonedID(volumeID).ID)
+			err = detachVolume(nil, instanceAPI, zone, expandZonedID(volumeID).ID)
 			if err != nil {
-				return err
+				return diag.FromErr(err)
 			}
 			volumes[strconv.Itoa(i+1)] = &instance.VolumeTemplate{
 				ID:   expandZonedID(volumeID).ID,
@@ -548,7 +550,7 @@ func resourceScalewayInstanceServerUpdate(d *schema.ResourceData, m interface{})
 			ServerID: ID,
 		})
 		if err != nil {
-			return err
+			return diag.FromErr(err)
 		}
 		newIPID := expandZonedID(d.Get("ip_id")).ID
 
@@ -560,7 +562,7 @@ func resourceScalewayInstanceServerUpdate(d *schema.ResourceData, m interface{})
 				Server: &instance.NullableStringValue{Null: true},
 			})
 			if err != nil {
-				return err
+				return diag.FromErr(err)
 			}
 		}
 
@@ -570,9 +572,9 @@ func resourceScalewayInstanceServerUpdate(d *schema.ResourceData, m interface{})
 				Zone:   zone,
 				IP:     newIPID,
 				Server: &instance.NullableStringValue{Value: ID},
-			})
+			}, scw.WithContext(ctx))
 			if err != nil {
-				return err
+				return diag.FromErr(err)
 			}
 		}
 	}
@@ -603,7 +605,7 @@ func resourceScalewayInstanceServerUpdate(d *schema.ResourceData, m interface{})
 
 		err := instanceAPI.SetAllServerUserData(userDataRequests)
 		if err != nil {
-			return err
+			return diag.FromErr(err)
 		}
 	}
 
@@ -614,53 +616,53 @@ func resourceScalewayInstanceServerUpdate(d *schema.ResourceData, m interface{})
 	defer lockLocalizedID(d.Id())()
 
 	if forceReboot {
-		err = reachState(instanceAPI, zone, ID, InstanceServerStateStopped)
+		err = reachState(ctx, instanceAPI, zone, ID, InstanceServerStateStopped)
 		if err != nil {
-			return err
+			return diag.FromErr(err)
 		}
 	}
 	_, err = instanceAPI.UpdateServer(updateRequest)
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 
 	targetState, err := serverStateExpand(d.Get("state").(string))
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 
 	// reach expected state
-	err = reachState(instanceAPI, zone, ID, targetState)
+	err = reachState(ctx, instanceAPI, zone, ID, targetState)
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 
-	return resourceScalewayInstanceServerRead(d, m)
+	return resourceScalewayInstanceServerRead(ctx, d, m)
 }
 
-func resourceScalewayInstanceServerDelete(d *schema.ResourceData, m interface{}) error {
+func resourceScalewayInstanceServerDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	instanceAPI, zone, ID, err := instanceAPIWithZoneAndID(m, d.Id())
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 	defer lockLocalizedID(d.Id())()
 
 	// reach stopped state
-	err = reachState(instanceAPI, zone, ID, instance.ServerStateStopped)
+	err = reachState(ctx, instanceAPI, zone, ID, instance.ServerStateStopped)
 	if is404Error(err) {
 		return nil
 	}
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 
 	err = instanceAPI.DeleteServer(&instance.DeleteServerRequest{
 		Zone:     zone,
 		ServerID: ID,
-	})
+	}, scw.WithContext(ctx))
 
 	if err != nil && !is404Error(err) {
-		return err
+		return diag.FromErr(err)
 	}
 
 	if d.Get("root_volume.0.delete_on_termination").(bool) {
@@ -669,7 +671,7 @@ func resourceScalewayInstanceServerDelete(d *schema.ResourceData, m interface{})
 			VolumeID: expandZonedID(d.Get("root_volume.0.volume_id")).ID,
 		})
 		if err != nil && !is404Error(err) {
-			return err
+			return diag.FromErr(err)
 		}
 	}
 
