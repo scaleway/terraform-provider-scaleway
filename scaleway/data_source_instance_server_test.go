62a07864da283d327118bad2d23cb525719ab1d0
Rémy Léone <rleone@scaleway.com>
2020-10-19 15:54:51 +0200

    chore(instance): add support for cassettes based testing (#645)

--- scaleway/data_source_instance_server_test.go
+++ scaleway/data_source_instance_server_test.go
@@ -1,39 +1,50 @@
 package scaleway
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccScalewayDataSourceInstanceServer_Basic(t *testing.T) {
-	serverName := acctest.RandString(10)
+	tt := NewTestTools(t)
+	defer tt.Cleanup()
+	serverName := "tf-server"
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckScalewayInstanceServerDestroy,
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      testAccCheckScalewayInstanceServerDestroy(tt),
 		Steps: []resource.TestStep{
 			{
-				Config: `
-resource "scaleway_instance_server" "main" {
-  name 	= "` + serverName + `"
-  image = "ubuntu_focal"
-  type  = "DEV1-S"
-  tags  = [ "terraform-test", "data_scaleway_instance_server", "basic" ]
-}
-
-data "scaleway_instance_server" "prod" {
-  name = "${scaleway_instance_server.main.name}"
-}
-
-data "scaleway_instance_server" "stg" {
-  server_id = "${scaleway_instance_server.main.id}"
-}`,
+				Config: fmt.Sprintf(`
+					resource "scaleway_instance_server" "main" {
+					  	name 	= "%s"
+					  	image = "ubuntu_focal"
+					  	type  = "DEV1-S"
+					  	tags  = [ "terraform-test", "data_scaleway_instance_server", "basic" ]
+					}`, serverName),
+			},
+			{
+				Config: fmt.Sprintf(`
+					resource "scaleway_instance_server" "main" {
+					  	name 	= "%s"
+					  	image = "ubuntu_focal"
+					  	type  = "DEV1-S"
+					  	tags  = [ "terraform-test", "data_scaleway_instance_server", "basic" ]
+					}
+					
+					data "scaleway_instance_server" "prod" {
+					  	name = "${scaleway_instance_server.main.name}"
+					}
+					
+					data "scaleway_instance_server" "stg" {
+					  	server_id = "${scaleway_instance_server.main.id}"
+					}`, serverName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckScalewayInstanceServerExists("data.scaleway_instance_server.prod"),
+					testAccCheckScalewayInstanceServerExists(tt, "data.scaleway_instance_server.prod"),
 					resource.TestCheckResourceAttr("data.scaleway_instance_server.prod", "name", serverName),
-					testAccCheckScalewayInstanceServerExists("data.scaleway_instance_server.stg"),
+					testAccCheckScalewayInstanceServerExists(tt, "data.scaleway_instance_server.stg"),
 					resource.TestCheckResourceAttr("data.scaleway_instance_server.stg", "name", serverName),
 				),
 			},
