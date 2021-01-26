7200d6d65a2ae363acbaa5db5e8cdcb2e35e6cf4
Lichard Torman <didjcodt@users.noreply.github.com>
2021-01-26 16:50:21 +0100

    feat(iot): add support for hub resource (#503)
    Co-authored-by: Clément Decoodt <cdecoodt@scaleway.com>
    Co-authored-by: Rémy Léone <rleone@scaleway.com>

---
+++ scaleway/helpers_iot.go
@@ -0,0 +1,44 @@
+package scaleway
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	iot "github.com/scaleway/scaleway-sdk-go/api/iot/v1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+)
+
+func iotAPIWithRegion(d *schema.ResourceData, m interface{}) (*iot.API, scw.Region, error) {
+	meta := m.(*Meta)
+	iotAPI := iot.NewAPI(meta.scwClient)
+
+	region, err := extractRegion(d, meta)
+
+	return iotAPI, region, err
+}
+
+func iotAPIWithRegionAndID(m interface{}, id string) (*iot.API, scw.Region, string, error) {
+	meta := m.(*Meta)
+	iotAPI := iot.NewAPI(meta.scwClient)
+
+	region, ID, err := parseRegionalID(id)
+	return iotAPI, region, ID, err
+}
+
+func waitIotHub(iotAPI *iot.API, region scw.Region, hubID string, desiredStates ...iot.HubStatus) error {
+	hub, err := iotAPI.WaitForHub(&iot.WaitForHubRequest{
+		HubID:  hubID,
+		Region: region,
+	})
+	if err != nil {
+		return err
+	}
+
+	for _, desiredState := range desiredStates {
+		if hub.Status == desiredState {
+			return nil
+		}
+	}
+
+	return fmt.Errorf("hub %s has state %s, wants one of %+q", hubID, hub.Status, desiredStates)
+}
