359e7d9f36be22e785d2ac46be10c17180d69d0d
Rémy Léone <rleone@scaleway.com>
2021-02-05 11:16:50 +0100

    feat(as): add support for apple silicon M1-M

---
+++ scaleway/resource_apple_silicon_server.go
@@ -0,0 +1,178 @@
+package scaleway
+
+import (
+	"context"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	applesilicon "github.com/scaleway/scaleway-sdk-go/api/applesilicon/v1alpha1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+)
+
+func resourceScalewayAppleSiliconServer() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceScalewayAppleSiliconServerCreate,
+		ReadContext:   resourceScalewayAppleSiliconServerRead,
+		UpdateContext: resourceScalewayAppleSiliconServerUpdate,
+		DeleteContext: resourceScalewayAppleSiliconServerDelete,
+		Timeouts: &schema.ResourceTimeout{
+			Default: schema.DefaultTimeout(defaultAppleSiliconServerTimeout),
+		},
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		SchemaVersion: 0,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:        schema.TypeString,
+				Description: "Name of the server",
+				Optional:    true,
+			},
+			"type": {
+				Type:        schema.TypeString,
+				Description: "Type of the server",
+				Required:    true,
+				ForceNew:    true,
+			},
+
+			// Computed
+			"ip": {
+				Type:        schema.TypeString,
+				Description: "IPv4 address of the server",
+				Computed:    true,
+			},
+			"vnc_url": {
+				Type:        schema.TypeString,
+				Description: "VNC url use to connect remotely to the desktop GUI",
+				Computed:    true,
+			},
+
+			"created_at": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The date and time of the creation of the server",
+			},
+			"updated_at": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The date and time of the last update of the server",
+			},
+			"deletable_at": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The minimal date and time on which you can delete this server due to Apple licence",
+			},
+
+			// Common
+			"zone":            zoneSchema(),
+			"organization_id": organizationIDSchema(),
+			"project_id":      projectIDSchema(),
+		},
+	}
+}
+
+func resourceScalewayAppleSiliconServerCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	asAPI, zone, err := asAPIWithZone(d, m)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	createReq := &applesilicon.CreateServerRequest{
+		Name:      expandOrGenerateString(d.Get("name"), "m1"),
+		Type:      d.Get("type").(string),
+		ProjectID: d.Get("project_id").(string),
+	}
+
+	res, err := asAPI.CreateServer(createReq, scw.WithContext(ctx))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(newZonedIDString(zone, res.ID))
+
+	_, err = asAPI.WaitForServer(&applesilicon.WaitForServerRequest{
+		ServerID:      res.ID,
+		Timeout:       scw.TimeDurationPtr(defaultAppleSiliconServerTimeout),
+		RetryInterval: nil,
+	}, scw.WithContext(ctx))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return resourceScalewayRdbInstanceRead(ctx, d, m)
+}
+
+func resourceScalewayAppleSiliconServerRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	asAPI, zone, ID, err := asAPIWithZoneAndID(m, d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	res, err := asAPI.GetServer(&applesilicon.GetServerRequest{
+		Zone:     zone,
+		ServerID: ID,
+	}, scw.WithContext(ctx))
+	if err != nil {
+		if is404Error(err) {
+			d.SetId("")
+			return nil
+		}
+		return diag.FromErr(err)
+	}
+
+	_ = d.Set("name", res.Name)
+	_ = d.Set("type", res.Type)
+	_ = d.Set("created_at", res.CreatedAt.Format(time.RFC3339))
+	_ = d.Set("updated_at", res.UpdatedAt.Format(time.RFC3339))
+	_ = d.Set("deletable_at", res.DeletableAt.Format(time.RFC3339))
+	_ = d.Set("ip", res.IP)
+	_ = d.Set("vnc_url", res.VncURL)
+
+	_ = d.Set("zone", string(zone))
+	_ = d.Set("organization_id", res.OrganizationID)
+	_ = d.Set("project_id", res.ProjectID)
+
+	return nil
+}
+
+func resourceScalewayAppleSiliconServerUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	asAPI, zone, ID, err := asAPIWithZoneAndID(m, d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	req := &applesilicon.UpdateServerRequest{
+		Zone:     zone,
+		ServerID: ID,
+	}
+
+	if d.HasChange("name") {
+		req.Name = expandStringPtr(d.Get("name"))
+	}
+
+	_, err = asAPI.UpdateServer(req, scw.WithContext(ctx))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return resourceScalewayAppleSiliconServerRead(ctx, d, m)
+}
+
+func resourceScalewayAppleSiliconServerDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	asAPI, zone, ID, err := asAPIWithZoneAndID(m, d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	err = asAPI.DeleteServer(&applesilicon.DeleteServerRequest{
+		Zone:     zone,
+		ServerID: ID,
+	}, scw.WithContext(ctx))
+
+	if err != nil && !is404Error(err) {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
---
+++ scaleway/resource_apple_silicon_server_test.go
@@ -0,0 +1,128 @@
+package scaleway
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	applesilicon "github.com/scaleway/scaleway-sdk-go/api/applesilicon/v1alpha1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+)
+
+func init() {
+	resource.AddTestSweepers("scaleway_apple_silicon_instance", &resource.Sweeper{
+		Name: "scaleway_apple_silicon",
+		F:    testSweepAppleSiliconServer,
+	})
+}
+
+func testSweepAppleSiliconServer(_ string) error {
+	return sweepZones(scw.AllZones, func(scwClient *scw.Client, zone scw.Zone) error {
+		asAPI := applesilicon.NewAPI(scwClient)
+		l.Debugf("sweeper: destroying the apple silicon instance in (%s)", zone)
+		listServers, err := asAPI.ListServers(&applesilicon.ListServersRequest{}, scw.WithAllPages())
+		if err != nil {
+			return fmt.Errorf("error listing apple silicon servers in (%s) in sweeper: %s", zone, err)
+		}
+
+		for _, server := range listServers.Servers {
+			errDelete := asAPI.DeleteServer(&applesilicon.DeleteServerRequest{
+				ServerID: server.ID,
+				Zone:     zone,
+			})
+			if errDelete != nil {
+				return fmt.Errorf("error deleting apple silicon server in sweeper: %s", err)
+			}
+		}
+
+		return nil
+	})
+}
+
+func TestAccScalewayAppleSiliconServer_Basic(t *testing.T) {
+	tt := NewTestTools(t)
+	defer tt.Cleanup()
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      testAccCheckScalewayAppleSiliconServerDestroy(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					resource scaleway_apple_silicon_server main {
+						name = "test-m1"
+						type = "M1-M"
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayAppleSiliconExists(tt, "scaleway_apple_silicon_server.main"),
+					resource.TestCheckResourceAttr("scaleway_apple_silicon_server.main", "name", "test-m1"),
+					resource.TestCheckResourceAttr("scaleway_apple_silicon_server.main", "type", "M1-M"),
+					// Computed
+					resource.TestCheckResourceAttrSet("scaleway_apple_silicon_server.main", "ip"),
+					resource.TestCheckResourceAttrSet("scaleway_apple_silicon_server.main", "vnc_url"),
+					resource.TestCheckResourceAttrSet("scaleway_apple_silicon_server.main", "created_at"),
+					resource.TestCheckResourceAttrSet("scaleway_apple_silicon_server.main", "deletable_at"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckScalewayAppleSiliconExists(tt *TestTools, n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", n)
+		}
+
+		asAPI, zone, ID, err := asAPIWithZoneAndID(tt.Meta, rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		_, err = asAPI.GetServer(&applesilicon.GetServerRequest{
+			ServerID: ID,
+			Zone:     zone,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckScalewayAppleSiliconServerDestroy(tt *TestTools) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		for _, rs := range state.RootModule().Resources {
+			if rs.Type != "scaleway_apple_silicon_server" {
+				continue
+			}
+
+			asAPI, zone, ID, err := asAPIWithZoneAndID(tt.Meta, rs.Primary.ID)
+			if err != nil {
+				return err
+			}
+
+			_, err = asAPI.GetServer(&applesilicon.GetServerRequest{
+				ServerID: ID,
+				Zone:     zone,
+			})
+
+			// If no error resource still exist
+			if err == nil {
+				return fmt.Errorf("server (%s) still exists", rs.Primary.ID)
+			}
+
+			// Unexpected api error we return it
+			if !is404Error(err) {
+				return err
+			}
+		}
+
+		return nil
+	}
+}
