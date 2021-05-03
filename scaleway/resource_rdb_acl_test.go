e56d619ca32fdc301273d3cd7ef806b55758cc9c
Jean-Sébastien Hedde <jshedde@scaleway.com>
2021-05-03 20:59:30 +0200

    tests: testing update

--- scaleway/resource_rdb_acl_test.go
+++ scaleway/resource_rdb_acl_test.go
@@ -51,6 +51,34 @@ func TestAccScalewayRdbACL_Basic(t *testing.T) {
 					resource.TestCheckResourceAttr("scaleway_rdb_acl.main", "acl_rules.1.description", "bar"),
 				),
 			},
+			{
+				Config: fmt.Sprintf(`
+					resource scaleway_rdb_instance main {
+						name = "%s"
+						node_type = "db-dev-s"
+						engine = "PostgreSQL-12"
+						is_ha_cluster = false
+					}
+
+					resource scaleway_rdb_acl main {
+						instance_id = scaleway_rdb_instance.main.id
+						acl_rules {
+							ip = "1.2.3.4/32"
+							description = "foo"
+						}
+
+						acl_rules {
+							ip = "9.0.0.0/16"
+							description = "baz"
+						}
+					}`, instanceName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("scaleway_rdb_acl.main", "acl_rules.0.ip", "1.2.3.4/32"),
+					resource.TestCheckResourceAttr("scaleway_rdb_acl.main", "acl_rules.0.description", "foo"),
+					resource.TestCheckResourceAttr("scaleway_rdb_acl.main", "acl_rules.1.ip", "9.0.0.0/16"),
+					resource.TestCheckResourceAttr("scaleway_rdb_acl.main", "acl_rules.1.description", "baz"),
+				),
+			},
 		},
 	})
 }
