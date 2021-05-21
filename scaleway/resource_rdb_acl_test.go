1ada35825c906665fca1fed801fbca74b968c7e7
Rémy Léone <rleone@scaleway.com>
2021-05-21 16:13:24 +0200

    feat(rdb): add support for acl rules (#784)
    Co-authored-by: Jean-Sébastien Hedde <jshedde@scaleway.com>

---
+++ scaleway/resource_rdb_acl_test.go
@@ -0,0 +1,84 @@
+package scaleway
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func init() {
+	resource.AddTestSweepers("scaleway_rdb_acl", &resource.Sweeper{
+		Name: "scaleway_rdb_acl",
+		F:    testSweepRDBInstance,
+	})
+}
+
+func TestAccScalewayRdbACL_Basic(t *testing.T) {
+	tt := NewTestTools(t)
+	defer tt.Cleanup()
+	instanceName := "TestAccScalewayRdbACL_Basic"
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      testAccCheckScalewayRdbInstanceDestroy(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					resource scaleway_rdb_instance main {
+						name = "%s"
+						node_type = "db-dev-s"
+						engine = "PostgreSQL-12"
+						is_ha_cluster = false
+					}
+
+					resource scaleway_rdb_acl main {
+						instance_id = scaleway_rdb_instance.main.id
+						acl_rules {
+							ip = "1.2.3.4/32"
+							description = "foo"
+						}
+
+						acl_rules {
+							ip = "4.5.6.7/32"
+							description = "bar"
+						}
+					}`, instanceName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("scaleway_rdb_acl.main", "acl_rules.0.ip", "1.2.3.4/32"),
+					resource.TestCheckResourceAttr("scaleway_rdb_acl.main", "acl_rules.0.description", "foo"),
+					resource.TestCheckResourceAttr("scaleway_rdb_acl.main", "acl_rules.1.ip", "4.5.6.7/32"),
+					resource.TestCheckResourceAttr("scaleway_rdb_acl.main", "acl_rules.1.description", "bar"),
+				),
+			},
+			{
+				Config: fmt.Sprintf(`
+					resource scaleway_rdb_instance main {
+						name = "%s"
+						node_type = "db-dev-s"
+						engine = "PostgreSQL-12"
+						is_ha_cluster = false
+					}
+
+					resource scaleway_rdb_acl main {
+						instance_id = scaleway_rdb_instance.main.id
+						acl_rules {
+							ip = "1.2.3.4/32"
+							description = "foo"
+						}
+
+						acl_rules {
+							ip = "9.0.0.0/16"
+							description = "baz"
+						}
+					}`, instanceName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("scaleway_rdb_acl.main", "acl_rules.0.ip", "1.2.3.4/32"),
+					resource.TestCheckResourceAttr("scaleway_rdb_acl.main", "acl_rules.0.description", "foo"),
+					resource.TestCheckResourceAttr("scaleway_rdb_acl.main", "acl_rules.1.ip", "9.0.0.0/16"),
+					resource.TestCheckResourceAttr("scaleway_rdb_acl.main", "acl_rules.1.description", "baz"),
+				),
+			},
+		},
+	})
+}
