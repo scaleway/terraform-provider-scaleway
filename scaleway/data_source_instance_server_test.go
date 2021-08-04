fbdbd686478757cbea23f451637227a709218738
Rémy Léone <rleone@scaleway.com>
2021-08-04 14:55:40 +0200

    chore: Bump sdk and fix cassettes (#866)
    Co-authored-by: Jean-Sébastien Hedde <jshedde@scaleway.com>

--- scaleway/data_source_instance_server_test.go
+++ scaleway/data_source_instance_server_test.go
@@ -19,27 +19,29 @@ func TestAccScalewayDataSourceInstanceServer_Basic(t *testing.T) {
 			{
 				Config: fmt.Sprintf(`
 					resource "scaleway_instance_server" "main" {
-					  	name 	= "%s"
-					  	image = "ubuntu_focal"
-					  	type  = "DEV1-S"
-					  	tags  = [ "terraform-test", "data_scaleway_instance_server", "basic" ]
+						name 	= "%s"
+						image = "ubuntu_focal"
+						type  = "DEV1-S"
+						state = "stopped"
+						tags  = [ "terraform-test", "data_scaleway_instance_server", "basic" ]
 					}`, serverName),
 			},
 			{
 				Config: fmt.Sprintf(`
 					resource "scaleway_instance_server" "main" {
-					  	name 	= "%s"
-					  	image = "ubuntu_focal"
-					  	type  = "DEV1-S"
-					  	tags  = [ "terraform-test", "data_scaleway_instance_server", "basic" ]
+						name 	= "%s"
+						image = "ubuntu_focal"
+						type  = "DEV1-S"
+						state = "stopped"
+						tags  = [ "terraform-test", "data_scaleway_instance_server", "basic" ]
 					}
 					
 					data "scaleway_instance_server" "prod" {
-					  	name = "${scaleway_instance_server.main.name}"
+						name = "${scaleway_instance_server.main.name}"
 					}
 					
 					data "scaleway_instance_server" "stg" {
-					  	server_id = "${scaleway_instance_server.main.id}"
+						server_id = "${scaleway_instance_server.main.id}"
 					}`, serverName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckScalewayInstanceServerExists(tt, "data.scaleway_instance_server.prod"),
