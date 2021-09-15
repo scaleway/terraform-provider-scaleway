817691ed0cbbe46ad8bbd4767977d01982027dec
Jean-Sébastien Hedde <jshedde@scaleway.com>
2021-09-15 15:52:21 +0200

    refactoring: using ParseIP to validate the IP

--- scaleway/data_source_instance_ip_test.go
+++ scaleway/data_source_instance_ip_test.go
@@ -1,7 +1,6 @@
 package scaleway
 
 import (
-	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -10,7 +9,6 @@ import (
 func TestAccScalewayDataSourceInstanceIP_Basic(t *testing.T) {
 	tt := NewTestTools(t)
 	defer tt.Cleanup()
-	ipregexp := regexp.MustCompile("(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])")
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: tt.ProviderFactories,
@@ -32,9 +30,9 @@ func TestAccScalewayDataSourceInstanceIP_Basic(t *testing.T) {
 					}
 				`,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestMatchResourceAttr("scaleway_instance_ip.ip", "address", ipregexp),
-					resource.TestMatchResourceAttr("data.scaleway_instance_ip.ip-from-address", "address", ipregexp),
-					resource.TestMatchResourceAttr("data.scaleway_instance_ip.ip-from-id", "address", ipregexp),
+					testCheckResourceAttrIP("scaleway_instance_ip.ip", "address"),
+					testCheckResourceAttrIP("data.scaleway_instance_ip.ip-from-address", "address"),
+					testCheckResourceAttrIP("data.scaleway_instance_ip.ip-from-id", "address"),
 					resource.TestCheckResourceAttrPair("scaleway_instance_ip.ip", "address", "data.scaleway_instance_ip.ip-from-address", "address"),
 					resource.TestCheckResourceAttrPair("scaleway_instance_ip.ip", "address", "data.scaleway_instance_ip.ip-from-id", "address"),
 				),
