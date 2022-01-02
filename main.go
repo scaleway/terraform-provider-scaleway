03795adc8c310825b1501a8415dfd2067e5dc854
Lee Briggs <lee@leebriggs.co.uk>
2022-01-02 09:11:14 -0800

    update to use v2 imports

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/scaleway/terraform-provider-scaleway/scaleway"
+	"github.com/scaleway/terraform-provider-scaleway/scaleway/v2"
 )
 
 func main() {
