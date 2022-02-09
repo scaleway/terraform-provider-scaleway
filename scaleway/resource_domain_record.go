f4c0c72b109df5017fa8f93171664c5e4975fcbc
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2022-02-09 17:32:04 +0100

    feat(domain): add check for data

--- scaleway/resource_domain_record.go
+++ scaleway/resource_domain_record.go
@@ -263,11 +263,7 @@ func resourceScalewayDomainRecordRead(ctx context.Context, d *schema.ResourceDat
 	var dnsZone string
 	var projectID string
 
-	currentData, existData := d.GetOk("data")
-	if !existData {
-		return diag.FromErr(fmt.Errorf("no such data found"))
-	}
-
+	currentData := d.Get("data")
 	// check if this is an inline import. Like: "terraform import scaleway_domain_record.www subdomain.domain.tld/11111111-1111-1111-1111-111111111111"
 	if strings.Contains(d.Id(), "/") {
 		tab := strings.SplitN(d.Id(), "/", -1)
@@ -343,6 +339,10 @@ func resourceScalewayDomainRecordRead(ctx context.Context, d *schema.ResourceDat
 		projectID = z.ProjectID
 	}
 
+	// retrieve data from record
+	if len(currentData.(string)) == 0 {
+		currentData = flattenDomainData(record.Data, record.Type).(string)
+	}
 	d.SetId(record.ID)
 	_ = d.Set("dns_zone", dnsZone)
 	_ = d.Set("name", record.Name)
