d09c92f6742de281c4a783339eac85dbd6ad3246
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2022-04-13 12:17:49 +0200

    fix(instance-ip-reverse): documentation and test

--- scaleway/resource_instance_ip_reverse_dns_test.go
+++ scaleway/resource_instance_ip_reverse_dns_test.go
@@ -1,30 +1,41 @@
 package scaleway
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccScalewayInstanceIPReverseDns_Basic(t *testing.T) {
-	t.Skip("Skipping Reverse DNS because the domain is we can not execute dig +short www.scaleway-terraform.com ")
 	tt := NewTestTools(t)
 	defer tt.Cleanup()
+	testDNSZone := fmt.Sprintf("tf.%s", testDomain)
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: tt.ProviderFactories,
 		CheckDestroy:      testAccCheckScalewayInstanceIPDestroy(tt),
 		Steps: []resource.TestStep{
 			{
-				Config: `
-					resource "scaleway_instance_ip" "ip" {}
-					resource "scaleway_instance_ip_reverse_dns" "base" {
-						ip_id = scaleway_instance_ip.ip.id
-						reverse = "www.scaleway-terraform.com"
+				Config: fmt.Sprintf(`
+					resource "scaleway_instance_ip" "main" {}
+					
+					resource "scaleway_domain_record" "tf_A" {
+						dns_zone = %[1]q
+						name     = "tf"
+						type     = "A"
+						data     = "${scaleway_instance_ip.main.address}"
+						ttl      = 3600
+						priority = 1
 					}
-				`,
+
+					resource "scaleway_instance_ip_reverse_dns" "base" {
+						ip_id = scaleway_instance_ip.main.id
+						reverse = %[2]q
+					}
+				`, testDomain, testDNSZone),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("scaleway_instance_ip_reverse_dns.base", "reverse", "www.scaleway-terraform.com"),
+					resource.TestCheckResourceAttr("scaleway_instance_ip_reverse_dns.base", "reverse", testDNSZone),
 				),
 			},
 			{
