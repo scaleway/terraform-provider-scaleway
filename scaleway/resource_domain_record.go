761236b47091a8532b1cb2f593d4f6bdb2c9676c
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2022-04-20 16:27:17 +0200

    fix: add error and use tflog

--- scaleway/resource_domain_record.go
+++ scaleway/resource_domain_record.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
@@ -469,7 +470,7 @@ func resourceScalewayDomainRecordDelete(ctx context.Context, d *schema.ResourceD
 				hasRecords = true
 				break
 			}
-			l.Debugf("record [%s], type [%s]", r.Name, r.Type)
+			tflog.Debug(ctx, fmt.Sprintf("record [%s], type [%s]", r.Name, r.Type))
 		}
 
 		if !hasRecords {
@@ -482,7 +483,7 @@ func resourceScalewayDomainRecordDelete(ctx context.Context, d *schema.ResourceD
 				if is404Error(err) || is403Error(err) {
 					return nil
 				}
-				return nil
+				return diag.FromErr(err)
 			}
 		}
 	}
