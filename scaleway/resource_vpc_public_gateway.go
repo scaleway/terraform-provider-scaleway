bcca1e037d9e7a26aa433da0ee93bfceb9e6669d
Rémy Léone <rleone@scaleway.com>
2022-05-04 12:08:27 +0200

    fix: set resource id to "" when resource is found absent (#1241)

--- scaleway/resource_vpc_public_gateway.go
+++ scaleway/resource_vpc_public_gateway.go
@@ -117,6 +117,10 @@ func resourceScalewayVPCPublicGatewayRead(ctx context.Context, d *schema.Resourc
 
 	gateway, err := waitForVPCPublicGateway(ctx, vpcgwAPI, zone, id, d.Timeout(schema.TimeoutRead))
 	if err != nil {
+		if is404Error(err) {
+			d.SetId("")
+			return nil
+		}
 		return diag.FromErr(err)
 	}
 
