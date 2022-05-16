875f1eb21d1b492d5e12aac34214a6b2cc2bca15
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2022-05-16 15:57:57 +0200

    fix: lint import

--- scaleway/resource_rdb_acl.go
+++ scaleway/resource_rdb_acl.go
@@ -4,10 +4,10 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"net"
 	"sort"
 
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
