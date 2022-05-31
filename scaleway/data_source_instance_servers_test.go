7c3ffac4181dcfc3b7481d2f4d192842880613f9
Jules Casteran <jcasteran@scaleway.com>
2022-05-31 14:22:01 +0200

    fix tests split into steps

--- scaleway/data_source_instance_servers_test.go
+++ scaleway/data_source_instance_servers_test.go
@@ -22,6 +22,16 @@ func TestAccScalewayDataSourceInstanceServers_Basic(t *testing.T) {
 						type  = "DEV1-S"
 						state = "stopped"
 						tags  = [ "terraform-test", "data_scaleway_instance_servers", "basic" ]
+					}`,
+			},
+			{
+				Config: `
+					resource "scaleway_instance_server" "server1" {
+						name  = "tf-server-datasource0"
+						image = "ubuntu_focal"
+						type  = "DEV1-S"
+						state = "stopped"
+						tags  = [ "terraform-test", "data_scaleway_instance_servers", "basic" ]
 					}
 
 					resource "scaleway_instance_server" "server2" {
