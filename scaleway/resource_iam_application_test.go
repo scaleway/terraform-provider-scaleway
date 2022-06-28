3c553ab49f54f3dcbe89401e8abd245afb392f03
Jules Castéran <jcasteran@scaleway.com>
2022-06-28 11:54:21 +0200

    fix(iam_application): disable sweeper if beta is not enabled (#1345)

--- scaleway/resource_iam_application_test.go
+++ scaleway/resource_iam_application_test.go
@@ -11,6 +11,9 @@ import (
 )
 
 func init() {
+	if !terraformBetaEnabled {
+		return
+	}
 	resource.AddTestSweepers("scaleway_iam_application", &resource.Sweeper{
 		Name: "scaleway_iam_application",
 		F:    testSweepIamApplication,
