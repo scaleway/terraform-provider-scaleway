b9a32e5c04a695863efe40a3925e6ff64d7367e9
Yacine FODIL <yfodil@scaleway.com>
2022-06-30 10:46:41 +0200

    fix(iam): ssh_key: datasource refacto error handling

--- scaleway/data_source_iam_ssh_key.go
+++ scaleway/data_source_iam_ssh_key.go
@@ -1,13 +1,13 @@
 package scaleway
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	iam "github.com/scaleway/scaleway-sdk-go/api/iam/v1alpha1"
 	"github.com/scaleway/scaleway-sdk-go/scw"
-	"golang.org/x/net/context"
 )
 
 func dataSourceScalewayIamSSHKey() *schema.Resource {
@@ -32,15 +32,8 @@ func dataSourceScalewayIamSSHKey() *schema.Resource {
 func dataSourceScalewayIamSSHKeyRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	iamAPI := iamAPI(meta)
 
-	var sshKey *iam.SSHKey
-	sshKeyID, ok := d.GetOk("ssh_key_id")
-	if ok {
-		res, err := iamAPI.GetSSHKey(&iam.GetSSHKeyRequest{SSHKeyID: expandID(sshKeyID)}, scw.WithContext(ctx))
-		if err != nil {
-			return diag.FromErr(err)
-		}
-		sshKey = res
-	} else {
+	sshKeyID, sshKeyIDExists := d.GetOk("ssh_key_id")
+	if !sshKeyIDExists {
 		res, err := iamAPI.ListSSHKeys(&iam.ListSSHKeysRequest{
 			Name:      expandStringPtr(d.Get("name")),
 			ProjectID: expandStringPtr(d.Get("project_id")),
@@ -48,17 +41,34 @@ func dataSourceScalewayIamSSHKeyRead(ctx context.Context, d *schema.ResourceData
 		if err != nil {
 			return diag.FromErr(err)
 		}
-		if len(res.SSHKeys) == 0 {
+		for _, sshKey := range res.SSHKeys {
+			if sshKey.Name == d.Get("name").(string) {
+				if sshKeyID != "" {
+					return diag.FromErr(fmt.Errorf("more than 1 SSH Key found with the same name %s", d.Get("name")))
+				}
+				sshKeyID = sshKey.ID
+			}
+		}
+		if sshKeyID == "" {
 			return diag.FromErr(fmt.Errorf("no SSH Key found with the name %s", d.Get("name")))
 		}
-		if len(res.SSHKeys) > 1 {
-			return diag.FromErr(fmt.Errorf("%d SSH Keys found with the same name %s", len(res.SSHKeys), d.Get("name")))
-		}
-		sshKey = res.SSHKeys[0]
 	}
 
-	d.SetId(sshKey.ID)
-	_ = d.Set("ssh_key_id", sshKey.ID)
+	d.SetId(sshKeyID.(string))
 
-	return resourceScalewayIamSSHKeyRead(ctx, d, meta)
+	err := d.Set("ssh_key_id", sshKeyID)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	diags := resourceScalewayIamSSHKeyRead(ctx, d, meta)
+	if diags != nil {
+		return append(diags, diag.Errorf("failed to read iam ssh key state")...)
+	}
+
+	if d.Id() == "" {
+		return diag.Errorf("iam ssh key (%s) not found", sshKeyID)
+	}
+
+	return nil
 }
