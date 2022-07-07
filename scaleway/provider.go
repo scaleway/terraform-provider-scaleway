2e52e900b36dfbd1c106bc0e6a82bff0473f81a3
Yacine Fodil <105779815+yfodil@users.noreply.github.com>
2022-07-07 10:45:34 +0200

    feat(iam): add iam ssh key resource (#1340)
    Co-authored-by: Rémy Léone <rleone@scaleway.com>

--- scaleway/provider.go
+++ scaleway/provider.go
@@ -32,10 +32,12 @@ func addBetaResources(provider *schema.Provider) {
 		return
 	}
 	betaResources := map[string]*schema.Resource{
-		"scaleway_iam_application": resourceScalewayIamApplication(),
 		"scaleway_iam_api_key":     resourceScalewayIamAPIKey(),
+		"scaleway_iam_application": resourceScalewayIamApplication(),
+		"scaleway_iam_ssh_key":     resourceScalewayIamSSKKey(),
 	}
 	betaDataSources := map[string]*schema.Resource{
+		"scaleway_iam_ssh_key":     dataSourceScalewayIamSSHKey(),
 		"scaleway_iam_application": dataSourceScalewayIamApplication(),
 		"scaleway_iam_user":        dataSourceScalewayIamUser(),
 	}
