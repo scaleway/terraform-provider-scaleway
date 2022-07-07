f23025e97c858ae3a0cc29b5a8b767d6f290dadf
Yacine Fodil <105779815+yfodil@users.noreply.github.com>
2022-07-07 10:45:34 +0200

    feat(iam): add iam ssh key resource (#1340)
    Co-authored-by: Rémy Léone <rleone@scaleway.com>

---
+++ scaleway/resource_iam_ssh_key_test.go
@@ -0,0 +1,264 @@
+package scaleway
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	iam "github.com/scaleway/scaleway-sdk-go/api/iam/v1alpha1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+)
+
+const SSHKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICJEoOOgQBLJPs4g/XcPTKT82NywNPpxeuA20FlOPlpO opensource@scaleway.com"
+
+func init() {
+	if !terraformBetaEnabled {
+		return
+	}
+	resource.AddTestSweepers("scaleway_iam_ssh_key", &resource.Sweeper{
+		Name: "scaleway_iam_ssh_key",
+		F:    testSweepIamSSHKey,
+	})
+}
+
+func testSweepIamSSHKey(_ string) error {
+	return sweep(func(scwClient *scw.Client) error {
+		iamAPI := iam.NewAPI(scwClient)
+
+		l.Debugf("sweeper: destroying the SSH keys")
+
+		listSSHKeys, err := iamAPI.ListSSHKeys(&iam.ListSSHKeysRequest{}, scw.WithAllPages())
+		if err != nil {
+			return fmt.Errorf("error listing SSH keys in sweeper: %s", err)
+		}
+
+		for _, sshKey := range listSSHKeys.SSHKeys {
+			err := iamAPI.DeleteSSHKey(&iam.DeleteSSHKeyRequest{
+				SSHKeyID: sshKey.ID,
+			})
+			if err != nil {
+				return fmt.Errorf("error deleting SSH key in sweeper: %s", err)
+			}
+		}
+
+		return nil
+	})
+}
+
+func TestAccScalewayIamSSHKey_basic(t *testing.T) {
+	SkipBetaTest(t)
+	name := "tf-test-iam-ssh-key-basic"
+	tt := NewTestTools(t)
+	defer tt.Cleanup()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      testAccCheckScalewayIamSSHKeyDestroy(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					resource "scaleway_iam_ssh_key" "main" {
+						name 	   = "` + name + `"
+						public_key = "` + SSHKey + `"
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayIamSSHKeyExists(tt, "scaleway_iam_ssh_key.main"),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.main", "name", name),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.main", "public_key", SSHKey),
+				),
+			},
+			{
+				Config: `
+					resource "scaleway_iam_ssh_key" "main" {
+						name 	   = "` + name + `-updated"
+						public_key = "` + SSHKey + `"
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayIamSSHKeyExists(tt, "scaleway_iam_ssh_key.main"),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.main", "name", name+"-updated"),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.main", "public_key", SSHKey),
+				),
+			},
+		},
+	})
+}
+
+func TestAccScalewayIamSSHKey_WithNewLine(t *testing.T) {
+	SkipBetaTest(t)
+	name := "tf-test-iam-ssh-key-newline"
+	tt := NewTestTools(t)
+	defer tt.Cleanup()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      testAccCheckScalewayIamSSHKeyDestroy(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					resource "scaleway_iam_ssh_key" "main" {
+						name 	   = "` + name + `"
+						public_key = "\n\n` + SSHKey + `\n\n"
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayIamSSHKeyExists(tt, "scaleway_iam_ssh_key.main"),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.main", "name", name),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.main", "public_key", SSHKey),
+				),
+			},
+		},
+	})
+}
+
+func TestAccScalewayIamSSHKey_ChangeResourceName(t *testing.T) {
+	SkipBetaTest(t)
+	name := "TestAccScalewayIamSSHKey_ChangeResourceName"
+	tt := NewTestTools(t)
+	defer tt.Cleanup()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      testAccCheckScalewayIamSSHKeyDestroy(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					resource "scaleway_iam_ssh_key" "first" {
+						name 	   = "` + name + `"
+						public_key = "\n\n` + SSHKey + `\n\n"
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayIamSSHKeyExists(tt, "scaleway_iam_ssh_key.first"),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.first", "name", name),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.first", "public_key", SSHKey),
+				),
+			},
+			{
+				Config: `
+					resource "scaleway_iam_ssh_key" "second" {
+						name 	   = "` + name + `"
+						public_key = "\n\n` + SSHKey + `\n\n"
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayIamSSHKeyExists(tt, "scaleway_iam_ssh_key.second"),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.second", "name", name),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.second", "public_key", SSHKey),
+				),
+			},
+		},
+	})
+}
+
+func TestAccScalewayIamSSHKey_Disabled(t *testing.T) {
+	SkipBetaTest(t)
+	name := "tf-test-iam-ssh-key-disabled"
+	tt := NewTestTools(t)
+	defer tt.Cleanup()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      testAccCheckScalewayIamSSHKeyDestroy(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					resource "scaleway_iam_ssh_key" "main" {
+						name 	   = "` + name + `"
+						public_key = "\n\n` + SSHKey + `\n\n"
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayIamSSHKeyExists(tt, "scaleway_iam_ssh_key.main"),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.main", "name", name),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.main", "public_key", SSHKey),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.main", "disabled", "false"),
+				),
+			},
+			{
+				Config: `
+					resource "scaleway_iam_ssh_key" "main" {
+						name 	   = "` + name + `"
+						public_key = "\n\n` + SSHKey + `\n\n"
+						disabled = "true"
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayIamSSHKeyExists(tt, "scaleway_iam_ssh_key.main"),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.main", "name", name),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.main", "public_key", SSHKey),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.main", "disabled", "true"),
+				),
+			},
+			{
+				Config: `
+					resource "scaleway_iam_ssh_key" "main" {
+						name 	   = "` + name + `"
+						public_key = "\n\n` + SSHKey + `\n\n"
+						disabled = "false"
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayIamSSHKeyExists(tt, "scaleway_iam_ssh_key.main"),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.main", "name", name),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.main", "public_key", SSHKey),
+					resource.TestCheckResourceAttr("scaleway_iam_ssh_key.main", "disabled", "false"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckScalewayIamSSHKeyDestroy(tt *TestTools) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		for _, rs := range state.RootModule().Resources {
+			if rs.Type != "scaleway_iam_ssh_key" {
+				continue
+			}
+
+			iamAPI := iamAPI(tt.Meta)
+
+			_, err := iamAPI.GetSSHKey(&iam.GetSSHKeyRequest{
+				SSHKeyID: rs.Primary.ID,
+			})
+
+			// If no error resource still exist
+			if err == nil {
+				return fmt.Errorf("SSH key (%s) still exists", rs.Primary.ID)
+			}
+
+			// Unexpected api error we return it
+			if !is404Error(err) {
+				return err
+			}
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckScalewayIamSSHKeyExists(tt *TestTools, n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", n)
+		}
+
+		iamAPI := iamAPI(tt.Meta)
+
+		_, err := iamAPI.GetSSHKey(&iam.GetSSHKeyRequest{
+			SSHKeyID: rs.Primary.ID,
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
