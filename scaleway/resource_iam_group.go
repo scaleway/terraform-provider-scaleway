b17d2ed5d68ba3ff626a5125668ec43fcfebfabf
Olivier Cano <kindermoumoute@users.noreply.github.com>
2022-08-19 11:20:36 +0200

    fix(iam_group): save ID before adding members

--- scaleway/resource_iam_group.go
+++ scaleway/resource_iam_group.go
@@ -76,6 +76,8 @@ func resourceScalewayIamGroupCreate(ctx context.Context, d *schema.ResourceData,
 		return diag.FromErr(err)
 	}
 
+	d.SetId(group.ID)
+
 	appIds := expandStrings(d.Get("application_ids").(*schema.Set).List())
 	userIds := expandStrings(d.Get("user_ids").(*schema.Set).List())
 	if len(appIds) > 0 || len(userIds) > 0 {
@@ -88,8 +90,6 @@ func resourceScalewayIamGroupCreate(ctx context.Context, d *schema.ResourceData,
 			return diag.FromErr(err)
 		}
 	}
-
-	d.SetId(group.ID)
 
 	return resourceScalewayIamGroupRead(ctx, d, meta)
 }
