75a5d260eb338d35024b7a7ba27b96850c21ba5d
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2022-08-22 11:20:31 +0200

    fix: imports

--- scaleway/resource_object_bucket_website_configuration.go
+++ scaleway/resource_object_bucket_website_configuration.go
@@ -3,16 +3,16 @@ package scaleway
 import (
 	"context"
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/scaleway/scaleway-sdk-go/scw"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/scaleway/scaleway-sdk-go/scw"
 )
 
 const (
