086f51ca0d6e80d4b95b84e72adfc9c2b8aa133b
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2022-10-03 17:30:42 +0200

    fix: test

--- scaleway/resource_iam_policy_test.go
+++ scaleway/resource_iam_policy_test.go
@@ -2,7 +2,6 @@ package scaleway
 
 import (
 	"fmt"
-	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -177,20 +176,6 @@ func TestAccScalewayIamPolicy_ChangeLinkedEntity(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		ProviderFactories: tt.ProviderFactories,
 		Steps: []resource.TestStep{
-			{
-				Config: fmt.Sprintf(`
-					resource "scaleway_iam_policy" "main" {
-						name = "tf_tests_policy_changepermissions"
-						description = "a description"
-						no_principal = false
-						rule {
-							organization_id = "%s"
-							permission_set_names = ["AllProductsFullAccess"]
-						}
-					}
-					`, orgID),
-				ExpectError: regexp.MustCompile(`(?s)` + "no_principal does not respect constraint," + `(.*?)`),
-			},
 			{
 				Config: fmt.Sprintf(`
 					resource "scaleway_iam_policy" "main" {
