74a026467b1e99f7f23e22be47c480b133197b1e
Jules Casteran <jcasteran@scaleway.com>
2022-10-06 15:20:49 +0200

    update tests

--- scaleway/data_source_account_project_test.go
+++ scaleway/data_source_account_project_test.go
@@ -28,13 +28,19 @@ func TestAccScalewayDataSourceAccountProject_Basic(t *testing.T) {
 						name = "test-terraform-account-project"
 					}
 
-					data scaleway_account_project "project" {
+					data scaleway_account_project "by_name" {
 						name = scaleway_account_project.project.name
 						organization_id = "%s"
+					}
+					
+					data scaleway_account_project "by_id" {
+						project_id = scaleway_account_project.project.id
 					}`, orgID),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrPair("data.scaleway_account_project.project", "id", "scaleway_account_project.project", "id"),
-					resource.TestCheckResourceAttrPair("data.scaleway_account_project.project", "name", "scaleway_account_project.project", "name"),
+					resource.TestCheckResourceAttrPair("data.scaleway_account_project.by_name", "id", "scaleway_account_project.project", "id"),
+					resource.TestCheckResourceAttrPair("data.scaleway_account_project.by_name", "name", "scaleway_account_project.project", "name"),
+					resource.TestCheckResourceAttrPair("data.scaleway_account_project.by_id", "id", "scaleway_account_project.project", "id"),
+					resource.TestCheckResourceAttrPair("data.scaleway_account_project.by_id", "name", "scaleway_account_project.project", "name"),
 				),
 			},
 		},
@@ -45,30 +51,36 @@ func TestAccScalewayDataSourceAccountProject_Default(t *testing.T) {
 	SkipBetaTest(t)
 	tt := NewTestTools(t)
 	defer tt.Cleanup()
+	orgID, orgIDExists := tt.Meta.scwClient.GetDefaultOrganizationID()
+	if !orgIDExists {
+		orgID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+	}
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: tt.ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: `
+				Config: fmt.Sprintf(`
 					data scaleway_account_project "project" {
 						name = "default"
-					}`,
+						organization_id = "%s"
+					}`, orgID),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.scaleway_account_project.project", "id"),
 					resource.TestCheckResourceAttrSet("data.scaleway_account_project.project", "name"),
 				),
 			},
 			{
-				Config: `
+				Config: fmt.Sprintf(`
 					data scaleway_account_project "project" {
 						name = "default"
+						organization_id = "%s"
 					}
 
 					data scaleway_account_project project2 {
 						name = "default"
 						organization_id = data.scaleway_account_project.project.id
-					}`,
+					}`, orgID),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrPair("data.scaleway_account_project.project", "id", "data.scaleway_account_project.project2", "id"),
 					resource.TestCheckResourceAttrPair("data.scaleway_account_project.project", "name", "data.scaleway_account_project.project2", "name"),
