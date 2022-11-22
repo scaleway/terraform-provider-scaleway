63cb1a2265b92b81699235961ec6e57a129fa724
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2022-11-22 12:36:23 +0100

    fix(container_namespace): remove project_id check

--- scaleway/resource_container_namespace.go
+++ scaleway/resource_container_namespace.go
@@ -2,9 +2,7 @@ package scaleway
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
@@ -94,9 +92,6 @@ func resourceScalewayContainerNamespaceCreate(ctx context.Context, d *schema.Res
 	}
 
 	namespaceName := expandOrGenerateString(d.Get("name").(string), "ns")
-	if projectID, exist := d.GetOk("project_id"); !exist || projectID == nil || projectID.(string) == "" {
-		tflog.Warn(ctx, fmt.Sprintf("The namespace %s has any projectID settled", namespaceName))
-	}
 	ns, err := api.CreateNamespace(&container.CreateNamespaceRequest{
 		Description:                expandStringPtr(d.Get("description").(string)),
 		EnvironmentVariables:       expandMapPtrStringString(d.Get("environment_variables")),
