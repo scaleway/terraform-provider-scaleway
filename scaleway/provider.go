ed30c738d268b1fa46776b8478422761421a0c6f
Yacine FODIL <yfodil@scaleway.com>
2023-02-20 11:31:32 +0100

    feat(lb): add lbs datasource

--- scaleway/provider.go
+++ scaleway/provider.go
@@ -191,6 +191,7 @@ func Provider(config *ProviderConfig) plugin.ProviderFunc {
 				"scaleway_k8s_pool":                            dataSourceScalewayK8SPool(),
 				"scaleway_k8s_version":                         dataSourceScalewayK8SVersion(),
 				"scaleway_lb":                                  dataSourceScalewayLb(),
+				"scaleway_lbs":                                 dataSourceScalewayLbs(),
 				"scaleway_lb_certificate":                      dataSourceScalewayLbCertificate(),
 				"scaleway_lb_ip":                               dataSourceScalewayLbIP(),
 				"scaleway_marketplace_image":                   dataSourceScalewayMarketplaceImage(),
