b8d1e8ccb280efc3c5afa26d05b752032080eaf1
Yacine FODIL <yfodil@scaleway.com>
2023-02-24 13:33:20 +0100

    feat(lb): add backends datasource

--- scaleway/provider.go
+++ scaleway/provider.go
@@ -193,6 +193,7 @@ func Provider(config *ProviderConfig) plugin.ProviderFunc {
 				"scaleway_k8s_version":                         dataSourceScalewayK8SVersion(),
 				"scaleway_lb":                                  dataSourceScalewayLb(),
 				"scaleway_lb_backend":                          dataSourceScalewayLbBackend(),
+				"scaleway_lb_backends":                         dataSourceScalewayLbBackends(),
 				"scaleway_lb_certificate":                      dataSourceScalewayLbCertificate(),
 				"scaleway_lb_frontend":                         dataSourceScalewayLbFrontend(),
 				"scaleway_lb_ip":                               dataSourceScalewayLbIP(),
