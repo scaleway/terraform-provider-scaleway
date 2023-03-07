cff1d9542a5cf3d839aa5153e454caa3cfa91e77
Jules Casteran <jcasteran@scaleway.com>
2023-03-07 17:45:29 +0100

    add another format for string compare

--- scaleway/provider_test_helpers.go
+++ scaleway/provider_test_helpers.go
@@ -1,18 +1,22 @@
 package scaleway
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
-// Generated name has format: "tf-{prefix}-{generated}
-func extractGeneratedNamePrefix(name string) string {
-	// {prefix}-{generated}
-	name = strings.TrimPrefix(name, "tf-")
-
+// Test Generated name has format: "{prefix}-{generated_number}
+// example: test-acc-scaleway-project-3723338038624371236
+func extractTestGeneratedNamePrefix(name string) string {
 	// {prefix}-{generated}
 	//         ^
-	dashIndex := strings.Index(name, "-")
+	dashIndex := strings.LastIndex(name, "-")
 
-	if dashIndex == -1 {
-		// some are only tf-{name}
+	generated := name[dashIndex+1:]
+	_, generatedToIntErr := strconv.ParseInt(generated, 10, 64)
+
+	if dashIndex == -1 || generatedToIntErr != nil {
+		// some are only {name}
 		return name
 	}
 
@@ -20,14 +24,43 @@ func extractGeneratedNamePrefix(name string) string {
 	return name[:dashIndex]
 }
 
+// Generated names have format: "tf-{prefix}-{generated1}-{generated2}"
+// example: tf-sg-gifted-yonath
+func extractGeneratedNamePrefix(name string) string {
+	if strings.Count(name, "-") < 3 {
+		return name
+	}
+	// tf-{prefix}-gifted-yonath
+	name = strings.TrimPrefix(name, "tf-")
+
+	// {prefix}-gifted-yonath
+	//                ^
+	dashIndex := strings.LastIndex(name, "-")
+	name = name[:dashIndex]
+	// {prefix}-gifted
+	//         ^
+	dashIndex = strings.LastIndex(name, "-")
+	name = name[:dashIndex]
+	return name
+}
+
 // compareJSONFieldsStrings compare two strings from request JSON bodies
 // has special case when string are terraform generated names
 func compareJSONFieldsStrings(expected, actual string) bool {
-	if strings.HasPrefix(expected, "tf-") {
-		expectedPrefix := extractGeneratedNamePrefix(expected)
-		actualPrefix := extractGeneratedNamePrefix(actual)
-		return expectedPrefix == actualPrefix
+	expectedHandled := expected
+	actualHandled := actual
+
+	// Try to parse test generated name
+	if strings.Contains(expected, "-") {
+		expectedHandled = extractTestGeneratedNamePrefix(expected)
+		actualHandled = extractTestGeneratedNamePrefix(actual)
 	}
 
-	return expected == actual
+	// Try provider generated name
+	if actualHandled == actual && strings.HasPrefix(actual, "tf-") {
+		expectedHandled = extractGeneratedNamePrefix(expected)
+		actualHandled = extractGeneratedNamePrefix(actual)
+	}
+
+	return expectedHandled == actualHandled
 }
