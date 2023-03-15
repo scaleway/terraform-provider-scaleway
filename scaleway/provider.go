ad086ce9e526e79f341bdd41510dfd38971346b8
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2023-03-15 19:04:45 +0100

    feat(secret): data source secret (#1837)
    * feat(secret): data source secret
    
    * feat(secret): datasource secret
    
    * fix(secret): add organization id on schema and documentation

--- scaleway/provider.go
+++ scaleway/provider.go
@@ -216,6 +216,7 @@ func Provider(config *ProviderConfig) plugin.ProviderFunc {
 				"scaleway_redis_cluster":                       dataSourceScalewayRedisCluster(),
 				"scaleway_registry_namespace":                  dataSourceScalewayRegistryNamespace(),
 				"scaleway_tem_domain":                          dataSourceScalewayTemDomain(),
+				"scaleway_secret":                              dataSourceScalewaySecret(),
 				"scaleway_registry_image":                      dataSourceScalewayRegistryImage(),
 				"scaleway_vpc_public_gateway":                  dataSourceScalewayVPCPublicGateway(),
 				"scaleway_vpc_gateway_network":                 dataSourceScalewayVPCGatewayNetwork(),
