967909f41bee3e0ee2066dbadebfee93c5470b1c
Nathanael DEMACON <ndemacon@scaleway.com>
2023-04-07 17:20:12 +0200

    feat(account): extract project id if not given

--- scaleway/data_source_account_project.go
+++ scaleway/data_source_account_project.go
@@ -16,6 +16,7 @@ func dataSourceScalewayAccountProject() *schema.Resource {
 	dsSchema["name"].ConflictsWith = []string{"project_id"}
 	dsSchema["project_id"] = &schema.Schema{
 		Type:         schema.TypeString,
+		Computed:     true,
 		Optional:     true,
 		Description:  "The ID of the SSH key",
 		ValidateFunc: validationUUID(),
@@ -30,8 +31,9 @@ func dataSourceScalewayAccountProject() *schema.Resource {
 func dataSourceScalewayAccountProjectRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	accountAPI := accountV2API(meta)
 
-	projectID, projectIDExists := d.GetOk("project_id")
-	if !projectIDExists {
+	var projectID string
+
+	if name, nameExists := d.GetOk("name"); nameExists {
 		orgID := getOrganizationID(meta, d)
 		if orgID == nil {
 			// required not in schema as we could use default
@@ -39,13 +41,13 @@ func dataSourceScalewayAccountProjectRead(ctx context.Context, d *schema.Resourc
 		}
 		res, err := accountAPI.ListProjects(&accountV2.ListProjectsRequest{
 			OrganizationID: *orgID,
-			Name:           expandStringPtr(d.Get("name")),
+			Name:           expandStringPtr(name),
 		}, scw.WithContext(ctx))
 		if err != nil {
 			return diag.FromErr(err)
 		}
 		for _, project := range res.Projects {
-			if project.Name == d.Get("name").(string) {
+			if project.Name == name {
 				if projectID != "" {
 					return diag.Errorf("more than 1 project found with the same name %s", d.Get("name"))
 				}
@@ -55,9 +57,16 @@ func dataSourceScalewayAccountProjectRead(ctx context.Context, d *schema.Resourc
 		if projectID == "" {
 			diag.Errorf("no project found with the name %s", d.Get("name"))
 		}
+	} else {
+		extractedProjectID, _, err := extractProjectID(d, meta.(*Meta))
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		projectID = extractedProjectID
 	}
 
-	d.SetId(projectID.(string))
+	d.SetId(projectID)
 	_ = d.Set("project_id", projectID)
 
 	diags := resourceScalewayAccountProjectRead(ctx, d, meta)
