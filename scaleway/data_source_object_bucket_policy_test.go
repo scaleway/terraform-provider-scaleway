4ed0f9d9eb2d5e7508a2487b00636ab00d41c7e9
Nathanael DEMACON <ndemacon@scaleway.com>
2023-05-09 17:05:45 +0200

    feat(object): add bucket policy datasource

---
+++ scaleway/data_source_object_bucket_policy_test.go
@@ -0,0 +1,67 @@
+package scaleway
+
+import (
+	"fmt"
+	"testing"
+
+	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccScalewayDataSourceObjectBucketPolicy_Basic(t *testing.T) {
+	tt := NewTestTools(t)
+	defer tt.Cleanup()
+
+	bucketName := sdkacctest.RandomWithPrefix("test-acc-scw-obp-data-basic")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      testAccCheckScalewayRdbInstanceDestroy(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					resource "scaleway_object_bucket" "main" {
+						name = "%[1]s"
+					}
+
+					resource "scaleway_object_bucket_policy" "main" {
+						bucket = scaleway_object_bucket.main.name
+						policy = jsonencode(
+							{
+								Id = "MyPolicy"
+								Statement = [
+									{
+										Action = [
+											"s3:ListBucket",
+											"s3:GetObject",
+										]
+										Effect = "Allow"
+										Principal = {
+											SCW = "*"
+										}
+										Resource  = [
+											"${scaleway_object_bucket.main.name}",
+											"${scaleway_object_bucket.main.name}/*",
+										]
+										Sid = "GrantToEveryone"
+									},
+								]
+								Version = "2012-10-17"
+							}
+						)
+					}
+
+					data "scaleway_object_bucket_policy" "selected" {
+						bucket = scaleway_object_bucket_policy.main.bucket
+					}
+				`, bucketName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.scaleway_object_bucket_policy.selected", "bucket", bucketName),
+					resource.TestCheckResourceAttrSet("data.scaleway_object_bucket_policy.selected", "policy"),
+					resource.TestCheckResourceAttrPair("data.scaleway_object_bucket_policy.selected", "policy", "scaleway_object_bucket_policy.main", "policy"),
+				),
+			},
+		},
+	})
+}
