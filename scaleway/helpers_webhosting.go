59566a7a3a6cb12b6a9b775394de4d25f666db51
Yacine FODIL <yfodil@scaleway.com>
2023-05-11 16:21:29 +0200

    add support for webhosting, first try

--- scaleway/helpers_webhosting.go
+++ scaleway/helpers_webhosting.go
@@ -1,9 +1,17 @@
 package scaleway
 
 import (
+	"context"
+	"time"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	webhosting "github.com/scaleway/scaleway-sdk-go/api/webhosting/v1alpha1"
 	"github.com/scaleway/scaleway-sdk-go/scw"
+)
+
+const (
+	defaultWebhostingTimeout = 5 * time.Minute
+	hostingRetryInterval     = 5 * time.Second
 )
 
 // webhostingAPIWithRegion returns a new Webhosting API and the region for a Create request
@@ -16,6 +24,18 @@ func webhostingAPIWithRegion(d *schema.ResourceData, m interface{}) (*webhosting
 		return nil, "", err
 	}
 	return api, region, nil
+}
+
+// webhostingAPIWithRegionAndID returns a Webhosting API with region and ID extracted from the state
+func webhostingAPIWithRegionAndID(m interface{}, id string) (*webhosting.API, scw.Region, string, error) {
+	meta := m.(*Meta)
+	api := webhosting.NewAPI(meta.scwClient)
+
+	region, id, err := parseRegionalID(id)
+	if err != nil {
+		return nil, "", "", err
+	}
+	return api, region, id, nil
 }
 
 func flattenOfferProduct(product *webhosting.OfferProduct) interface{} {
@@ -36,4 +56,41 @@ func flattenOfferProduct(product *webhosting.OfferProduct) interface{} {
 
 func flattenOfferPrice(price *scw.Money) interface{} {
 	return price.String()
+}
+
+func flattenHostingCpanelUrls(cpanelURL *webhosting.HostingCpanelURLs) []map[string]interface{} {
+	return []map[string]interface{}{
+		{
+			"dashboard": cpanelURL.Dashboard,
+			"webmail":   cpanelURL.Webmail,
+		},
+	}
+}
+
+func flattenHostingOptions(options []*webhosting.HostingOption) []map[string]interface{} {
+	if options == nil {
+		return nil
+	}
+	flattenedOptions := []map[string]interface{}(nil)
+	for _, option := range options {
+		flattenedOptions = append(flattenedOptions, map[string]interface{}{
+			"id":   option.ID,
+			"name": option.Name,
+		})
+	}
+	return flattenedOptions
+}
+
+func waitForHosting(ctx context.Context, api *webhosting.API, region scw.Region, hostingID string, timeout time.Duration) (*webhosting.Hosting, error) {
+	retryInterval := hostingRetryInterval
+	if DefaultWaitRetryInterval != nil {
+		retryInterval = *DefaultWaitRetryInterval
+	}
+
+	return api.WaitForHosting(&webhosting.WaitForHostingRequest{
+		HostingID:     hostingID,
+		Region:        region,
+		Timeout:       scw.TimeDurationPtr(timeout),
+		RetryInterval: &retryInterval,
+	}, scw.WithContext(ctx))
 }
---
+++ scaleway/resource_webhosting_test.go
@@ -0,0 +1,141 @@
+package scaleway
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	webhosting "github.com/scaleway/scaleway-sdk-go/api/webhosting/v1alpha1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+)
+
+func init() {
+	resource.AddTestSweepers("scaleway_webhosting", &resource.Sweeper{
+		Name: "scaleway_webhosting",
+		F:    testSweepWebhosting,
+	})
+}
+
+func testSweepWebhosting(_ string) error {
+	return sweepRegions(scw.AllRegions, func(scwClient *scw.Client, region scw.Region) error {
+		webhsotingAPI := webhosting.NewAPI(scwClient)
+
+		l.Debugf("sweeper: deleting the hostings in (%s)", region)
+
+		listHostings, err := webhsotingAPI.ListHostings(&webhosting.ListHostingsRequest{Region: region}, scw.WithAllPages())
+		if err != nil {
+			return fmt.Errorf("error listing hostings in (%s) in sweeper: %s", region, err)
+		}
+
+		for _, hosting := range listHostings.Hostings {
+			_, err := webhsotingAPI.DeleteHosting(&webhosting.DeleteHostingRequest{
+				HostingID: hosting.ID,
+				Region:    region,
+			})
+			if err != nil {
+				l.Debugf("sweeper: error (%s)", err)
+
+				return fmt.Errorf("error deleting hosting in sweeper: %s", err)
+			}
+		}
+
+		return nil
+	})
+}
+
+func TestAccScalewayWebhosting_Basic(t *testing.T) {
+	tt := NewTestTools(t)
+	defer tt.Cleanup()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      testAccCheckScalewayWebhostingDestroy(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+				data "scaleway_webhosting_offer" "by_name" {
+				  name = "lite"
+				}
+
+				resource "scaleway_webhosting" "main" {
+				  offer_id     = data.scaleway_webhosting_offer.by_name.offer_id
+				  email        = "hashicorp@scaleway.com"
+				  domain       = "scaleway.com"
+				  tags         = ["devtools", "provider", "terraform"]
+				}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayWebhostingExists(tt, "scaleway_webhosting.main"),
+					resource.TestCheckResourceAttrPair("scaleway_webhosting.main", "offer_id", "data.scaleway_webhosting_offer.by_name", "offer_id"),
+					resource.TestCheckResourceAttr("scaleway_webhosting.main", "email", "hashicorp@scaleway.com"),
+					resource.TestCheckResourceAttr("scaleway_webhosting.main", "domain", "scaleway.com"),
+					resource.TestCheckResourceAttr("scaleway_webhosting.main", "status", webhosting.HostingStatusReady.String()),
+					resource.TestCheckResourceAttr("scaleway_webhosting.main", "tags.0", "devtools"),
+					resource.TestCheckResourceAttr("scaleway_webhosting.main", "tags.1", "provider"),
+					resource.TestCheckResourceAttr("scaleway_webhosting.main", "tags.2", "terraform"),
+					resource.TestCheckResourceAttrSet("scaleway_webhosting.main", "updated_at"),
+					resource.TestCheckResourceAttrSet("scaleway_webhosting.main", "created_at"),
+					testCheckResourceAttrUUID("scaleway_webhosting.main", "id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckScalewayWebhostingExists(tt *TestTools, n string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		rs, ok := state.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", n)
+		}
+
+		api, region, id, err := webhostingAPIWithRegionAndID(tt.Meta, rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		_, err = api.GetHosting(&webhosting.GetHostingRequest{
+			HostingID: id,
+			Region:    region,
+		})
+
+		// If no error resource still exist
+		if err == nil {
+			return fmt.Errorf("server (%s) still exists", rs.Primary.ID)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckScalewayWebhostingDestroy(tt *TestTools) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		for _, rs := range state.RootModule().Resources {
+			if rs.Type != "scaleway_webhosting" {
+				continue
+			}
+
+			api, region, id, err := webhostingAPIWithRegionAndID(tt.Meta, rs.Primary.ID)
+			if err != nil {
+				return err
+			}
+
+			_, err = api.DeleteHosting(&webhosting.DeleteHostingRequest{
+				HostingID: id,
+				Region:    region,
+			})
+
+			if err == nil {
+				return fmt.Errorf("hosting (%s) still exists", rs.Primary.ID)
+			}
+
+			if !is404Error(err) {
+				return err
+			}
+		}
+
+		return nil
+	}
+}
