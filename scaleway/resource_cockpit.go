f0fb67c04490280fa63e7e764555aac84a83e885
Nathanael DEMACON <ndemacon@scaleway.com>
2023-05-16 18:59:11 +0200

    feat(cockpit): add plan selection

--- scaleway/resource_cockpit.go
+++ scaleway/resource_cockpit.go
@@ -6,12 +6,14 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	cockpit "github.com/scaleway/scaleway-sdk-go/api/cockpit/v1beta1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
 )
 
 func resourceScalewayCockpit() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: resourceScalewayCockpitCreate,
 		ReadContext:   resourceScalewayCockpitRead,
+		UpdateContext: resourceScalewayCockpitUpdate,
 		DeleteContext: resourceScalewayCockpitDelete,
 		Timeouts: &schema.ResourceTimeout{
 			Create:  schema.DefaultTimeout(defaultCockpitTimeout),
@@ -24,6 +26,16 @@ func resourceScalewayCockpit() *schema.Resource {
 		},
 		Schema: map[string]*schema.Schema{
 			"project_id": projectIDSchema(),
+			"plan": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Name or ID of the plan",
+			},
+			"plan_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The plan ID of the cockpit",
+			},
 			"endpoints": {
 				Type:        schema.TypeList,
 				Computed:    true,
@@ -67,9 +79,43 @@ func resourceScalewayCockpitCreate(ctx context.Context, d *schema.ResourceData, 
 
 	res, err := api.ActivateCockpit(&cockpit.ActivateCockpitRequest{
 		ProjectID: projectID,
-	})
+	}, scw.WithContext(ctx))
 	if err != nil {
 		return diag.FromErr(err)
+	}
+
+	_, err = waitForCockpit(ctx, api, d.Id(), d.Timeout(schema.TimeoutRead))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if targetPlanI, ok := d.GetOk("plan"); ok {
+		targetPlan := targetPlanI.(string)
+
+		plans, err := api.ListPlans(&cockpit.ListPlansRequest{}, scw.WithContext(ctx), scw.WithAllPages())
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		var planID string
+		for _, plan := range plans.Plans {
+			if plan.Name.String() == targetPlan || plan.ID == targetPlan {
+				planID = plan.ID
+				break
+			}
+		}
+
+		if planID == "" {
+			return diag.Errorf("plan %s not found", targetPlan)
+		}
+
+		_, err = api.SelectPlan(&cockpit.SelectPlanRequest{
+			ProjectID: projectID,
+			PlanID:    planID,
+		}, scw.WithContext(ctx))
+		if err != nil {
+			return diag.FromErr(err)
+		}
 	}
 
 	d.SetId(res.ProjectID)
@@ -84,17 +130,61 @@ func resourceScalewayCockpitRead(ctx context.Context, d *schema.ResourceData, me
 
 	res, err := waitForCockpit(ctx, api, d.Id(), d.Timeout(schema.TimeoutRead))
 	if err != nil {
-		if is404Error(err) {
-			d.SetId("")
-			return nil
-		}
 		return diag.FromErr(err)
 	}
 
 	_ = d.Set("project_id", res.ProjectID)
+	_ = d.Set("plan_id", res.Plan.ID)
 	_ = d.Set("endpoints", flattenCockpitEndpoints(res.Endpoints))
 
 	return nil
+}
+
+func resourceScalewayCockpitUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	api, err := cockpitAPI(meta)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	projectID := d.Id()
+	_, err = waitForCockpit(ctx, api, projectID, d.Timeout(schema.TimeoutDelete))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if d.HasChange("plan") {
+		targetPlan := cockpit.PlanNameFree.String()
+		if targetPlanI, ok := d.GetOk("plan"); ok {
+			targetPlan = targetPlanI.(string)
+		}
+
+		plans, err := api.ListPlans(&cockpit.ListPlansRequest{}, scw.WithContext(ctx), scw.WithAllPages())
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		var planID string
+		for _, plan := range plans.Plans {
+			if plan.Name.String() == targetPlan || plan.ID == targetPlan {
+				planID = plan.ID
+				break
+			}
+		}
+
+		if planID == "" {
+			return diag.Errorf("plan %s not found", targetPlan)
+		}
+
+		_, err = api.SelectPlan(&cockpit.SelectPlanRequest{
+			ProjectID: projectID,
+			PlanID:    planID,
+		}, scw.WithContext(ctx))
+		if err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	return resourceScalewayCockpitRead(ctx, d, meta)
 }
 
 func resourceScalewayCockpitDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
@@ -110,7 +200,7 @@ func resourceScalewayCockpitDelete(ctx context.Context, d *schema.ResourceData, 
 
 	_, err = api.DeactivateCockpit(&cockpit.DeactivateCockpitRequest{
 		ProjectID: d.Id(),
-	})
+	}, scw.WithContext(ctx))
 	if err != nil {
 		return diag.FromErr(err)
 	}
