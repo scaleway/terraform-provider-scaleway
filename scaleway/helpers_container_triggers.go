592b0fa1b98ba823aec1bd261353dfd99a74ea15
Jules Casteran <jcasteran@scaleway.com>
2023-07-06 11:45:04 +0200

    feat(container): add trigger resource

---
+++ scaleway/helpers_container_triggers.go
@@ -0,0 +1,56 @@
+package scaleway
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	container "github.com/scaleway/scaleway-sdk-go/api/container/v1beta1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+)
+
+func waitForContainerTrigger(ctx context.Context, containerAPI *container.API, region scw.Region, id string, timeout time.Duration) (*container.Trigger, error) {
+	retryInterval := defaultFunctionRetryInterval
+	if DefaultWaitRetryInterval != nil {
+		retryInterval = *DefaultWaitRetryInterval
+	}
+
+	trigger, err := containerAPI.WaitForTrigger(&container.WaitForTriggerRequest{
+		Region:        region,
+		TriggerID:     id,
+		RetryInterval: &retryInterval,
+		Timeout:       scw.TimeDurationPtr(timeout),
+	}, scw.WithContext(ctx))
+
+	return trigger, err
+}
+
+func expandContainerTriggerMnqSqsCreationConfig(i interface{}) *container.CreateTriggerRequestMnqSqsClientConfig {
+	m := i.(map[string]interface{})
+
+	return &container.CreateTriggerRequestMnqSqsClientConfig{
+		MnqNamespaceID: expandID(m["namespace_id"].(string)),
+		Queue:          m["queue"].(string),
+		MnqProjectID:   m["project_id"].(string),
+		MnqRegion:      m["region"].(string),
+	}
+}
+
+func completeContainerTriggerMnqSqsCreationConfig(i interface{}, d *schema.ResourceData, meta interface{}, region scw.Region) error {
+	m := i.(map[string]interface{})
+
+	if sqsRegion, exists := m["region"]; !exists || sqsRegion == "" {
+		m["region"] = region.String()
+	}
+
+	if projectID, exists := m["project_id"]; !exists || projectID == "" {
+		projectID, _, err := extractProjectID(d, meta.(*Meta))
+		if err != nil {
+			return fmt.Errorf("failed to find a valid project_id")
+		}
+		m["project_id"] = projectID
+	}
+
+	return nil
+}
