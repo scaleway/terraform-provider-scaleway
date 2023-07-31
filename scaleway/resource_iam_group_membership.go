db0ed23f76c2fe4a5524daf569456aecfb37eda0
Jules Castéran <jcasteran@scaleway.com>
2023-07-31 14:39:41 +0200

    feat(iam): add iam_group_membership resource (#2067)
    * feat(iam): add iam_group_membership resource
    
    * fix(iam): group external_membership
    
    * fix(iam): set iam_group_memberships ids to exactly one of
    
    * tests(iam): import group membership
    
    * docs(iam): update iam_group and add iam_group_membership
    
    * tests(iam): move iam group membership import test into basic
    
    * fix errors on 404

---
+++ scaleway/resource_iam_group_membership.go
@@ -0,0 +1,172 @@
+package scaleway
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	iam "github.com/scaleway/scaleway-sdk-go/api/iam/v1alpha1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+)
+
+func resourceScalewayIamGroupMembership() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceScalewayIamGroupMembershipCreate,
+		ReadContext:   resourceScalewayIamGroupMembershipRead,
+		DeleteContext: resourceScalewayIamGroupMembershipDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		SchemaVersion: 0,
+		Schema: map[string]*schema.Schema{
+			"user_id": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Description:  "The ID of the user",
+				ExactlyOneOf: []string{"application_id"},
+				ForceNew:     true,
+			},
+			"application_id": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Description:  "The ID of the user",
+				ExactlyOneOf: []string{"user_id"},
+				ForceNew:     true,
+			},
+			"group_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The ID of the group to add the user to",
+				ForceNew:    true,
+			},
+		},
+	}
+}
+
+func resourceScalewayIamGroupMembershipCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	api := iamAPI(meta)
+
+	userID := expandStringPtr(d.Get("user_id"))
+	applicationID := expandStringPtr(d.Get("application_id"))
+
+	group, err := api.AddGroupMember(&iam.AddGroupMemberRequest{
+		GroupID:       d.Get("group_id").(string),
+		UserID:        userID,
+		ApplicationID: applicationID,
+	}, scw.WithContext(ctx))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(groupMembershipID(group.ID, userID, applicationID))
+
+	return resourceScalewayIamGroupMembershipRead(ctx, d, meta)
+}
+
+func resourceScalewayIamGroupMembershipRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	api := iamAPI(meta)
+	groupID, userID, applicationID, err := expandGroupMembershipID(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	group, err := api.GetGroup(&iam.GetGroupRequest{
+		GroupID: groupID,
+	}, scw.WithContext(ctx))
+	if err != nil {
+		if is404Error(err) {
+			d.SetId("")
+
+			return nil
+		}
+
+		return diag.FromErr(err)
+	}
+
+	foundInGroup := false
+	if userID != "" {
+		for _, groupUserID := range group.UserIDs {
+			if groupUserID == userID {
+				foundInGroup = true
+				break
+			}
+		}
+	} else if applicationID != "" {
+		for _, groupApplicationID := range group.ApplicationIDs {
+			if groupApplicationID == applicationID {
+				foundInGroup = true
+				break
+			}
+		}
+	}
+
+	if !foundInGroup {
+		d.SetId("")
+
+		return nil
+	}
+
+	_ = d.Set("group_id", groupID)
+	_ = d.Set("user_id", userID)
+	_ = d.Set("application_id", applicationID)
+
+	return nil
+}
+
+func resourceScalewayIamGroupMembershipDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	api := iamAPI(meta)
+	groupID, userID, applicationID, err := expandGroupMembershipID(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	req := &iam.RemoveGroupMemberRequest{
+		GroupID: groupID,
+	}
+
+	if userID != "" {
+		req.UserID = &userID
+	} else if applicationID != "" {
+		req.ApplicationID = &applicationID
+	}
+
+	_, err = api.RemoveGroupMember(req, scw.WithContext(ctx))
+	if err != nil {
+		if is404Error(err) {
+			d.SetId("")
+
+			return nil
+		}
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func groupMembershipID(groupID string, userID *string, applicationID *string) string {
+	if userID != nil {
+		return fmt.Sprintf("%s/user/%s", groupID, *userID)
+	}
+
+	return fmt.Sprintf("%s/app/%s", groupID, *applicationID)
+}
+
+func expandGroupMembershipID(id string) (groupID string, userID string, applicationID string, err error) {
+	elems := strings.Split(id, "/")
+	if len(elems) != 3 {
+		return "", "", "", fmt.Errorf("invalid group member id format, expected {groupID}/{type}/{memberID}, got: %s", id)
+	}
+
+	groupID = elems[0]
+
+	switch elems[1] {
+	case "user":
+		userID = elems[2]
+	case "app":
+		applicationID = elems[2]
+	}
+
+	return
+}
---
+++ scaleway/resource_iam_group_membership_test.go
@@ -0,0 +1,230 @@
+package scaleway
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	iam "github.com/scaleway/scaleway-sdk-go/api/iam/v1alpha1"
+)
+
+func TestAccScalewayIamGroupMembership_Basic(t *testing.T) {
+	tt := NewTestTools(t)
+	defer tt.Cleanup()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy: resource.ComposeTestCheckFunc(
+			testAccCheckScalewayIamGroupDestroy(tt),
+			testAccCheckScalewayIamApplicationDestroy(tt),
+		), Steps: []resource.TestStep{
+			{
+				Config: `
+					resource scaleway_iam_group main {
+						name = "tf-tests-iam-group-membership-basic"
+						external_membership = true
+					}
+
+					resource scaleway_iam_application main {
+						name = "tf-tests-iam-group-membership-basic"
+					}
+
+					resource scaleway_iam_group_membership main {
+						group_id = scaleway_iam_group.main.id
+						application_id = scaleway_iam_application.main.id
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayIamGroupMembershipApplicationInGroup(tt, "scaleway_iam_group_membership.main", "scaleway_iam_application.main"),
+					testCheckResourceAttrUUID("scaleway_iam_group_membership.main", "id"),
+				),
+			},
+			{
+				Config: `
+					resource scaleway_iam_group main {
+						name = "tf-tests-iam-group-membership-basic"
+						external_membership = true
+					}
+
+					resource scaleway_iam_application main {
+						name = "tf-tests-iam-group-membership-basic"
+					}
+
+					resource scaleway_iam_group_membership main {
+						group_id = scaleway_iam_group.main.id
+						application_id = scaleway_iam_application.main.id
+					}
+
+					resource scaleway_iam_group_membership import {
+						group_id = scaleway_iam_group.main.id
+						application_id = scaleway_iam_application.main.id
+					}
+				`,
+				ImportState:  true,
+				ResourceName: "scaleway_iam_group_membership.import",
+				ImportStateIdFunc: func(state *terraform.State) (string, error) {
+					groupID := state.RootModule().Resources["scaleway_iam_group.main"].Primary.ID
+					applicationID := state.RootModule().Resources["scaleway_iam_application.main"].Primary.ID
+
+					return groupMembershipID(groupID, nil, &applicationID), nil
+				},
+				ImportStatePersist: true,
+			},
+			{
+				Config: `
+					resource scaleway_iam_group main {
+						name = "tf-tests-iam-group-membership-basic"
+						external_membership = true
+					}
+
+					resource scaleway_iam_application main {
+						name = "tf-tests-iam-group-membership-basic"
+					}
+
+					resource scaleway_iam_group_membership main {
+						group_id = scaleway_iam_group.main.id
+						application_id = scaleway_iam_application.main.id
+					}
+
+					resource scaleway_iam_group_membership import {
+						group_id = scaleway_iam_group.main.id
+						application_id = scaleway_iam_application.main.id
+					}
+				`,
+				PlanOnly: true,
+			},
+		},
+	})
+}
+
+func TestAccScalewayIamGroupMembership_User(t *testing.T) {
+	tt := NewTestTools(t)
+	defer tt.Cleanup()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy: resource.ComposeTestCheckFunc(
+			testAccCheckScalewayIamGroupDestroy(tt),
+			testAccCheckScalewayIamApplicationDestroy(tt),
+		), Steps: []resource.TestStep{
+			{
+				Config: `
+					resource scaleway_iam_group main {
+						name = "tf-tests-iam-group-membership-user"
+						external_membership = true
+					}
+
+					data "scaleway_iam_user" "main" {
+						user_id = "b6360d4f-831c-45a8-889e-0b65ed079e63"
+					}
+
+					resource scaleway_iam_group_membership main {
+						group_id = scaleway_iam_group.main.id
+						user_id = data.scaleway_iam_user.main.id
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayIamGroupMembershipUserInGroup(tt, "scaleway_iam_group_membership.main", "data.scaleway_iam_user.main"),
+					testCheckResourceAttrUUID("scaleway_iam_group_membership.main", "id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckScalewayIamGroupMembershipApplicationInGroup(tt *TestTools, n string, appN string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		rs, ok := state.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", n)
+		}
+
+		appRS, ok := state.RootModule().Resources[appN]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", n)
+		}
+
+		expectedApplicationID := appRS.Primary.ID
+
+		api := iamAPI(tt.Meta)
+		groupID, _, applicationID, err := expandGroupMembershipID(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		if applicationID != expectedApplicationID {
+			return fmt.Errorf("group membership id does not contain expected application id, expected %s, got %s", expectedApplicationID, applicationID)
+		}
+
+		group, err := api.GetGroup(&iam.GetGroupRequest{
+			GroupID: groupID,
+		})
+		if err != nil {
+			return err
+		}
+
+		foundInGroup := false
+
+		for _, groupApplicationID := range group.ApplicationIDs {
+			if groupApplicationID == applicationID {
+				foundInGroup = true
+			}
+		}
+
+		if !foundInGroup {
+			return fmt.Errorf("application not found in group")
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckScalewayIamGroupMembershipUserInGroup(tt *TestTools, n string, appN string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		rs, ok := state.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", n)
+		}
+
+		appRS, ok := state.RootModule().Resources[appN]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", n)
+		}
+
+		expectedUserID := appRS.Primary.ID
+
+		api := iamAPI(tt.Meta)
+		groupID, userID, _, err := expandGroupMembershipID(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		if userID != expectedUserID {
+			return fmt.Errorf("group membership id does not contain expected user id, expected %s, got %s", expectedUserID, userID)
+		}
+
+		group, err := api.GetGroup(&iam.GetGroupRequest{
+			GroupID: groupID,
+		})
+		if err != nil {
+			return err
+		}
+
+		foundInGroup := false
+
+		for _, groupUserID := range group.UserIDs {
+			if groupUserID == userID {
+				foundInGroup = true
+			}
+		}
+
+		if !foundInGroup {
+			return fmt.Errorf("user not found in group")
+		}
+
+		return nil
+	}
+}
