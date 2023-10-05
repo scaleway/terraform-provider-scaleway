2f91ca546c65ffa338a9727794be662cc620ae18
Jules Castéran <jcasteran@scaleway.com>
2023-10-05 15:48:00 +0200

    feat(document_db): add resources (#2109)
    * feat(document_db): add instance resource
    
    * lint
    
    * feat(document_db): add database resource
    
    * feat(documentdb): resources documentdb
    
    * feat(documentdb): replica resource
    
    * chore: add DocumentDB to CI test
    
    * chore: test cassettes
    
    * chore: test cassettes
    
    * fix(documentdb): fix test
    
    * docs(documentdb): add documentation resources
    
    * feat(documentdb): add resources private network and datasource load balander
    
    * fix(documentdb): remve unused helpers
    
    * fix(documentdb): test resource naming
    
    * feat(documentdb): add privilege basic cassette
    
    * feat(documentdb): add missing cassettes
    
    * docs: update documentdb documentation
    
    * fix(documentdb): rename telemetry variable
    
    * docs: update documentation link
    
    * fix(documentdb): renambe file test
    
    * chore: add cassettes files
    
    * fix(documentdb): update test
    
    * fix(documentdb): update test
    
    * fix(documentdb): update test
    
    * fix(documentdb): linter issue on set const
    
    * fix(documentdb): remove telemetry option
    
    * fix(documentdb): revert telemetry setter
    
    * fix(documentdb): remove telemetry enabled
    
    * fix(documentdb): remove optional parameter on test
    
    * fix(documentdb): remove is_ha_cluter on test
    
    * fix(documentdb): remove telemetry_enabled on test
    
    ---------
    
    Co-authored-by: jaime Bernabe <6184069+Monitob@users.noreply.github.com>

---
+++ scaleway/data_source_document_db_database_test.go
@@ -0,0 +1,48 @@
+package scaleway
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccScalewayDataSourceDocumentDBDatabase_Basic(t *testing.T) {
+	tt := NewTestTools(t)
+	defer tt.Cleanup()
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy: resource.ComposeTestCheckFunc(
+			testAccCheckScalewayDocumentDBInstanceDestroy(tt),
+		),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					resource scaleway_document_db_instance main {
+						name = "test-ds-document_db-database-basic"
+						node_type = "docdb-play2-pico"
+						engine = "FerretDB-1"
+						user_name = "my_initial_user"
+						password = "thiZ_is_v&ry_s3cret"
+						volume_size_in_gb = 20
+					}
+
+					resource scaleway_document_db_database main {
+						instance_id = scaleway_document_db_instance.main.id
+						name        = "test-ds-document_db-database-basic"
+					}
+
+					data scaleway_document_db_database main {
+						instance_id = scaleway_document_db_instance.main.id
+						name        = scaleway_document_db_database.main.name
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayDocumentDBDatabaseExists(tt, "scaleway_document_db_database.main"),
+
+					resource.TestCheckResourceAttrPair("scaleway_document_db_database.main", "id", "data.scaleway_document_db_database.main", "id"),
+				),
+			},
+		},
+	})
+}
---
+++ scaleway/data_source_document_db_load_balancer_endpoint.go
@@ -0,0 +1,113 @@
+package scaleway
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	documentdb "github.com/scaleway/scaleway-sdk-go/api/documentdb/v1beta1"
+)
+
+func dataSourceScalewayDocumentDBEndpointLoadBalancer() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceScalewayDocumentDBLoadBalancerRead,
+		Schema: map[string]*schema.Schema{
+			"instance_id": {
+				Type:             schema.TypeString,
+				Optional:         true,
+				Computed:         true,
+				Description:      "Instance on which the endpoint is attached",
+				ConflictsWith:    []string{"instance_name"},
+				ValidateFunc:     validationUUIDorUUIDWithLocality(),
+				DiffSuppressFunc: diffSuppressFuncLocality,
+			},
+			"instance_name": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Computed:      true,
+				Description:   "Instance Name on which the endpoint is attached",
+				ConflictsWith: []string{"instance_id"},
+			},
+			"ip": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The IP of your load balancer service",
+			},
+			"port": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "The port of your load balancer service",
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The name of your load balancer service",
+			},
+			"hostname": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The hostname of your endpoint",
+			},
+			"region":     regionSchema(),
+			"project_id": projectIDSchema(),
+		},
+	}
+}
+
+func dataSourceScalewayDocumentDBLoadBalancerRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	api, region, err := documentDBAPIWithRegion(d, meta)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	rawInstanceID, instanceIDExists := d.GetOk("instance_id")
+	if !instanceIDExists {
+		res, err := api.ListInstances(&documentdb.ListInstancesRequest{
+			Region:    region,
+			Name:      expandStringPtr(d.Get("instance_name")),
+			ProjectID: expandStringPtr(d.Get("project_id")),
+		})
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		for _, instance := range res.Instances {
+			if instance.Name == d.Get("instance_name").(string) {
+				if rawInstanceID != "" {
+					return diag.Errorf("more than 1 instance found with the same name %s", d.Get("name"))
+				}
+				rawInstanceID = instance.ID
+			}
+		}
+		if rawInstanceID == "" {
+			return diag.Errorf("no instance found with the name %s", d.Get("instance_name"))
+		}
+	}
+
+	instanceID := expandID(rawInstanceID)
+	instance, err := waitForDocumentDBInstance(ctx, api, region, instanceID, d.Timeout(schema.TimeoutRead))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	lb := getEndPointDocumentDBLoadBalancer(instance.Endpoints)
+	_ = d.Set("instance_id", instanceID)
+	_ = d.Set("instance_name", instance.Name)
+	_ = d.Set("hostname", flattenStringPtr(lb.Hostname))
+	_ = d.Set("port", int(lb.Port))
+	_ = d.Set("ip", flattenIPPtr(lb.IP))
+	_ = d.Set("name", lb.Name)
+
+	d.SetId(datasourceNewRegionalID(lb.ID, region))
+
+	return nil
+}
+
+func getEndPointDocumentDBLoadBalancer(endpoints []*documentdb.Endpoint) *documentdb.Endpoint {
+	for _, endpoint := range endpoints {
+		if endpoint.LoadBalancer != nil {
+			return endpoint
+		}
+	}
+
+	return nil
+}
