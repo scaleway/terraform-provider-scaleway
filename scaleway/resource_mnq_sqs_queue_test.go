4c5d59bacd472132fae940ddde91857856851d38
Jules Casteran <jcasteran@scaleway.com>
2023-10-17 14:42:05 +0200

    omit endpoint in mnq sqs queue default project test

--- scaleway/resource_mnq_sqs_queue_test.go
+++ scaleway/resource_mnq_sqs_queue_test.go
@@ -144,7 +144,6 @@ func TestAccScalewayMNQSQSQueue_DefaultProject(t *testing.T) {
 					resource scaleway_mnq_sqs_queue main {
 						project_id = scaleway_mnq_sqs.main.project_id
 						name = "test-mnq-sqs-queue-basic"
-						endpoint = scaleway_mnq_sqs.main.endpoint
 						access_key = scaleway_mnq_sqs_credentials.main.access_key
 						secret_key = scaleway_mnq_sqs_credentials.main.secret_key
 					}
