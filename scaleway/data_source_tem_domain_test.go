856626d5dcbc93e1ae82684222b83ab179ca18f7
Yacine FODIL <yfodil@scaleway.com>
2023-11-09 17:03:41 +0100

    feat(tem): add reputation field

--- scaleway/data_source_tem_domain_test.go
+++ scaleway/data_source_tem_domain_test.go
@@ -44,3 +44,34 @@ func TestAccScalewayDataSourceTemDomain_Basic(t *testing.T) {
 		},
 	})
 }
+
+func TestAccScalewayDataSourceTemDomain_Reputation(t *testing.T) {
+	tt := NewTestTools(t)
+	defer tt.Cleanup()
+
+	domainName := "test.scaleway-terraform.com"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      testAccCheckScalewayTemDomainDestroy(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					data "scaleway_tem_domain" "test" {
+						name = "%s"
+					}
+				`, domainName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewayTemDomainExists(tt, "data.scaleway_tem_domain.test"),
+					resource.TestCheckResourceAttr("data.scaleway_tem_domain.test", "name", domainName),
+					resource.TestCheckResourceAttrSet("scaleway_tem_domain.test", "reputation.0.status"),
+					resource.TestCheckResourceAttrSet("scaleway_tem_domain.test", "reputation.0.score"),
+					resource.TestCheckResourceAttrSet("scaleway_tem_domain.test", "reputation.0.scored_at"),
+					resource.TestCheckResourceAttrSet("scaleway_tem_domain.test", "reputation.0.previous_score"),
+					resource.TestCheckResourceAttrSet("scaleway_tem_domain.test", "reputation.0.previous_scored_at"),
+				),
+			},
+		},
+	})
+}
