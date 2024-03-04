ec8a276fa03c6032afd6071c98e2fb1bd871d77a
Jules Casteran <jcasteran@scaleway.com>
2024-03-04 14:22:23 +0100

    test(secret): add datasource path test

--- scaleway/data_source_secret_test.go
+++ scaleway/data_source_secret_test.go
@@ -59,3 +59,39 @@ func TestAccScalewayDataSourceSecret_Basic(t *testing.T) {
 		},
 	})
 }
+
+func TestAccScalewayDataSourceSecret_Path(t *testing.T) {
+	tt := NewTestTools(t)
+	defer tt.Cleanup()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		CheckDestroy:      testAccCheckScalewaySecretDestroy(tt),
+		ProviderFactories: tt.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					resource "scaleway_account_project" "project" {
+						name = "tf-tests-secret-ds-path"
+					}
+
+					resource "scaleway_secret" "main" {
+					  name = "test-secret-ds-path"
+					  path = "/test-secret-ds-path-path"
+					  project_id = scaleway_account_project.project.id
+					}
+					
+					data "scaleway_secret" "by_name" {
+					  name = scaleway_secret.main.name
+					  path = "/test-secret-ds-path-path"
+					  project_id = scaleway_account_project.project.id
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckScalewaySecretExists(tt, "data.scaleway_secret.by_name"),
+					resource.TestCheckResourceAttr("data.scaleway_secret.by_name", "name", "test-secret-ds-path"),
+				),
+			},
+		},
+	})
+}
