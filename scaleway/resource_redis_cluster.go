7a1caa351dadc2d56d39669963c6b35ba490d190
Leïla MARABESE <lmarabese@scaleway.com>
2024-03-26 19:28:36 +0100

    rdb changes needed for 1st gen success

--- scaleway/resource_redis_cluster.go
+++ scaleway/resource_redis_cluster.go
@@ -165,10 +165,10 @@ func ResourceScalewayRedisCluster() *schema.Resource {
 			},
 			// Computed
 			"public_network": {
-				Type:        schema.TypeList,
-				Optional:    true,
-				Computed:    true,
-				MaxItems:    1,
+				Type: schema.TypeList,
+				//Optional:    true,
+				Computed: true,
+				//MaxItems:    1,
 				Description: "Public network specs details",
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
