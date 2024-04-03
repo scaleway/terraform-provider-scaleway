03a30dd7486333ff5cab5df33595b9e767f80ab8
Rémy Léone <rleone@scaleway.com>
2024-04-03 11:56:04 +0200

    chore: enable multiplexing to allow different providers (#2491)

--- main.go
+++ main.go
@@ -5,27 +5,39 @@ import (
 	"flag"
 	"log"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5/tf5server"
+	"github.com/hashicorp/terraform-plugin-mux/tf5muxserver"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/provider"
 )
 
 func main() {
-	var debugMode bool
+	ctx := context.Background()
 
+	var debugMode bool
 	flag.BoolVar(&debugMode, "debuggable", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
-
+	var serveOpts []tf5server.ServeOpt
 	if debugMode {
-		err := plugin.Debug(context.Background(), "registry.terraform.io/scaleway/scaleway",
-			&plugin.ServeOpts{
-				ProviderFunc: provider.Provider(provider.DefaultConfig()),
-			})
-		if err != nil {
-			log.Println(err.Error())
-		}
-	} else {
-		plugin.Serve(&plugin.ServeOpts{
-			ProviderFunc: provider.Provider(provider.DefaultConfig()),
-		})
+		serveOpts = append(serveOpts, tf5server.WithManagedDebug())
+	}
+
+	providers := []func() tfprotov5.ProviderServer{
+		// Provider using terraform-plugin-sdk
+		provider.Provider(provider.DefaultConfig())().GRPCProvider,
+	}
+
+	muxServer, err := tf5muxserver.NewMuxServer(ctx, providers...)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tf5server.Serve(
+		"registry.terraform.io/scaleway/scaleway",
+		muxServer.ProviderServer,
+		serveOpts...,
+	)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
