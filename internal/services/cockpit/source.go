3c82f2fd71f542a6510a668a56dfcfd25ef54101
Yacine Fodil <105779815+yfodil@users.noreply.github.com>
2024-04-22 13:38:44 +0200

    feat(cockpit): add datasources (#2543)
    * feat(cockpit): add datasources
    
    * add type possible values
    
    * rename to source
    
    ---------
    
    Co-authored-by: Rémy Léone <rleone@scaleway.com>

---
+++ internal/services/cockpit/source.go
@@ -0,0 +1,147 @@
+package cockpit
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/scaleway/scaleway-sdk-go/api/cockpit/v1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/httperrors"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/locality/regional"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/services/account"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/types"
+)
+
+func ResourceCockpitSource() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: ResourceCockpitSourceCreate,
+		ReadContext:   ResourceCockpitSourceRead,
+		DeleteContext: ResourceCockpitSourceDelete,
+		Timeouts: &schema.ResourceTimeout{
+			Create:  schema.DefaultTimeout(DefaultCockpitTimeout),
+			Read:    schema.DefaultTimeout(DefaultCockpitTimeout),
+			Delete:  schema.DefaultTimeout(DefaultCockpitTimeout),
+			Default: schema.DefaultTimeout(DefaultCockpitTimeout),
+		},
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+				Description: "Name of the datasource",
+			},
+			"type": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+				Description: "The type of the datasource",
+				ValidateFunc: validation.StringInSlice([]string{
+					cockpit.DataSourceTypeMetrics.String(),
+					cockpit.DataSourceTypeLogs.String(),
+					cockpit.DataSourceTypeTraces.String(),
+				}, false),
+			},
+			// computed
+			"url": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The URL of the datasource",
+			},
+			"origin": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The origin of the datasource",
+			},
+			"synchronized_with_grafana": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Indicates whether the data source is synchronized with Grafana",
+			},
+			"created_at": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The date and time of the creation of the cockpit datasource",
+			},
+			"updated_at": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The date and time of the last update of the cockpit datasource",
+			},
+			"project_id": account.ProjectIDSchema(),
+			"region":     regional.Schema(),
+		},
+	}
+}
+
+func ResourceCockpitSourceCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	api, region, err := cockpitAPIWithRegion(d, meta)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	res, err := api.CreateDataSource(&cockpit.RegionalAPICreateDataSourceRequest{
+		Region:    region,
+		ProjectID: d.Get("project_id").(string),
+		Name:      d.Get("name").(string),
+		Type:      cockpit.DataSourceType(d.Get("type").(string)),
+	}, scw.WithContext(ctx))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(regional.NewIDString(region, res.ID))
+	return ResourceCockpitSourceRead(ctx, d, meta)
+}
+
+func ResourceCockpitSourceRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	api, region, id, err := NewAPIWithRegionAndID(meta, d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	res, err := api.GetDataSource(&cockpit.RegionalAPIGetDataSourceRequest{
+		Region:       region,
+		DataSourceID: id,
+	}, scw.WithContext(ctx))
+	if err != nil {
+		if httperrors.Is404(err) {
+			d.SetId("")
+			return nil
+		}
+		return diag.FromErr(err)
+	}
+
+	_ = d.Set("name", res.Name)
+	_ = d.Set("type", res.Type.String())
+	_ = d.Set("url", res.URL)
+	_ = d.Set("origin", res.Origin)
+	_ = d.Set("synchronized_with_grafana", res.SynchronizedWithGrafana)
+	_ = d.Set("region", res.Region)
+	_ = d.Set("created_at", types.FlattenTime(res.CreatedAt))
+	_ = d.Set("updated_at", types.FlattenTime(res.UpdatedAt))
+	_ = d.Set("project_id", res.ProjectID)
+
+	return nil
+}
+
+func ResourceCockpitSourceDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	api, region, id, err := NewAPIWithRegionAndID(meta, d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	err = api.DeleteDataSource(&cockpit.RegionalAPIDeleteDataSourceRequest{
+		DataSourceID: id,
+		Region:       region,
+	}, scw.WithContext(ctx))
+	if err != nil && !httperrors.Is404(err) {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
---
+++ internal/services/cockpit/source_test.go
@@ -0,0 +1,105 @@
+package cockpit_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	cockpitSDK "github.com/scaleway/scaleway-sdk-go/api/cockpit/v1"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/acctest"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/httperrors"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/services/cockpit"
+)
+
+func TestAccCockpitSource_Basic(t *testing.T) {
+	tt := acctest.NewTestTools(t)
+	defer tt.Cleanup()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.PreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      isSourceDestroyed(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					resource "scaleway_account_project" "project" {
+						name = "tf_tests_cockpit_datasource_basic"
+				  	}
+
+					resource "scaleway_cockpit_source" "main" {
+					  project_id = scaleway_account_project.project.id
+					  name       = "my-source"
+					  type       = "metrics"
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					isSourcePresent(tt, "scaleway_cockpit_source.main"),
+					resource.TestCheckResourceAttr("scaleway_cockpit_source.main", "name", "my-source"),
+					resource.TestCheckResourceAttr("scaleway_cockpit_source.main", "type", "metrics"),
+					resource.TestCheckResourceAttr("scaleway_cockpit_source.main", "region", "fr-par"),
+					resource.TestCheckResourceAttrSet("scaleway_cockpit_source.main", "url"),
+					resource.TestCheckResourceAttrSet("scaleway_cockpit_source.main", "origin"),
+					resource.TestCheckResourceAttrSet("scaleway_cockpit_source.main", "created_at"),
+					resource.TestCheckResourceAttrSet("scaleway_cockpit_source.main", "updated_at"),
+					resource.TestCheckResourceAttrSet("scaleway_cockpit_source.main", "synchronized_with_grafana"),
+					resource.TestCheckResourceAttrPair("scaleway_cockpit_source.main", "project_id", "scaleway_account_project.project", "id"),
+				),
+			},
+		},
+	})
+}
+
+func isSourcePresent(tt *acctest.TestTools, n string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		rs, ok := state.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("resource cockpit source not found: %s", n)
+		}
+
+		api, region, ID, err := cockpit.NewAPIWithRegionAndID(tt.Meta, rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		_, err = api.GetDataSource(&cockpitSDK.RegionalAPIGetDataSourceRequest{
+			Region:       region,
+			DataSourceID: ID,
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func isSourceDestroyed(tt *acctest.TestTools) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		for _, rs := range state.RootModule().Resources {
+			if rs.Type != "scaleway_cockpit_source" {
+				continue
+			}
+
+			api, region, ID, err := cockpit.NewAPIWithRegionAndID(tt.Meta, rs.Primary.ID)
+			if err != nil {
+				return err
+			}
+
+			_, err = api.GetDataSource(&cockpitSDK.RegionalAPIGetDataSourceRequest{
+				Region:       region,
+				DataSourceID: ID,
+			})
+
+			if err == nil {
+				return fmt.Errorf("cockpit source (%s) still exists", rs.Primary.ID)
+			}
+
+			if !httperrors.Is404(err) {
+				return err
+			}
+		}
+
+		return nil
+	}
+}
