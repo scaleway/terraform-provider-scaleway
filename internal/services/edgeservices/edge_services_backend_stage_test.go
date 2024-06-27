7377188f2c13591e91a38af42ff3d58311243d39
Yacine FODIL <yfodil@scaleway.com>
2024-06-27 17:26:56 +0200

    update test

--- internal/services/edgeservices/edge_services_backend_stage_test.go
+++ internal/services/edgeservices/edge_services_backend_stage_test.go
@@ -1,10 +1,8 @@
 package edgeservices_test
 
 import (
-	"fmt"
 	"testing"
 
-	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/acctest"
 	edgeservicestestfuncs "github.com/scaleway/terraform-provider-scaleway/v2/internal/services/edgeservices/testfuncs"
@@ -13,16 +11,15 @@ import (
 func TestAccEdgeServicesBackend_Basic(t *testing.T) {
 	tt := acctest.NewTestTools(t)
 	defer tt.Cleanup()
-	bucketBasic := sdkacctest.RandomWithPrefix("test-acc-scaleway-object-bucket-basic-es")
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { acctest.PreCheck(t) },
 		ProviderFactories: tt.ProviderFactories,
 		CheckDestroy:      edgeservicestestfuncs.CheckEdgeServicesBackendDestroy(tt),
 		Steps: []resource.TestStep{
 			{
-				Config: fmt.Sprintf(`
+				Config: `
 					resource "scaleway_object_bucket" "main" {
-						name = "%[1]s"
+						name = "test-acc-scaleway-object-bucket-basic-es"
 						tags = {
 							foo = "bar"
 						}
@@ -34,7 +31,7 @@ func TestAccEdgeServicesBackend_Basic(t *testing.T) {
 						bucket_region = "fr-par"
 					  }
 					}
-				`, bucketBasic),
+				`,
 				Check: resource.ComposeTestCheckFunc(
 					edgeservicestestfuncs.CheckEdgeServicesBackendExists(tt, "scaleway_edge_services_backend_stage.main"),
 					resource.TestCheckResourceAttr("scaleway_edge_services_backend_stage.main", "s3_backend_config.0.is_website", "false"),
