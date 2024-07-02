b3b63220ec6aa8a5325d4b30b5d58de13e965429
Tony BELLAVIA <tbellavia@scaleway.com>
2024-07-02 16:44:14 +0200

    feat(iam): add iam api key datasource

---
+++ internal/services/iam/api_key_data_source.go
@@ -0,0 +1,38 @@
+package iam
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/datasource"
+)
+
+func DataSourceApiKey() *schema.Resource {
+	dsSchema := datasource.SchemaFromResourceSchema(ResourceAPIKey().Schema)
+
+	dsSchema["access_key"].Required = true
+	dsSchema["access_key"].Computed = false
+	delete(dsSchema, "secret_key")
+	return &schema.Resource{
+		ReadContext: DataSourceIamApiKeyRead,
+		Schema:      dsSchema,
+	}
+}
+
+func DataSourceIamApiKeyRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	accessKey := d.Get("access_key").(string)
+
+	d.SetId(accessKey)
+
+	diags := resourceIamAPIKeyRead(ctx, d, m)
+	if diags != nil {
+		return append(diags, diag.Errorf("failed to read iam api key state")...)
+	}
+
+	if d.Id() == "" {
+		return diag.Errorf("iam api key (%s) not found", accessKey)
+	}
+
+	return nil
+}
