211cfac1d093570b5c27519aaa8ec5df13d3e147
Yacine FODIL <yfodil@scaleway.com>
2024-07-03 16:47:04 +0200

    add support for ipam ids

--- internal/services/lb/lb.go
+++ internal/services/lb/lb.go
@@ -143,6 +143,16 @@ func ResourceLb() *schema.Resource {
 							Optional:    true,
 							Computed:    true,
 						},
+						"ipam_ids": {
+							Type: schema.TypeList,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+							MaxItems:    1,
+							Optional:    true,
+							Computed:    true,
+							Description: "IPAM ID of a pre-reserved IP address to assign to the Load Balancer on this Private Network. In the future, it will be possible to specify multiple IPs in this field (IPv4 and IPv6), for now only one ID of an IPv4 address is expected",
+						},
 						// Readonly attributes
 						"status": {
 							Type:        schema.TypeString,
