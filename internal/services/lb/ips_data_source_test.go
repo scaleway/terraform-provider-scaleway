45cddcd58de08e5c9aa7726711892d19efea8bd7
Yacine Fodil <105779815+yfodil@users.noreply.github.com>
2024-07-08 11:01:57 +0200

    feat(lb): add support for tags in LB IPs (#2564)
    * bump sdk go
    
    * support tags in lb ips
    
    * add doc
    
    * update cassettes
    
    * decouple test
    
    * fix

--- internal/services/lb/ips_data_source_test.go
+++ internal/services/lb/ips_data_source_test.go
@@ -29,23 +29,127 @@ func TestAccDataSourceIPs_Basic(t *testing.T) {
 					resource scaleway_lb_ip ip2 {
 					}
 
-					data "scaleway_lb_ips" "lbs_by_cidr_range" {
+					data "scaleway_lb_ips" "ips_by_cidr_range" {
 						ip_cidr_range = "0.0.0.0/0"
 						depends_on = [scaleway_lb_ip.ip1, scaleway_lb_ip.ip2]
 					}
-					data "scaleway_lb_ips" "lbs_by_cidr_range_other_zone" {
+					data "scaleway_lb_ips" "ips_by_cidr_range_other_zone" {
 						ip_cidr_range  = "0.0.0.0/0"
 						zone = "fr-par-2"
 						depends_on = [scaleway_lb_ip.ip1, scaleway_lb_ip.ip2]
 					}
 					`,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.lbs_by_cidr_range", "ips.0.id"),
-					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.lbs_by_cidr_range", "ips.0.ip_address"),
-					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.lbs_by_cidr_range", "ips.1.id"),
-					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.lbs_by_cidr_range", "ips.1.ip_address"),
+					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.ips_by_cidr_range", "ips.0.id"),
+					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.ips_by_cidr_range", "ips.0.ip_address"),
+					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.ips_by_cidr_range", "ips.1.id"),
+					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.ips_by_cidr_range", "ips.1.ip_address"),
 
-					resource.TestCheckNoResourceAttr("data.scaleway_lb_ips.lbs_by_cidr_range_other_zone", "ips.0.id"),
+					resource.TestCheckNoResourceAttr("data.scaleway_lb_ips.ips_by_cidr_range_other_zone", "ips.0.id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceIPs_WithType(t *testing.T) {
+	tt := acctest.NewTestTools(t)
+	defer tt.Cleanup()
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.PreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      lbchecks.IsIPDestroyed(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					resource scaleway_lb_ip ip1 {
+					}
+				`,
+			},
+			{
+				Config: `
+					resource scaleway_lb_ip ip1 {
+					}
+					resource scaleway_lb_ip ip2 {
+					  is_ipv6 = true
+					}
+				`,
+			},
+			{
+				Config: `
+					resource scaleway_lb_ip ip1 {
+					}
+					resource scaleway_lb_ip ip2 {
+					  is_ipv6 = true
+					}
+					resource scaleway_lb_ip ip3 {
+					}
+
+					data "scaleway_lb_ips" "ips_by_type" {
+						ip_type = "ipv4"
+						depends_on = [scaleway_lb_ip.ip1, scaleway_lb_ip.ip2, scaleway_lb_ip.ip3]
+					}
+					`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.scaleway_lb_ips.ips_by_type", "ips.#", "2"),
+					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.ips_by_type", "ips.0.id"),
+					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.ips_by_type", "ips.0.ip_address"),
+					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.ips_by_type", "ips.1.id"),
+					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.ips_by_type", "ips.1.ip_address"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceIPs_WithTags(t *testing.T) {
+	tt := acctest.NewTestTools(t)
+	defer tt.Cleanup()
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.PreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      lbchecks.IsIPDestroyed(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					resource scaleway_lb_ip ip1 {
+					  tags = [ "ipv4", "ip" ]
+					}
+				`,
+			},
+			{
+				Config: `
+					resource scaleway_lb_ip ip1 {
+					  tags = [ "ipv4", "ip" ]
+					}
+					resource scaleway_lb_ip ip2 {
+					  tags = [ "ipv4", "ip" ]
+					}
+				`,
+			},
+			{
+				Config: `
+					resource scaleway_lb_ip ip1 {
+					  tags = [ "ipv4", "ip" ]
+					}
+					resource scaleway_lb_ip ip2 {
+					  tags = [ "ipv4", "ip" ]
+					}
+					resource scaleway_lb_ip ip3 {
+					  tags = [ "other", "tags" ]
+					}
+
+					data "scaleway_lb_ips" "ips_by_tags" {
+					    tags = [ "ipv4", "ip" ]
+						depends_on = [scaleway_lb_ip.ip1, scaleway_lb_ip.ip2, scaleway_lb_ip.ip3]
+					}
+					`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.scaleway_lb_ips.ips_by_tags", "ips.#", "2"),
+					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.ips_by_tags", "ips.0.id"),
+					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.ips_by_tags", "ips.0.ip_address"),
+					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.ips_by_tags", "ips.1.id"),
+					resource.TestCheckResourceAttrSet("data.scaleway_lb_ips.ips_by_tags", "ips.1.ip_address"),
 				),
 			},
 		},
