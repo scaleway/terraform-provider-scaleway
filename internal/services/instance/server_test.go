8364c0046b3f65fe198b71aa155ce4a3c4b211e6
Jules Casteran <jcasteran@scaleway.com>
2024-07-19 11:38:03 +0200

    support IOPS configuration

--- internal/services/instance/server_test.go
+++ internal/services/instance/server_test.go
@@ -1995,12 +1995,16 @@ func TestAccServer_BlockExternalRootVolume(t *testing.T) {
 						type  = "PLAY2-PICO"
 						root_volume {
 							volume_type = "sbs_volume"
+							size_in_gb = 50
+							sbs_iops = 15000
 						}
 					}`,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("scaleway_instance_server.main", "type", "PLAY2-PICO"),
 					resource.TestCheckResourceAttr("scaleway_instance_server.main", "additional_volume_ids.#", "0"),
 					resource.TestCheckResourceAttr("scaleway_instance_server.main", "root_volume.0.volume_type", string(instanceSDK.VolumeVolumeTypeSbsVolume)),
+					resource.TestCheckResourceAttr("scaleway_instance_server.main", "root_volume.0.sbs_iops", "15000"),
+					resource.TestCheckResourceAttr("scaleway_instance_server.main", "root_volume.0.size_in_gb", "50"),
 				),
 			},
 		},
