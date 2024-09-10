7b2f95a1392abe023a878bfb86905565cc2dae9c
Laure-di <masson.laure.l@gmail.com>
2024-09-10 16:59:16 +0200

    feat(inference): add support

---
+++ internal/services/inference/deployment_test.go
@@ -0,0 +1,92 @@
+package inference_test
+
+import (
+	"fmt"
+	inferenceSDK "github.com/scaleway/scaleway-sdk-go/api/inference/v1beta1"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/acctest"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/httperrors"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/services/inference"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccDeployment_Basic(t *testing.T) {
+	tt := acctest.NewTestTools(t)
+	defer tt.Cleanup()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.PreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      testAccCheckDeploymentDestroy(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					resource "scaleway_inference_deployment" "main" {
+						name = "test-inferenceSDK-deployment-basic"
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDeploymentExists(tt, "scaleway_inference_deployment.main"),
+					resource.TestCheckResourceAttr("scaleway_inference_deployment.main", "name", "test-inferenceSDK-deployment-basic"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckDeploymentExists(tt *acctest.TestTools, n string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		rs, ok := state.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", n)
+		}
+
+		api, region, id, err := inference.NewAPIWithRegionAndID(tt.Meta, rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		_, err = api.GetDeployment(&inferenceSDK.GetDeploymentRequest{
+			DeploymentID: id,
+			Region:       region,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckDeploymentDestroy(tt *acctest.TestTools) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		for _, rs := range state.RootModule().Resources {
+			if rs.Type != "scaleway_inference_deployment" {
+				continue
+			}
+
+			api, region, id, err := inference.NewAPIWithRegionAndID(tt.Meta, rs.Primary.ID)
+			if err != nil {
+				return err
+			}
+
+			_, err = api.DeleteDeployment(&inferenceSDK.DeleteDeploymentRequest{
+				DeploymentID: id,
+				Region:       region,
+			})
+
+			if err == nil {
+				return fmt.Errorf("inferenceSDK deployment (%s) still exists", rs.Primary.ID)
+			}
+
+			if !httperrors.Is404(err) {
+				return err
+			}
+		}
+
+		return nil
+	}
+}
