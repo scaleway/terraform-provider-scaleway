1909bce6ac751ae1af5613d05a4dfa159f6938cf
Jonathan R. <jremy@scaleway.com>
2024-09-18 10:22:16 +0200

    fix(cockpit): add grafana url manual creation (#2741)

--- internal/services/cockpit/cockpit_data_source.go
+++ internal/services/cockpit/cockpit_data_source.go
@@ -76,6 +76,9 @@ func dataSourceCockpitRead(ctx context.Context, d *schema.ResourceData, m interf
 	if err != nil {
 		return diag.FromErr(err)
 	}
+	if grafana.GrafanaURL == "" {
+		grafana.GrafanaURL = createGrafanaURL(d.Get("project_id").(string), region)
+	}
 
 	alertManager, err := regionalAPI.GetAlertManager(&cockpit.RegionalAPIGetAlertManagerRequest{
 		ProjectID: d.Get("project_id").(string),
