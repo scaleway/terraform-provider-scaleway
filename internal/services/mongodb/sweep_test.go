3e7ffa23e37a385214d3b3affdf5a2aef95f68d0
jremy <jremy@scaleway.com>
2024-09-30 09:34:55 +0200

    feat(mongodb): resource instance

---
+++ internal/services/mongodb/sweep_test.go
@@ -0,0 +1,16 @@
+package redis_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	redistestfuncs "github.com/scaleway/terraform-provider-scaleway/v2/internal/services/redis/testfuncs"
+)
+
+func init() {
+	redistestfuncs.AddTestSweepers()
+}
+
+func TestMain(m *testing.M) {
+	resource.TestMain(m)
+}
