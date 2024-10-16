4ff9576315627da5cf42297be910628490a5dbf6
Jules Casteran <jcasteran@scaleway.com>
2024-10-16 14:54:06 +0200

    fix(instance): remove bootscript usage

--- internal/services/instance/server.go
+++ internal/services/instance/server.go
@@ -423,10 +423,6 @@ func ResourceInstanceServerCreate(ctx context.Context, d *schema.ResourceData, m
 		req.EnableIPv6 = scw.BoolPtr(enableIPv6.(bool)) //nolint:staticcheck
 	}
 
-	if bootScriptID, ok := d.GetOk("bootscript_id"); ok {
-		req.Bootscript = types.ExpandStringPtr(bootScriptID) //nolint:staticcheck
-	}
-
 	if bootType, ok := d.GetOk("boot_type"); ok {
 		bootType := instanceSDK.BootType(bootType.(string))
 		req.BootType = &bootType
@@ -616,11 +612,6 @@ func ResourceInstanceServerRead(ctx context.Context, d *schema.ResourceData, m i
 		_ = d.Set("zone", string(zone))
 		_ = d.Set("name", server.Name)
 		_ = d.Set("boot_type", server.BootType)
-
-		// Bootscript is deprecated
-		if server.Bootscript != nil { //nolint:staticcheck
-			_ = d.Set("bootscript_id", server.Bootscript.ID) //nolint:staticcheck
-		}
 
 		_ = d.Set("type", server.CommercialType)
 		if len(server.Tags) > 0 {
@@ -919,17 +910,6 @@ func ResourceInstanceServerUpdate(ctx context.Context, d *schema.ResourceData, m
 			warnings = append(warnings, diag.Diagnostic{
 				Severity: diag.Warning,
 				Summary:  "instanceSDK may need to be rebooted to use the new boot type",
-			})
-		}
-	}
-
-	if d.HasChanges("bootscript_id") {
-		serverShouldUpdate = true
-		updateRequest.Bootscript = types.ExpandStringPtr(d.Get("bootscript_id").(string))
-		if !isStopped {
-			warnings = append(warnings, diag.Diagnostic{
-				Severity: diag.Warning,
-				Summary:  "instanceSDK may need to be rebooted to use the new bootscript",
 			})
 		}
 	}
