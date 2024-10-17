cbc5f4827dfbc4552af0bd418c7f9767e075726d
jremy <jremy@scaleway.com>
2024-10-17 13:02:49 +0200

    feat(mongodb): fix lint

--- internal/services/mongodb/data_source_instance.go
+++ internal/services/mongodb/data_source_instance.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/scaleway/scaleway-sdk-go/api/mongodb/v1alpha1"
+	mongodb "github.com/scaleway/scaleway-sdk-go/api/mongodb/v1alpha1"
 	"github.com/scaleway/scaleway-sdk-go/scw"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/datasource"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/locality"
