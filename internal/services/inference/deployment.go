adb9c7dbb33b4978eb48fea1e2e988e888276dc2
Laure-di <masson.laure.l@gmail.com>
2024-10-23 09:09:08 +0200

    fix lint

--- internal/services/inference/deployment.go
+++ internal/services/inference/deployment.go
@@ -2,6 +2,7 @@ package inference
 
 import (
 	"context"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	inference "github.com/scaleway/scaleway-sdk-go/api/inference/v1beta1"
