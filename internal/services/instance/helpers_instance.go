e712a91d8905846377280876c8d9c67e48ad5442
Jules Castéran <jcasteran@scaleway.com>
2024-12-04 10:43:54 +0100

    feat(instance_image): support block snapshots (#2823)
    * feat(instance_image): support block snapshots
    
    * fix: do not query snapshots before creating instance_image
    
    * record test
    
    * go mod tidy
    
    * fix test
    split block volumes creations in two steps. Forcing order also force IDs with cassettes

--- internal/services/instance/helpers_instance.go
+++ internal/services/instance/helpers_instance.go
@@ -179,7 +179,7 @@ func reachState(ctx context.Context, api *BlockAndInstanceAPI, zone scw.Zone, se
 			if err != nil {
 				return err
 			}
-		} else if volume.State != instance.VolumeServerStateAvailable {
+		} else if volume.State != nil && *volume.State != instance.VolumeServerStateAvailable {
 			_, err = api.WaitForVolume(&instance.WaitForVolumeRequest{
 				Zone:          zone,
 				VolumeID:      volume.ID,
@@ -405,25 +405,6 @@ func (ph *privateNICsHandler) get(key string) (interface{}, error) {
 		"zone":        loc,
 		"pnic_id":     pn.ID,
 	}, nil
-}
-
-func getSnapshotsFromIDs(ctx context.Context, snapIDs []interface{}, instanceAPI *instance.API) ([]*instance.GetSnapshotResponse, error) {
-	snapResponses := []*instance.GetSnapshotResponse(nil)
-	for _, snapID := range snapIDs {
-		zone, id, err := zonal.ParseID(snapID.(string))
-		if err != nil {
-			return nil, err
-		}
-		snapshot, err := instanceAPI.GetSnapshot(&instance.GetSnapshotRequest{
-			Zone:       zone,
-			SnapshotID: id,
-		}, scw.WithContext(ctx))
-		if err != nil {
-			return snapResponses, fmt.Errorf("extra volumes : could not find snapshot with id %s", snapID)
-		}
-		snapResponses = append(snapResponses, snapshot)
-	}
-	return snapResponses, nil
 }
 
 func formatImageLabel(imageUUID string) string {
