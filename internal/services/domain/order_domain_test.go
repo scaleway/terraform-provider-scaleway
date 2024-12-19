ba5c259655a3d9bb5e3ec86f306099371185457a
devtools-ci-cd <jremy@scaleway.com>
2024-12-19 15:01:17 +0100

    feat(domain): add chore of new resource order domain

--- internal/services/domain/order_domain_test.go
+++ internal/services/domain/order_domain_test.go
@@ -6,14 +6,17 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	domainSDK "github.com/scaleway/scaleway-sdk-go/api/domain/v2beta1"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/acctest"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/httperrors"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/services/domain"
 )
 
 func TestAccOrderDomain_Basic(t *testing.T) {
 	tt := acctest.NewTestTools(t)
 	defer tt.Cleanup()
 
-	domainName := "test-basic-domain11.com"
+	domainName := "test-basic-domain56.com"
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { acctest.PreCheck(t) },
@@ -21,6 +24,7 @@ func TestAccOrderDomain_Basic(t *testing.T) {
 		CheckDestroy:      testAccCheckOrderDomainDestroy(tt),
 		Steps: []resource.TestStep{
 			{
+				//test and doc with for each for multiple domains
 				Config: fmt.Sprintf(`
 				resource "scaleway_domain_order_domain" "test" {
 				domain_name       = "%s"
@@ -28,9 +32,9 @@ func TestAccOrderDomain_Basic(t *testing.T) {
 
 				owner_contact {
 					  firstname       = "John"
-					  lastname        = "Doe"
+					  lastname        = "DOE"
 					  email           = "john.doe@example.com"
-					  phone_number    = "+123456789"
+					  phone_number    = "+1.23456789"
 					  address_line_1  = "123 Main Street"
 					  city            = "Paris"
 					  zip             = "75001"
@@ -42,13 +46,27 @@ func TestAccOrderDomain_Basic(t *testing.T) {
 				}`, domainName, 1),
 
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("scaleway_order_domain.test", "domain_name", domainName),
-					resource.TestCheckResourceAttr("scaleway_order_domain.test", "duration_in_years", "1"),
-					resource.TestCheckResourceAttrSet("scaleway_order_domain.test", "id"),
-					resource.TestCheckResourceAttrSet("scaleway_order_domain.test", "auto_renew_status"),
-					resource.TestCheckResourceAttrSet("scaleway_order_domain.test", "status"),
-					resource.TestCheckResourceAttrSet("scaleway_order_domain.test", "created_at"),
-					resource.TestCheckResourceAttrSet("scaleway_order_domain.test", "updated_at"),
+					resource.TestCheckResourceAttr("scaleway_domain_order_domain.test", "domain_name", domainName),
+					resource.TestCheckResourceAttr("scaleway_domain_order_domain.test", "duration_in_years", "1"),
+
+					resource.TestCheckResourceAttrSet("scaleway_domain_order_domain.test", "id"),
+					resource.TestCheckResourceAttrSet("scaleway_domain_order_domain.test", "auto_renew_status"),
+					resource.TestCheckResourceAttrSet("scaleway_domain_order_domain.test", "dnssec_status"),
+					resource.TestCheckResourceAttr("scaleway_domain_order_domain.test", "epp_code.0", "clientTransferProhibited"),
+					resource.TestCheckResourceAttrSet("scaleway_domain_order_domain.test", "expired_at"),
+					resource.TestCheckResourceAttrSet("scaleway_domain_order_domain.test", "updated_at"),
+					resource.TestCheckResourceAttrSet("scaleway_domain_order_domain.test", "registrar"),
+					resource.TestCheckResourceAttrSet("scaleway_domain_order_domain.test", "is_external"),
+					resource.TestCheckResourceAttrSet("scaleway_domain_order_domain.test", "status"),
+					resource.TestCheckResourceAttrSet("scaleway_domain_order_domain.test", "organization_id"),
+					resource.TestCheckResourceAttrSet("scaleway_domain_order_domain.test", "pending_trade"),
+
+					resource.TestCheckResourceAttrSet("scaleway_domain_order_domain.test", "tld.0.name"),
+					resource.TestCheckResourceAttrSet("scaleway_domain_order_domain.test", "tld.0.dnssec_support"),
+					resource.TestCheckResourceAttrSet("scaleway_domain_order_domain.test", "dns_zones.0.domain"),
+					resource.TestCheckResourceAttrSet("scaleway_domain_order_domain.test", "dns_zones.0.status"),
+					resource.TestCheckResourceAttrSet("scaleway_domain_order_domain.test", "dns_zones.0.ns_default.0"),
+					resource.TestCheckResourceAttr("scaleway_domain_order_domain.test", "linked_products.#", "0"),
 				),
 			},
 		},
@@ -62,6 +80,26 @@ func testAccCheckOrderDomainDestroy(tt *acctest.TestTools) resource.TestCheckFun
 				continue
 			}
 
+			registrarAPI := domain.NewRegistrarDomainAPI(tt.Meta)
+
+			domainName, err := domain.ExtractDomainFromID(rs.Primary.ID)
+
+			domainResp, err := registrarAPI.GetDomain(&domainSDK.RegistrarAPIGetDomainRequest{
+				Domain: domainName,
+			})
+
+			if err != nil {
+				if httperrors.Is404(err) {
+					return nil
+				}
+				return fmt.Errorf("failed to get domain details: %w", err)
+			}
+
+			if domainResp.AutoRenewStatus != domainSDK.DomainFeatureStatusDisabled {
+				return fmt.Errorf("expected auto-renew to be 'disabled' for domain %s, got %s",
+					domainName, domainResp.AutoRenewStatus)
+			}
+			return fmt.Errorf("domain %s still exists and auto-renew is not 'disabled'", domainName)
 		}
 
 		return nil
