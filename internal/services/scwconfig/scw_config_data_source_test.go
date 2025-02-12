3464e613d98c46c8fd17d27a29f631578bcaaf69
Rémy Léone <rleone@scaleway.com>
2025-02-12 17:22:48 +0100

    test: fix linting errors

--- internal/services/scwconfig/scw_config_data_source_test.go
+++ internal/services/scwconfig/scw_config_data_source_test.go
@@ -30,7 +30,7 @@ func TestAccDataSourceConfig_ActiveProfile(t *testing.T) {
 		PreCheck: func() { acctest.PreCheck(t) },
 		ProviderFactories: func() map[string]func() (*schema.Provider, error) {
 			_ = os.Unsetenv("SCW_PROFILE")
-			_ = os.Setenv("SCW_CONFIG_PATH", "./testfixture/test_config.yaml")
+			t.Setenv("SCW_CONFIG_PATH", "./testfixture/test_config.yaml")
 			metaDefault, err := meta.NewMeta(ctx, &meta.Config{
 				TerraformVersion: "terraform-tests",
 				HTTPClient:       tt.Meta.HTTPClient(),
@@ -82,8 +82,8 @@ func TestAccDataSourceConfig_OtherProfile(t *testing.T) {
 		PreCheck: func() { acctest.PreCheck(t) },
 		ProviderFactories: func() map[string]func() (*schema.Provider, error) {
 			_ = os.Unsetenv("SCW_PROFILE")
-			_ = os.Setenv("SCW_CONFIG_PATH", "./testfixture/test_config.yaml")
-			_ = os.Setenv("SCW_PROFILE", "other")
+			t.Setenv("SCW_CONFIG_PATH", "./testfixture/test_config.yaml")
+			t.Setenv("SCW_PROFILE", "other")
 			metaOther, err := meta.NewMeta(ctx, &meta.Config{
 				TerraformVersion: "terraform-tests",
 				HTTPClient:       tt.Meta.HTTPClient(),
@@ -135,9 +135,9 @@ func TestAccDataSourceConfig_MixedProfile(t *testing.T) {
 		PreCheck: func() { acctest.PreCheck(t) },
 		ProviderFactories: func() map[string]func() (*schema.Provider, error) {
 			_ = os.Unsetenv("SCW_PROFILE")
-			_ = os.Setenv("SCW_CONFIG_PATH", "./testfixture/test_config.yaml")
-			_ = os.Setenv("SCW_PROFILE", "incomplete")
-			_ = os.Setenv("SCW_DEFAULT_PROJECT_ID", "77777777-7777-7777-7777-777777777777")
+			t.Setenv("SCW_CONFIG_PATH", "./testfixture/test_config.yaml")
+			t.Setenv("SCW_PROFILE", "incomplete")
+			t.Setenv("SCW_DEFAULT_PROJECT_ID", "77777777-7777-7777-7777-777777777777")
 			metaMixed, err := meta.NewMeta(ctx, &meta.Config{
 				TerraformVersion: "terraform-tests",
 				HTTPClient:       tt.Meta.HTTPClient(),
