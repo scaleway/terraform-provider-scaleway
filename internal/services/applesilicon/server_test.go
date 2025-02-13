fa12aa6104abf70ed98334f2a64dad9ee07d38d7
Laure-di <masson.laure.l@gmail.com>
2025-02-13 15:17:05 +0100

    update cassettes

--- internal/services/applesilicon/server_test.go
+++ internal/services/applesilicon/server_test.go
@@ -13,8 +13,6 @@ import (
 )
 
 func TestAccServer_Basic(t *testing.T) {
-	t.Skip("Skipping AppleSilicon test as this kind of server can't be deleted before 24h")
-
 	tt := acctest.NewTestTools(t)
 	defer tt.Cleanup()
 	resource.ParallelTest(t, resource.TestCase{
