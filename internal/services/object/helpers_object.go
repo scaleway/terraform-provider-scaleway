8d2bc0b30b7860d3341ed5e8c3fc09bd6bc40dc7
Laure-di <masson.laure.l@gmail.com>
2025-02-13 15:49:16 +0100

    fix golangci-lint

--- internal/services/object/helpers_object.go
+++ internal/services/object/helpers_object.go
@@ -73,6 +73,7 @@ func newS3Client(ctx context.Context, region, accessKey, secretKey string, httpC
 		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accessKey, secretKey, "")),
 		config.WithHTTPClient(httpClient),
 	)
+
 	if err != nil {
 		return nil, err
 	}
