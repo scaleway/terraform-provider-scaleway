379c74adef508a6638d592a79c609e18755b408b
Jules Casteran <jcasteran@scaleway.com>
2025-02-14 09:46:48 +0100

    feat(block_volume): support migration from instance volume

--- internal/services/block/volume.go
+++ internal/services/block/volume.go
@@ -13,6 +13,7 @@ import (
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/locality"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/locality/zonal"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/services/account"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/services/instance/instancehelpers"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/types"
 )
 
@@ -60,6 +61,14 @@ func ResourceVolume() *schema.Resource {
 				ExactlyOneOf:     []string{"size_in_gb"},
 				DiffSuppressFunc: dsf.Locality,
 			},
+			"instance_volume_id": {
+				Type:          schema.TypeString,
+				Computed:      true,
+				Optional:      true,
+				Description:   "The instance volume to create the block volume from",
+				ForceNew:      true,
+				ConflictsWith: []string{"snapshot_id"},
+			},
 			"tags": {
 				Type: schema.TypeList,
 				Elem: &schema.Schema{
@@ -78,44 +87,55 @@ func ResourceVolume() *schema.Resource {
 }
 
 func ResourceBlockVolumeCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	api, zone, err := blockAPIWithZone(d, m)
+	api, zone, err := instancehelpers.InstanceAndBlockAPIWithZone(d, m)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	req := &block.CreateVolumeRequest{
-		Zone:      zone,
-		Name:      types.ExpandOrGenerateString(d.Get("name").(string), "volume"),
-		ProjectID: d.Get("project_id").(string),
-		Tags:      types.ExpandStrings(d.Get("tags")),
-		PerfIops:  types.ExpandUint32Ptr(d.Get("iops")),
-	}
+	var volume *block.Volume
+	instanceVolumeID, migrateVolume := d.GetOk("instance_volume_id")
 
-	if iops, ok := d.GetOk("iops"); ok {
-		req.PerfIops = types.ExpandUint32Ptr(iops)
-	}
-
-	if size, ok := d.GetOk("size_in_gb"); ok {
-		volumeSizeInBytes := scw.Size(size.(int)) * scw.GB
-		req.FromEmpty = &block.CreateVolumeRequestFromEmpty{
-			Size: volumeSizeInBytes,
+	if migrateVolume {
+		volume, err = migrateInstanceToBlockVolume(ctx, api, zone, locality.ExpandID(instanceVolumeID.(string)), d.Timeout(schema.TimeoutCreate))
+		if err != nil {
+			return diag.FromErr(err)
 		}
-	}
 
-	if snapshotID, ok := d.GetOk("snapshot_id"); ok {
-		req.FromSnapshot = &block.CreateVolumeRequestFromSnapshot{
-			SnapshotID: locality.ExpandID(snapshotID.(string)),
+	} else {
+		req := &block.CreateVolumeRequest{
+			Zone:      zone,
+			Name:      types.ExpandOrGenerateString(d.Get("name").(string), "volume"),
+			ProjectID: d.Get("project_id").(string),
+			Tags:      types.ExpandStrings(d.Get("tags")),
+			PerfIops:  types.ExpandUint32Ptr(d.Get("iops")),
 		}
-	}
 
-	volume, err := api.CreateVolume(req, scw.WithContext(ctx))
-	if err != nil {
-		return diag.FromErr(err)
+		if iops, ok := d.GetOk("iops"); ok {
+			req.PerfIops = types.ExpandUint32Ptr(iops)
+		}
+
+		if size, ok := d.GetOk("size_in_gb"); ok {
+			volumeSizeInBytes := scw.Size(size.(int)) * scw.GB
+			req.FromEmpty = &block.CreateVolumeRequestFromEmpty{
+				Size: volumeSizeInBytes,
+			}
+		}
+
+		if snapshotID, ok := d.GetOk("snapshot_id"); ok {
+			req.FromSnapshot = &block.CreateVolumeRequestFromSnapshot{
+				SnapshotID: locality.ExpandID(snapshotID.(string)),
+			}
+		}
+
+		volume, err = api.BlockAPI.CreateVolume(req, scw.WithContext(ctx))
+		if err != nil {
+			return diag.FromErr(err)
+		}
 	}
 
 	d.SetId(zonal.NewIDString(zone, volume.ID))
 
-	_, err = waitForBlockVolume(ctx, api, zone, volume.ID, d.Timeout(schema.TimeoutCreate))
+	_, err = waitForBlockVolume(ctx, api.BlockAPI, zone, volume.ID, d.Timeout(schema.TimeoutCreate))
 	if err != nil {
 		return diag.FromErr(err)
 	}
