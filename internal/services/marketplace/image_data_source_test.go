7a229e6fe6862ad4acdd0afb5bae8c342f04b2da
Jules Castéran <jcasteran@scaleway.com>
2025-02-27 15:16:52 +0100

    feat(marketplace_image): add image_type filter (#2943)
    * feat(marketplace_image): add image_type filter
    
    * add local image type validation

--- internal/services/marketplace/image_data_source_test.go
+++ internal/services/marketplace/image_data_source_test.go
@@ -24,6 +24,31 @@ func TestAccDataSourceMarketplaceImage_Basic(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					instancechecks.DoesImageExists(tt, "data.scaleway_marketplace_image.test1"),
 					resource.TestCheckResourceAttr("data.scaleway_marketplace_image.test1", "label", "ubuntu_focal"),
+					resource.TestCheckResourceAttr("data.scaleway_marketplace_image.test1", "image_type", "instance_local"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceMarketplaceImage_SBS(t *testing.T) {
+	tt := acctest.NewTestTools(t)
+	defer tt.Cleanup()
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.PreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					data "scaleway_marketplace_image" "test1" {
+						label = "ubuntu_focal"
+						image_type = "instance_sbs"
+					}
+					`,
+				Check: resource.ComposeTestCheckFunc(
+					instancechecks.DoesImageExists(tt, "data.scaleway_marketplace_image.test1"),
+					resource.TestCheckResourceAttr("data.scaleway_marketplace_image.test1", "label", "ubuntu_focal"),
+					resource.TestCheckResourceAttr("data.scaleway_marketplace_image.test1", "image_type", "instance_sbs"),
 				),
 			},
 		},
