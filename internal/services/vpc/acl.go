5f1aabc7b6923ebaef2143dd1fbffc3cf73e61d5
Yacine FODIL <yfodil@scaleway.com>
2025-03-24 17:14:49 +0100

    add vpc acls

---
+++ internal/services/vpc/acl.go
@@ -0,0 +1,211 @@
+package vpc
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/scaleway/scaleway-sdk-go/api/vpc/v2"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/httperrors"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/locality"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/locality/regional"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/verify"
+)
+
+func ResourceACL() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: ResourceVPCACLCreate,
+		ReadContext:   ResourceVPCACLRead,
+		UpdateContext: ResourceVPCACLUpdate,
+		DeleteContext: ResourceVPCACLDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		SchemaVersion: 0,
+		Schema: map[string]*schema.Schema{
+			"vpc_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The VPC in which to create the ACL rule",
+			},
+			"default_policy": {
+				Type:             schema.TypeString,
+				Required:         true,
+				Description:      "The action to take for packets which do not match any rules",
+				ValidateDiagFunc: verify.ValidateEnum[vpc.Action](),
+			},
+			"is_ipv6": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "Defines whether this set of ACL rules is for IPv6 (false = IPv4). Each Network ACL can have rules for only one IP type",
+			},
+			"rules": {
+				Type:        schema.TypeList,
+				Required:    true,
+				Description: "The list of Network ACL rules",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"protocol": {
+							Type:             schema.TypeString,
+							Optional:         true,
+							Default:          "ANY",
+							Description:      "The protocol to which this rule applies. Default value: ANY",
+							ValidateDiagFunc: verify.ValidateEnum[vpc.ACLRuleProtocol](),
+						},
+						"source": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "Source IP range to which this rule applies (CIDR notation with subnet mask)",
+						},
+						"src_port_low": {
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "Starting port of the source port range to which this rule applies (inclusive)",
+						},
+						"src_port_high": {
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "Ending port of the source port range to which this rule applies (inclusive)",
+						},
+						"destination": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "Destination IP range to which this rule applies (CIDR notation with subnet mask)",
+						},
+						"dst_port_low": {
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "Starting port of the destination port range to which this rule applies (inclusive)",
+						},
+						"dst_port_high": {
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "Ending port of the destination port range to which this rule applies (inclusive)",
+						},
+						"action": {
+							Type:             schema.TypeString,
+							Optional:         true,
+							Description:      "The policy to apply to the packet",
+							ValidateDiagFunc: verify.ValidateEnum[vpc.Action](),
+						},
+						"description": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "The rule description",
+						},
+					},
+				},
+			},
+			"region": regional.Schema(),
+		},
+	}
+}
+
+func ResourceVPCACLCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	vpcAPI, region, err := vpcAPIWithRegion(d, m)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	req := &vpc.SetACLRequest{
+		VpcID:         locality.ExpandID(d.Get("vpc_id").(string)),
+		IsIPv6:        d.Get("is_ipv6").(bool),
+		DefaultPolicy: vpc.Action(d.Get("default_policy").(string)),
+		Region:        region,
+	}
+
+	expandedRules, err := expandACLRules(d.Get("rules"))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if d.Get("rules") != nil {
+		req.Rules = expandedRules
+	}
+
+	_, err = vpcAPI.SetACL(req, scw.WithContext(ctx))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(regional.NewIDString(region, regional.ExpandID(d.Get("vpc_id").(string)).ID))
+
+	return ResourceVPCACLRead(ctx, d, m)
+}
+
+func ResourceVPCACLRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	vpcAPI, region, ID, err := NewAPIWithRegionAndID(m, d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	acl, err := vpcAPI.GetACL(&vpc.GetACLRequest{
+		VpcID:  locality.ExpandID(ID),
+		Region: region,
+		IsIPv6: d.Get("is_ipv6").(bool),
+	}, scw.WithContext(ctx))
+	if err != nil {
+		if httperrors.Is404(err) {
+			d.SetId("")
+
+			return nil
+		}
+
+		return diag.FromErr(err)
+	}
+
+	_ = d.Set("rules", flattenACLRules(acl.Rules))
+	_ = d.Set("default_policy", acl.DefaultPolicy.String())
+
+	return nil
+}
+
+func ResourceVPCACLUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	vpcAPI, region, ID, err := NewAPIWithRegionAndID(m, d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	req := &vpc.SetACLRequest{
+		VpcID:         locality.ExpandID(ID),
+		IsIPv6:        d.Get("is_ipv6").(bool),
+		DefaultPolicy: vpc.Action(d.Get("default_policy").(string)),
+		Region:        region,
+	}
+
+	expandedRules, err := expandACLRules(d.Get("rules"))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if d.Get("rules") != nil {
+		req.Rules = expandedRules
+	}
+
+	_, err = vpcAPI.SetACL(req, scw.WithContext(ctx))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return ResourceVPCACLRead(ctx, d, m)
+}
+
+func ResourceVPCACLDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	vpcAPI, region, ID, err := NewAPIWithRegionAndID(m, d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	_, err = vpcAPI.SetACL(&vpc.SetACLRequest{
+		VpcID:         locality.ExpandID(ID),
+		Region:        region,
+		DefaultPolicy: "drop",
+	}, scw.WithContext(ctx))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
---
+++ internal/services/vpc/acl_test.go
@@ -0,0 +1,180 @@
+package vpc_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	vpcSDK "github.com/scaleway/scaleway-sdk-go/api/vpc/v2"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/acctest"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/httperrors"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/services/vpc"
+)
+
+func TestAccACL_Basic(t *testing.T) {
+	tt := acctest.NewTestTools(t)
+	defer tt.Cleanup()
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.PreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      isACLDestroyed(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					resource "scaleway_vpc" "vpc01" {
+					  name = "tf-vpc-acl"
+					}
+					
+					resource "scaleway_vpc_acl" "acl01" {
+					  vpc_id   = scaleway_vpc.vpc01.id
+					  is_ipv6  = false
+					  rules {
+						protocol      = "TCP"
+						src_port_low  = 0
+						src_port_high = 0
+						dst_port_low  = 80
+						dst_port_high = 80
+						source        = "0.0.0.0/0"
+						destination   = "0.0.0.0/0"
+						description   = "Allow HTTP traffic from any source"
+						action        = "accept"
+					  }
+					  default_policy = "drop"
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					isACLPresent(tt, "scaleway_vpc_acl.acl01"),
+					resource.TestCheckResourceAttrPair("scaleway_vpc_acl.acl01", "vpc_id", "scaleway_vpc.vpc01", "id"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "is_ipv6", "false"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "default_policy", "drop"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.#", "1"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.protocol", "TCP"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.src_port_low", "0"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.src_port_high", "0"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.dst_port_low", "80"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.dst_port_high", "80"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.source", "0.0.0.0/0"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.destination", "0.0.0.0/0"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.description", "Allow HTTP traffic from any source"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.action", "accept"),
+				),
+			},
+			{
+				Config: `
+					resource "scaleway_vpc" "vpc01" {
+					  name = "tf-vpc-acl"
+					}
+					
+					resource "scaleway_vpc_acl" "acl01" {
+					  vpc_id   = scaleway_vpc.vpc01.id
+					  is_ipv6  = false
+					  rules {
+						protocol      = "TCP"
+						src_port_low  = 0
+						src_port_high = 0
+						dst_port_low  = 80
+						dst_port_high = 80
+						source        = "0.0.0.0/0"
+						destination   = "0.0.0.0/0"
+						description   = "Allow HTTP traffic from any source"
+						action        = "accept"
+					  }
+					  rules {
+						protocol      = "TCP"
+						src_port_low  = 0
+						src_port_high = 0
+						dst_port_low  = 443
+						dst_port_high = 443
+						source        = "0.0.0.0/0"
+						destination   = "0.0.0.0/0"
+						description   = "Allow HTTPS traffic from any source"
+						action        = "accept"
+					  }
+					  default_policy = "drop"
+					}
+					
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					isACLPresent(tt, "scaleway_vpc_acl.acl01"),
+					resource.TestCheckResourceAttrPair("scaleway_vpc_acl.acl01", "vpc_id", "scaleway_vpc.vpc01", "id"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "is_ipv6", "false"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "default_policy", "drop"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.#", "2"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.protocol", "TCP"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.src_port_low", "0"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.src_port_high", "0"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.dst_port_low", "80"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.dst_port_high", "80"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.source", "0.0.0.0/0"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.destination", "0.0.0.0/0"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.description", "Allow HTTP traffic from any source"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.0.action", "accept"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.1.protocol", "TCP"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.1.src_port_low", "0"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.1.src_port_high", "0"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.1.dst_port_low", "443"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.1.dst_port_high", "443"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.1.source", "0.0.0.0/0"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.1.destination", "0.0.0.0/0"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.1.description", "Allow HTTPS traffic from any source"),
+					resource.TestCheckResourceAttr("scaleway_vpc_acl.acl01", "rules.1.action", "accept"),
+				),
+			},
+		},
+	})
+}
+
+func isACLPresent(tt *acctest.TestTools, n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", n)
+		}
+
+		vpcAPI, region, ID, err := vpc.NewAPIWithRegionAndID(tt.Meta, rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		_, err = vpcAPI.GetACL(&vpcSDK.GetACLRequest{
+			VpcID:  ID,
+			Region: region,
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func isACLDestroyed(tt *acctest.TestTools) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		for _, rs := range state.RootModule().Resources {
+			if rs.Type != "scaleway_vpc_acl" {
+				continue
+			}
+
+			vpcAPI, region, ID, err := vpc.NewAPIWithRegionAndID(tt.Meta, rs.Primary.ID)
+			if err != nil {
+				return err
+			}
+
+			_, err = vpcAPI.GetACL(&vpcSDK.GetACLRequest{
+				VpcID:  ID,
+				Region: region,
+			})
+
+			if err == nil {
+				return fmt.Errorf("acl (%s) still exists", rs.Primary.ID)
+			}
+
+			if !httperrors.Is404(err) {
+				return err
+			}
+		}
+
+		return nil
+	}
+}
