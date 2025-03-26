f8a8259120a6f8741d925b1822f293e0d09668c6
Jonathan R. <jremy@scaleway.com>
2025-03-26 11:34:38 +0100

    feat(tem): add support for blockedlist (#2983)
    * feat(tem): add support for blockedlist
    
    * add test
    
    * fix test
    
    * fix test blockedlist

---
+++ internal/services/tem/blockedlist_test.go
@@ -0,0 +1,133 @@
+package tem_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	temSDK "github.com/scaleway/scaleway-sdk-go/api/tem/v1alpha1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/acctest"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/services/tem"
+)
+
+func TestAccBlockedList_Basic(t *testing.T) {
+	tt := acctest.NewTestTools(t)
+	defer tt.Cleanup()
+
+	subDomainName := "test-blockedlist"
+
+	blockedEmail := "spam@example.com"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.PreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      isBlockedEmailDestroyed(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+
+				resource "scaleway_domain_zone" "test" {
+  						domain    = "%s"
+  						subdomain = "%s"
+					}
+
+					resource scaleway_tem_domain cr01 {
+						name       = scaleway_domain_zone.test.id
+						accept_tos = true
+						autoconfig = true
+					}
+
+					resource scaleway_tem_domain_validation valid {
+  						domain_id = scaleway_tem_domain.cr01.id
+  						region = scaleway_tem_domain.cr01.region
+						timeout = 3600
+					}
+
+					resource "scaleway_tem_blocked_list" "test" {
+						domain_id  = scaleway_tem_domain.cr01.id
+						email      = "%s"
+						type       = "mailbox_full"
+						reason     = "Spam detected"
+						region     = "fr-par"
+ 						depends_on = [
+    						scaleway_tem_domain_validation.valid
+  						]
+					}
+				`, domainNameValidation, subDomainName, blockedEmail),
+				Check: resource.ComposeTestCheckFunc(
+					isBlockedEmailPresent(tt, "scaleway_tem_blocked_list.test"),
+					resource.TestCheckResourceAttr("scaleway_tem_blocked_list.test", "email", blockedEmail),
+					resource.TestCheckResourceAttr("scaleway_tem_blocked_list.test", "type", "mailbox_full"),
+					resource.TestCheckResourceAttr("scaleway_tem_blocked_list.test", "reason", "Spam detected"),
+					acctest.CheckResourceAttrUUID("scaleway_tem_blocked_list.test", "id"),
+				),
+			},
+		},
+	})
+}
+
+func isBlockedEmailPresent(tt *acctest.TestTools, n string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		rs, ok := state.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", n)
+		}
+
+		api, region, domainID, err := tem.NewAPIWithRegionAndID(tt.Meta, rs.Primary.Attributes["domain_id"])
+		if err != nil {
+			return err
+		}
+
+		blockedEmail := rs.Primary.Attributes["email"]
+
+		blocklists, err := api.ListBlocklists(&temSDK.ListBlocklistsRequest{
+			Region:   region,
+			DomainID: domainID,
+			Email:    scw.StringPtr(blockedEmail),
+		}, scw.WithContext(context.Background()))
+		if err != nil {
+			return err
+		}
+
+		if len(blocklists.Blocklists) == 0 {
+			return fmt.Errorf("blocked email %s not found in blocklist", blockedEmail)
+		}
+
+		return nil
+	}
+}
+
+func isBlockedEmailDestroyed(tt *acctest.TestTools) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		for _, rs := range state.RootModule().Resources {
+			if rs.Type != "scaleway_tem_blocked_list" {
+				continue
+			}
+
+			api, region, domainID, err := tem.NewAPIWithRegionAndID(tt.Meta, rs.Primary.Attributes["domain_id"])
+			if err != nil {
+				return err
+			}
+
+			blockedEmail := rs.Primary.Attributes["email"]
+
+			blocklists, err := api.ListBlocklists(&temSDK.ListBlocklistsRequest{
+				Region:   region,
+				DomainID: domainID,
+				Email:    scw.StringPtr(blockedEmail),
+			}, scw.WithContext(context.Background()))
+			if err != nil {
+				return err
+			}
+
+			if len(blocklists.Blocklists) > 0 {
+				return fmt.Errorf("blocked email %s still present after deletion", blockedEmail)
+			}
+		}
+
+		return nil
+	}
+}
