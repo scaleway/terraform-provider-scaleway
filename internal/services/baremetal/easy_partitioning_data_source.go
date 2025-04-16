7b6a41cb2d5f9f6ea72aec58a35f069a3859f6bb
Laure-di <lmasson@scaleway.com>
2025-04-16 10:31:36 +0200

    tests and golangci

--- internal/services/baremetal/easy_partitioning_data_source.go
+++ internal/services/baremetal/easy_partitioning_data_source.go
@@ -3,14 +3,16 @@ package baremetal
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/scaleway/scaleway-sdk-go/api/baremetal/v1"
 	"github.com/scaleway/scaleway-sdk-go/scw"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/dsf"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/locality/zonal"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/verify"
-	"strings"
 )
 
 func DataEasyPartitioning() *schema.Resource {
@@ -41,7 +43,7 @@ func DataEasyPartitioning() *schema.Resource {
 				Default:     true,
 				Description: "set extra ext_4 partition",
 			},
-			"ext_4_mountpoint": { //TODO change to mount point
+			"ext_4_mountpoint": { // TODO change to mount point
 				Type:        schema.TypeString,
 				Optional:    true,
 				Default:     "/hello",
@@ -56,129 +58,45 @@ func DataEasyPartitioning() *schema.Resource {
 	}
 }
 
-func removeSwap(originalDisks []*baremetal.SchemaDisk, withExtraPartition bool) []*baremetal.SchemaDisk {
-	var result []*baremetal.SchemaDisk
-
-	for _, disk := range originalDisks {
-		i := 1
-		newPartitions := []*baremetal.SchemaPartition{}
-		for _, p := range disk.Partitions {
-			if p.Label == "swap" {
-				continue
-			}
-			if p.Label == "root" {
-				if !withExtraPartition {
-					p.Size = 0
-					p.UseAllAvailableSpace = true
-				} else {
-					p.Size = 20000000000
-				}
-			}
-			p.Number = uint32(i)
-			i++
-			newPartitions = append(newPartitions, p)
-		}
-		result = append(result, &baremetal.SchemaDisk{
-			Device:     disk.Device,
-			Partitions: newPartitions,
-		})
-	}
-	return result
-}
-
-func addExtraPartition(mountpoint string, newDisksSchema []*baremetal.SchemaDisk, defaultPartitionSchema *baremetal.Schema) *baremetal.Schema {
-	raidDevices := []string{}
-
-	for _, disk := range newDisksSchema {
-		partIndex := uint32(len(disk.Partitions)) + 1
-		deviceIndex := partIndex + 1
-		data := &baremetal.SchemaPartition{
-			Label:                baremetal.SchemaPartitionLabel("data"),
-			Number:               partIndex,
-			Size:                 0,
-			UseAllAvailableSpace: true,
-		}
-		disk.Partitions = append(disk.Partitions, data)
-
-		device := fmt.Sprintf("%sp%d", disk.Device, deviceIndex)
-		raidDevices = append(raidDevices, device)
-		deviceIndex--
-	}
-	defaultPartitionSchema.Disks = newDisksSchema
-
-	filesystem := &baremetal.SchemaFilesystem{
-		Device:     "/dev/md2",
-		Format:     "ext4",
-		Mountpoint: mountpoint,
-	}
-	defaultPartitionSchema.Filesystems = append(defaultPartitionSchema.Filesystems, filesystem)
-
-	//raid := &baremetal.SchemaRAID{
-	//	Name:    "/dev/md2",
-	//	Level:   baremetal.SchemaRAIDLevelRaidLevel1,
-	//	Devices: raidDevices,
-	//}
-	//defaultPartitionSchema.Raids = append(defaultPartitionSchema.Raids, raid)
-
-	return defaultPartitionSchema
-}
-
-func manageRootSize(originalDisks []*baremetal.SchemaDisk, withSwap bool, withExtraPartition bool) {
-	for _, disk := range originalDisks {
-		for _, partition := range disk.Partitions {
-			if partition.Label == "root" {
-				if !withSwap && !withExtraPartition {
-					partition.Size = 0
-					partition.UseAllAvailableSpace = true
-				}
-				if withExtraPartition {
-					partition.Size = 20000000000
-				}
-			}
-		}
-	}
-}
-
 func dataEasyPartitioningRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	api, fallBackZone, err := newAPIWithZone(d, m)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	osID := d.Get("os_id").(string)
+	osID := zonal.ExpandID(d.Get("os_id").(string))
 
 	os, err := api.GetOS(&baremetal.GetOSRequest{
 		Zone: fallBackZone,
-		OsID: osID,
+		OsID: osID.ID,
 	}, scw.WithContext(ctx))
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
 	if !os.CustomPartitioningSupported {
-		return diag.FromErr(fmt.Errorf("custom partitioning is not supported with this OS"))
+		return diag.FromErr(errors.New("custom partitioning is not supported with this OS"))
 	}
 
-	offerID := d.Get("offer_id").(string)
+	offerID := zonal.ExpandID(d.Get("offer_id").(string))
 
 	offer, err := api.GetOffer(&baremetal.GetOfferRequest{
 		Zone:    fallBackZone,
-		OfferID: offerID,
+		OfferID: offerID.ID,
 	}, scw.WithContext(ctx))
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
 	if !isOSCompatible(offer, os) {
-		return diag.FromErr(fmt.Errorf("os and offer are not compatible"))
+		return diag.FromErr(errors.New("OS and offer are not compatible"))
 	}
 
 	defaultPartitioningSchema, err := api.GetDefaultPartitioningSchema(&baremetal.GetDefaultPartitioningSchemaRequest{
 		Zone:    fallBackZone,
-		OfferID: offerID,
-		OsID:    osID,
+		OfferID: offerID.ID,
+		OsID:    osID.ID,
 	}, scw.WithContext(ctx))
-
 	if err != nil {
 		return diag.FromErr(err)
 	}
@@ -187,12 +105,13 @@ func dataEasyPartitioningRead(ctx context.Context, d *schema.ResourceData, m int
 	swap := d.Get("swap").(bool)
 
 	if swap && !extraPart {
-		jsonSchema, _ := json.Marshal(defaultPartitioningSchema)
+		jsonSchema, err := json.Marshal(defaultPartitioningSchema)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
 		d.SetId(fmt.Sprintf("%s-%s", offerID, osID))
 		_ = d.Set("json_partition", string(jsonSchema))
-		//_ = d.Set("disks", flattenDisksSchema(defaultPartitioningSchema.Disks))
-		//_ = d.Set("raids", flattenRaids(defaultPartitioningSchema.Raids))
-		//_ = d.Set("filesystems", flattenFilesystems(defaultPartitioningSchema.Filesystems))
 
 		return nil
 	}
@@ -207,6 +126,7 @@ func dataEasyPartitioningRead(ctx context.Context, d *schema.ResourceData, m int
 	}
 
 	var newCustomPartition *baremetal.Schema
+
 	if extraPart {
 		mountpoint := d.Get("ext_4_mountpoint").(string)
 		newCustomPartition = addExtraPartition(mountpoint, newDiskSchema, defaultPartitioningSchema)
@@ -216,11 +136,10 @@ func dataEasyPartitioningRead(ctx context.Context, d *schema.ResourceData, m int
 
 	err = api.ValidatePartitioningSchema(&baremetal.ValidatePartitioningSchemaRequest{
 		Zone:               fallBackZone,
-		OfferID:            offerID,
-		OsID:               osID,
+		OfferID:            offerID.ID,
+		OsID:               osID.ID,
 		PartitioningSchema: defaultPartitioningSchema,
 	})
-
 	if err != nil {
 		return diag.FromErr(err)
 	}
@@ -231,12 +150,91 @@ func dataEasyPartitioningRead(ctx context.Context, d *schema.ResourceData, m int
 	}
 
 	d.SetId(fmt.Sprintf("%s-%s", offerID, osID))
+
 	jsonSchemaStr := string(jsonSchema)
-	strings.ReplaceAll(jsonSchemaStr, "\"", "\\\"")
+
 	_ = d.Set("json_partition", jsonSchemaStr)
-	//_ = d.Set("disks", flattenDisksSchema(newCustomPartition.Disks))
-	//_ = d.Set("raids", flattenRaids(newCustomPartition.Raids))
-	//_ = d.Set("filesystems", flattenFilesystems(newCustomPartition.Filesystems))
 
 	return nil
+}
+
+func removeSwap(originalDisks []*baremetal.SchemaDisk, withExtraPartition bool) []*baremetal.SchemaDisk {
+	lenOfDisks := len(originalDisks)
+	if !withExtraPartition {
+		lenOfDisks = len(originalDisks) - 1
+	}
+
+	result := make([]*baremetal.SchemaDisk, 0, lenOfDisks)
+
+	for _, disk := range originalDisks {
+		i := 1
+		newPartitions := []*baremetal.SchemaPartition{}
+
+		for _, p := range disk.Partitions {
+			if p.Label == "swap" {
+				continue
+			}
+
+			if p.Label == "root" {
+				if !withExtraPartition {
+					p.Size = 0
+					p.UseAllAvailableSpace = true
+				} else {
+					p.Size = 20000000000
+				}
+			}
+
+			p.Number = uint32(i)
+			i++
+
+			newPartitions = append(newPartitions, p)
+		}
+
+		result = append(result, &baremetal.SchemaDisk{
+			Device:     disk.Device,
+			Partitions: newPartitions,
+		})
+	}
+
+	return result
+}
+
+func addExtraPartition(mountpoint string, newDisksSchema []*baremetal.SchemaDisk, defaultPartitionSchema *baremetal.Schema) *baremetal.Schema {
+	for _, disk := range newDisksSchema {
+		partIndex := uint32(len(disk.Partitions)) + 1
+		data := &baremetal.SchemaPartition{
+			Label:                baremetal.SchemaPartitionLabel("data"),
+			Number:               partIndex,
+			Size:                 0,
+			UseAllAvailableSpace: true,
+		}
+		disk.Partitions = append(disk.Partitions, data)
+	}
+
+	defaultPartitionSchema.Disks = newDisksSchema
+	filesystem := &baremetal.SchemaFilesystem{
+		Device:     "/dev/md2",
+		Format:     "ext4",
+		Mountpoint: mountpoint,
+	}
+	defaultPartitionSchema.Filesystems = append(defaultPartitionSchema.Filesystems, filesystem)
+
+	return defaultPartitionSchema
+}
+
+func manageRootSize(originalDisks []*baremetal.SchemaDisk, withSwap bool, withExtraPartition bool) {
+	for _, disk := range originalDisks {
+		for _, partition := range disk.Partitions {
+			if partition.Label == "root" {
+				if !withSwap && !withExtraPartition {
+					partition.Size = 0
+					partition.UseAllAvailableSpace = true
+				}
+
+				if withExtraPartition {
+					partition.Size = 20000000000
+				}
+			}
+		}
+	}
 }
--- internal/services/baremetal/easy_partitioning_source_test.go
+++ internal/services/baremetal/easy_partitioning_source_test.go
@@ -3,68 +3,80 @@ package baremetal_test
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	baremetalSDK "github.com/scaleway/scaleway-sdk-go/api/baremetal/v1"
-	"github.com/scaleway/terraform-provider-scaleway/v2/internal/services/baremetal"
 	"reflect"
 	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	baremetalSDK "github.com/scaleway/scaleway-sdk-go/api/baremetal/v1"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/acctest"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/services/baremetal"
 )
 
 const (
-	offerIDPartitioning = "a60ae97c-268c-40cb-af5f-dd276e917ed7"
-	osID                = "7d1914e1-f4ab-47fc-bd8c-b3a23143e87a"
-	incompatibleOsID    = "4aff4d9d-b1f4-44b0-ab6f-e4711ac11711"
-	incompatibleOfferId = "a204136d-656b-44b7-9735-88ca2f62cb1f"
+	offerNameEasyPartitioning            = "EM-B220E-NVME"
+	incompatibleOfferName                = "EM-L110X-SATA"
+	AlternativeOfferNameEasyPartitioning = "EM-B420E-NVME"
+	mountpoint                           = "/hello"
 )
 
-func TestAccDataSourceEasyParitioning_Basic(t *testing.T) {
+func TestAccEasyPartitioningDataSource_Basic(t *testing.T) {
 	tt := acctest.NewTestTools(t)
 	defer tt.Cleanup()
 
-	//mountpoint := "/hello"
-	SSHKeyName := "TestAccServer_Basic"
-	name := "TestAccServer_Basic"
+	sshKeyName := "TestAccEasyPartitioningDataSource_Basic"
+	serverName := "TestAccEasyPartitioningDataSource_Basic"
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { acctest.PreCheck(t) },
 		ProviderFactories: tt.ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: fmt.Sprintf(
-					`
+				Config: fmt.Sprintf(`
+					data "scaleway_baremetal_offer" "my_offer" {
+						zone = "%s"
+						name = "%s"
+					}
+
 					data "scaleway_baremetal_os" "my_os" {
-					  zone    = "%s"
-					  name    = "Ubuntu"
-					  version = "22.04 LTS (Jammy Jellyfish)"
+						zone    = "%s"
+						name    = "Ubuntu"
+						version = "22.04 LTS (Jammy Jellyfish)"
 					}
 
 					resource "scaleway_iam_ssh_key" "main" {
-						name 	   = "%s"
+						name       = "%s"
 						public_key = "%s"
 					}
 
 					data "scaleway_baremetal_easy_partitioning" "test" {
-						offer_id = "%s"
-						os_id = "%s"
-						swap = false
-						ext_4_mountpoint = "/hello"
+						offer_id          = data.scaleway_baremetal_offer.my_offer.offer_id
+						os_id             = data.scaleway_baremetal_os.my_os.os_id
+						swap              = false
+						ext_4_mountpoint  = "%s"
 					}
-					
+
 					resource "scaleway_baremetal_server" "base" {
-						name        = "%s"
-						zone        = "%s"
-						description = "test a description"
-						offer       = "%s"
-						os          = data.scaleway_baremetal_os.my_os.os_id
+						name         = "%s"
+						zone         = "%s"
+						description  = "test a description"
+						offer        = data.scaleway_baremetal_offer.my_offer.offer_id
+						os           = data.scaleway_baremetal_os.my_os.os_id
 						partitioning = data.scaleway_baremetal_easy_partitioning.test.json_partition
-					
-						tags = [ "terraform-test", "scaleway_baremetal_server", "minimal", "edited" ]
-						ssh_key_ids = [ scaleway_iam_ssh_key.main.id ]
-					}`, Zone, SSHKeyName, SSHKeyBaremetal, offerIDPartitioning, osID, name, Zone, offerIDPartitioning),
+						tags         = ["terraform-test", "scaleway_baremetal_server", "minimal", "edited"]
+						ssh_key_ids  = [scaleway_iam_ssh_key.main.id]
+					}
+				`,
+					Zone,
+					offerNameEasyPartitioning,
+					Zone,
+					sshKeyName,
+					SSHKeyBaremetal,
+					mountpoint,
+					serverName,
+					Zone,
+				),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckBaremetalServerExists(tt, "scaleway_baremetal_server.base"),
 					testAccCheckEasyPartitioning(tt, "scaleway_baremetal_server.base", "data.scaleway_baremetal_easy_partitioning.test"),
@@ -74,13 +86,12 @@ func TestAccDataSourceEasyParitioning_Basic(t *testing.T) {
 	})
 }
 
-func TestAccDataSourceEasyParitioning_NotCompatibleOS(t *testing.T) {
+func TestAccEasyPartitioningDataSource_WithAlternateOffer(t *testing.T) {
 	tt := acctest.NewTestTools(t)
 	defer tt.Cleanup()
 
-	//mountpoint := "/hello"
-	SSHKeyName := "TestAccServer_Basic"
-	name := "TestAccServer_Basic"
+	SSHKeyName := "TestAccEasyPartitioningDataSource_WithAlternateOffer"
+	name := "TestAccEasyPartitioningDataSource_WithAlternateOffer"
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { acctest.PreCheck(t) },
@@ -89,10 +100,15 @@ func TestAccDataSourceEasyParitioning_NotCompatibleOS(t *testing.T) {
 			{
 				Config: fmt.Sprintf(
 					`
+					data "scaleway_baremetal_offer" "my_offer" {
+						zone = "%s"
+						name = "%s"
+					}
+
 					data "scaleway_baremetal_os" "my_os" {
 					  zone    = "%s"
-					  name    = "Ubuntu"
-					  version = "22.04 LTS (Jammy Jellyfish)"
+					  name    = "Debian"
+					  version = "12 (Bookworm)"
 					}
 
 					resource "scaleway_iam_ssh_key" "main" {
@@ -101,36 +117,106 @@ func TestAccDataSourceEasyParitioning_NotCompatibleOS(t *testing.T) {
 					}
 
 					data "scaleway_baremetal_easy_partitioning" "test" {
-						offer_id = "%s"
-						os_id = "%s"
+						offer_id = data.scaleway_baremetal_offer.my_offer.offer_id
+						os_id = data.scaleway_baremetal_os.my_os.os_id
 						swap = false
-						ext_4_mountpoint = "/hello"
+						ext_4_mountpoint = "%s"
 					}
 					
 					resource "scaleway_baremetal_server" "base" {
 						name        = "%s"
 						zone        = "%s"
 						description = "test a description"
-						offer       = "%s"
+						offer       = data.scaleway_baremetal_offer.my_offer.offer_id
 						os          = data.scaleway_baremetal_os.my_os.os_id
 						partitioning = data.scaleway_baremetal_easy_partitioning.test.json_partition
 					
 						tags = [ "terraform-test", "scaleway_baremetal_server", "minimal", "edited" ]
 						ssh_key_ids = [ scaleway_iam_ssh_key.main.id ]
-					}`, Zone, SSHKeyName, SSHKeyBaremetal, offerIDPartitioning, incompatibleOsID, name, Zone, offerIDPartitioning),
+					}`,
+					Zone,
+					AlternativeOfferNameEasyPartitioning,
+					Zone,
+					SSHKeyName,
+					SSHKeyBaremetal,
+					mountpoint,
+					name,
+					Zone,
+				),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckBaremetalServerExists(tt, "scaleway_baremetal_server.base"),
+					testAccCheckEasyPartitioning(tt, "scaleway_baremetal_server.base", "data.scaleway_baremetal_easy_partitioning.test"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccEasyPartitioningDataSource_IncompatibleOS(t *testing.T) {
+	tt := acctest.NewTestTools(t)
+	defer tt.Cleanup()
+
+	SSHKeyName := "TestAccEasyPartitioningDataSource_IncompatibleOS"
+	name := "TestAccEasyPartitioningDataSource_IncompatibleOS"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.PreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(
+					`
+					data "scaleway_baremetal_offer" "my_offer" {
+						zone = "%s"
+						name = "%s"
+					}
+
+					data "scaleway_baremetal_os" "my_os" {
+					  zone    = "%s"
+					  name    = "Windows"
+					  version = "2022 64BITS standard"
+					}
+
+					resource "scaleway_iam_ssh_key" "main" {
+						name 	   = "%s"
+						public_key = "%s"
+					}
+
+					data "scaleway_baremetal_easy_partitioning" "test" {
+						offer_id = data.scaleway_baremetal_offer.my_offer.offer_id
+						os_id = data.scaleway_baremetal_os.my_os.os_id
+						swap = false
+						ext_4_mountpoint = "%s"
+					}
+					
+					resource "scaleway_baremetal_server" "base" {
+						name        = "%s"
+						zone        = "%s"
+						description = "test a description"
+						offer       = data.scaleway_baremetal_offer.my_offer.offer_id
+						os          = data.scaleway_baremetal_os.my_os.os_id
+						partitioning = data.scaleway_baremetal_easy_partitioning.test.json_partition
+					
+						tags = [ "terraform-test", "scaleway_baremetal_server", "minimal", "edited" ]
+						ssh_key_ids = [ scaleway_iam_ssh_key.main.id ]
+					}`,
+					Zone,
+					incompatibleOfferName,
+					Zone,
+					SSHKeyName,
+					SSHKeyBaremetal, mountpoint, name, Zone),
 				ExpectError: regexp.MustCompile("custom partitioning is not supported with this OS"),
 			},
 		},
 	})
 }
 
-func TestAccDataSourceEasyParitioning_NotCompatibleOffer(t *testing.T) {
+func TestAccEasyPartitioningDataSource_IncompatibleOffer(t *testing.T) {
 	tt := acctest.NewTestTools(t)
 	defer tt.Cleanup()
 
-	//mountpoint := "/hello"
-	SSHKeyName := "TestAccServer_Basic"
-	name := "TestAccServer_Basic"
+	SSHKeyName := "TestAccEasyPartitioningDataSource_IncompatibleOffer"
+	name := "TestAccEasyPartitioningDataSource_IncompatibleOffer"
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { acctest.PreCheck(t) },
@@ -139,6 +225,11 @@ func TestAccDataSourceEasyParitioning_NotCompatibleOffer(t *testing.T) {
 			{
 				Config: fmt.Sprintf(
 					`
+					data "scaleway_baremetal_offer" "my_offer" {
+						zone = "%s"
+						name = "%s"
+					}
+
 					data "scaleway_baremetal_os" "my_os" {
 					  zone    = "%s"
 					  name    = "Ubuntu"
@@ -151,24 +242,24 @@ func TestAccDataSourceEasyParitioning_NotCompatibleOffer(t *testing.T) {
 					}
 
 					data "scaleway_baremetal_easy_partitioning" "test" {
-						offer_id = "%s"
-						os_id = "%s"
+						offer_id = data.scaleway_baremetal_offer.my_offer.offer_id
+						os_id = data.scaleway_baremetal_os.my_os.os_id
 						swap = false
-						ext_4_mountpoint = "/hello"
+						ext_4_mountpoint = "%s"
 					}
 					
 					resource "scaleway_baremetal_server" "base" {
 						name        = "%s"
 						zone        = "%s"
 						description = "test a description"
-						offer       = "%s"
+						offer       = data.scaleway_baremetal_offer.my_offer.offer_id
 						os          = data.scaleway_baremetal_os.my_os.os_id
 						partitioning = data.scaleway_baremetal_easy_partitioning.test.json_partition
 					
 						tags = [ "terraform-test", "scaleway_baremetal_server", "minimal", "edited" ]
 						ssh_key_ids = [ scaleway_iam_ssh_key.main.id ]
-					}`, Zone, SSHKeyName, SSHKeyBaremetal, offerIDPartitioning, osID, name, Zone, offerIDPartitioning),
-				ExpectError: regexp.MustCompile(".*offer_id not compatible.*"),
+					}`, Zone, incompatibleOfferName, Zone, SSHKeyName, SSHKeyBaremetal, mountpoint, name, Zone),
+				ExpectError: regexp.MustCompile("Custom Partitioning not supported"),
 			},
 		},
 	})
@@ -209,6 +300,7 @@ func testAccCheckEasyPartitioning(tt *acctest.TestTools, serverName, dataSourceN
 		}
 
 		var expectedSchema baremetalSDK.Schema
+
 		err = json.Unmarshal([]byte(partitionJSON), &expectedSchema)
 		if err != nil {
 			return fmt.Errorf("failed to unmarshal partitionJSON: %w", err)
@@ -217,6 +309,7 @@ func testAccCheckEasyPartitioning(tt *acctest.TestTools, serverName, dataSourceN
 		if !reflect.DeepEqual(&expectedSchema, server.Install.PartitioningSchema) {
 			expectedStr, _ := json.MarshalIndent(expectedSchema, "", "  ")
 			actualStr, _ := json.MarshalIndent(server.Install.PartitioningSchema, "", "  ")
+
 			return fmt.Errorf("partitioning schema mismatch:\nExpected:\n%s\nActual:\n%s", expectedStr, actualStr)
 		}
 
