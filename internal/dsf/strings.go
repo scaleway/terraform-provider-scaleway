0083b228dbdcad9750e564521fb1fada61d8e75e
Guillaume Noale <gnoale@scaleway.com>
2025-04-17 13:41:28 +0200

    fix(containers): handle secret_environment_variable lifecycle

--- internal/dsf/strings.go
+++ internal/dsf/strings.go
@@ -3,6 +3,7 @@ package dsf
 import (
 	"strings"
 
+	"github.com/alexedwards/argon2id"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
@@ -12,4 +13,13 @@ func IgnoreCase(_, oldValue, newValue string, _ *schema.ResourceData) bool {
 
 func IgnoreCaseAndHyphen(_, oldValue, newValue string, _ *schema.ResourceData) bool {
 	return strings.ReplaceAll(strings.ToLower(oldValue), "-", "_") == strings.ReplaceAll(strings.ToLower(newValue), "-", "_")
+}
+
+func CompareArgon2idPasswordAndHash(_, oldValue, newValue string, _ *schema.ResourceData) bool {
+	match, err := argon2id.ComparePasswordAndHash(newValue, oldValue)
+	if err != nil {
+		return false
+	}
+
+	return match
 }
