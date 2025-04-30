4099af8b786c87a5370fd75cbd76960802442d48
Laure-di <lmasson@scaleway.com>
2025-04-30 09:23:49 +0200

    activate tests

--- internal/services/inference/custom_model_test.go
+++ internal/services/inference/custom_model_test.go
@@ -44,8 +44,6 @@ func TestAccCustomModel_Basic(t *testing.T) {
 }
 
 func TestAccCustomModel_DeployModelOnServer(t *testing.T) {
-	t.Skip("Can not run tests at the moment")
-
 	tt := acctest.NewTestTools(t)
 	defer tt.Cleanup()
 
