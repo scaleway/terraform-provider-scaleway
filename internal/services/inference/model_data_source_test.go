788b85c5f20e3159d1d61f802e59ff0ea40706da
Laure-di <62625835+Laure-di@users.noreply.github.com>
2025-05-15 13:58:55 +0200

    feat(inference): migration v1 and support BYOM (#3048)
    * feat(inference): migration deployment to v1
    
    * feat(inference): BYOM support
    
    * add sweeper
    
    * manage id from regional or not
    
    * remove error_message
    
    * fix linter
    
    * last cassette
    
    * fix documentation
    
    * fix documentation lint
    
    * remove comment
    
    * Update docs/resources/inference_custom_model.md
    
    Co-authored-by: Guillaume Noale <g.noale@gmail.com>
    
    * change model_id format
    
    * use of dsf.locality
    
    * ResourceCustomModelDelete return right err and testAccCheckCustomModelExists return err
    
    * fix(doc): add import part and fix typo
    
    * fix(doc): deployment required attribute
    
    * fix(inference): use of existing function cast
    
    * Update docs/resources/inference_custom_model.md
    
    Co-authored-by: Yacine Fodil <105779815+yfodil@users.noreply.github.com>
    
    * skip tests until further notice
    
    * activate tests
    
    * fix(inference): rename resource from custom_model to model
    
    * update sdk-go
    
    * remove unecessary file
    
    * fix(doc): put real URL and more context
    
    * add support model data-source
    
    * testing
    
    * add test
    
    * update doc and tests
    
    * fix linter
    
    * fix linter
    
    * remove custom reference
    
    * update cassette
    
    * update cassette deployment with datasource
    
    ---------
    
    Co-authored-by: Guillaume Noale <g.noale@gmail.com>
    Co-authored-by: Yacine Fodil <105779815+yfodil@users.noreply.github.com>
    Co-authored-by: Rémy Léone <rleone@scaleway.com>

---
+++ internal/services/inference/model_data_source_test.go
@@ -0,0 +1,78 @@
+package inference_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/acctest"
+	inferencetestfuncs "github.com/scaleway/terraform-provider-scaleway/v2/internal/services/inference/testfuncs"
+)
+
+func TestAccDataSourceModel_Basic(t *testing.T) {
+	tt := acctest.NewTestTools(t)
+	defer tt.Cleanup()
+
+	modelName := "mistral/pixtral-12b-2409:bf16"
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.PreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+				data "scaleway_inference_model" "my-model" {
+					name = "%s"
+				}
+
+`, modelName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckModelExists(tt, "data.scaleway_inference_model.my-model"),
+					resource.TestCheckResourceAttr("data.scaleway_inference_model.my-model", "name", modelName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceModel_Custom(t *testing.T) {
+	tt := acctest.NewTestTools(t)
+	defer tt.Cleanup()
+
+	modelName := "TestAccDataSourceModel_Custom"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acctest.PreCheck(t) },
+		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      inferencetestfuncs.IsModelDestroyed(tt),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					resource "scaleway_inference_model" "test" {
+						name = "%s"
+						url = "%s"
+					}
+				
+				`, modelName, modelURLCompatible),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckModelExists(tt, "scaleway_inference_model.test"),
+					resource.TestCheckResourceAttr("scaleway_inference_model.test", "name", modelName),
+				),
+			},
+			{
+				Config: fmt.Sprintf(`
+					resource "scaleway_inference_model" "test" {
+						name = "%s"
+						url = "%s"
+					}
+				
+					data "scaleway_inference_model" "my-model" {
+						name = "%s"
+					}`, modelName, modelURLCompatible, modelName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckModelExists(tt, "data.scaleway_inference_model.my-model"),
+					resource.TestCheckResourceAttr("data.scaleway_inference_model.my-model", "name", modelName),
+				),
+			},
+		},
+	})
+}
