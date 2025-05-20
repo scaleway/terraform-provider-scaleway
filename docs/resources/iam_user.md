e782e5c664e5773fd2a8d9134ad423edd01c66d6
Giacomo Sirri <91158410+giacomosirri@users.noreply.github.com>
2025-05-20 17:57:56 +0200

    feat(iam): creation of user as Member (#3029)
    * Distinguish between Member users and Guest users in user creation
    
    * Add all API parameters to resource schema
    
    * Complete implementation of user creation
    
    * Add fields to user read function
    
    * Fix schema of some parameters
    
    * Reimplement acceptance tests to test creation for both user types
    
    * Complete acceptance tests
    
    * Add comments
    
    * Final test runs: no errors
    
    * Update docs
    
    * Update CRUD operations for the User resource to reflect changes in the API
    
    * Fix typo in docs
    
    * Update acceptance tests
    
    * Add support for username update
    
    * Remove ForceNew from Email field definition
    
    * Update and run acceptance tests
    
    * Update docs
    
    * fix linting issues
    
    ---------
    
    Co-authored-by: Guillaume Noale <gnoale@scaleway.com>

--- docs/resources/iam_user.md
+++ docs/resources/iam_user.md
@@ -10,13 +10,28 @@ For more information, see the [API documentation](https://www.scaleway.com/en/de
 
 ## Example Usage
 
-### Basic
+### Guest user
 
 ```terraform
-resource "scaleway_iam_user" "basic" {
-  email = "test@test.com"
+resource "scaleway_iam_user" "guest" {
+  email = "foo@test.com"
+  tags  = ["test-tag"]
 }
 ```
+
+### Member user
+
+```terraform
+resource "scaleway_iam_user" "member" {
+  email      = "foo@test.com"
+  tags       = ["test-tag"]
+  username   = "foo"
+  first_name = "Foo"
+  last_name  = "Bar"
+}
+```
+
+When `username` is set, the user is created as a [Member](https://www.scaleway.com/en/docs/iam/concepts/#member). Otherwise, it is created as a [Guest](https://www.scaleway.com/en/docs/iam/concepts/#guest).
 
 ### Multiple users
 
@@ -28,7 +43,7 @@ locals {
   ])
 }
 
-resource scaleway_iam_user user {
+resource "scaleway_iam_user" "user" {
   for_each = local.users
   email    = each.key
 }
@@ -36,27 +51,44 @@ resource scaleway_iam_user user {
 
 ## Argument Reference
 
-- `email` - (Required) The email of the IAM user.
+- `organization_id` - (Defaults to [provider](../index.md#organization_id) `organization_id`) The ID of the organization the user is associated with.
+
+- `email` - (Required) The email of the IAM user. For Guest users, this argument is not editable.
 
 - `tags` - (Optional) The tags associated with the user.
 
-- `organization_id` - (Defaults to [provider](../index.md#organization_d) `organization_id`) The ID of the organization the user is associated with.
+- `username` - (Optional) The username of the IAM user. When it is set, the user is created as a Member. When it is not set, the user is created as a Guest and the username is set as equal to the email.
+
+- `password` - The password for first access.
+
+- `send_password_email` - Whether or not to send an email containing the password for first access.
+
+- `send_welcome_email` - Whether or not to send a welcome email that includes onboarding information.
+
+- `first_name` - The user's first name.
+
+- `last_name` - The user's last name.
+
+- `phone_number` - The user's phone number.
+
+- `locale` - The user's locale (e.g., en_US).
+
+Important: When creating a Guest user, all arguments are ignored, except for `organization_id`, `email` and `tags`.
 
 ## Attributes Reference
 
 In addition to all arguments above, the following attributes are exported:
 
 - `id` - The ID of the user (UUID format).
-- `email` - The email of the user
 - `created_at` - The date and time of the creation of the IAM user.
 - `updated_at` - The date and time of the last update of the IAM user.
 - `deletable` - Whether the IAM user is deletable.
-- `organization_id` - The ID of the organization the user.
 - `last_login_at` - The date of the last login.
 - `type` - The type of user. Check the possible values in the [API doc](https://www.scaleway.com/en/developers/api/iam/#path-users-get-a-given-user).
 - `status` - The status of user invitation. Check the possible values in the [API doc](https://www.scaleway.com/en/developers/api/iam/#path-users-get-a-given-user).
 - `mfa` - Whether the MFA is enabled.
 - `account_root_user_id` - The ID of the account root user associated with the user.
+- `locked` - Whether the user is locked.
 
 ## Import
 
--- internal/services/iam/user.go
+++ internal/services/iam/user.go
@@ -2,6 +2,7 @@ package iam
 
 import (
 	"context"
+	"errors"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -11,6 +12,8 @@ import (
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/services/account"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/types"
 )
+
+var descParameterIgnoredForGuest = " (this parameter is ignored in case of guest users)"
 
 func ResourceUser() *schema.Resource {
 	return &schema.Resource{
@@ -23,20 +26,62 @@ func ResourceUser() *schema.Resource {
 		},
 		SchemaVersion: 0,
 		Schema: map[string]*schema.Schema{
+			"organization_id": account.OrganizationIDOptionalSchema(),
+			// User input data
 			"email": {
 				Type:        schema.TypeString,
 				Required:    true,
-				ForceNew:    true,
-				Description: "The description of the iam user",
+				Description: "The email of the user, which is not editable for guests",
 			},
 			"tags": {
-				Type: schema.TypeList,
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
-				},
+				Type:        schema.TypeList,
+				Elem:        &schema.Schema{Type: schema.TypeString},
 				Optional:    true,
 				Description: "The tags associated with the user",
 			},
+			"send_password_email": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Description: "Whether or not to send an email containing the member's password" + descParameterIgnoredForGuest,
+			},
+			"send_welcome_email": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Description: "Whether or not to send a welcome email that includes onboarding information" + descParameterIgnoredForGuest,
+			},
+			"username": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				Description: "The member's username" + descParameterIgnoredForGuest,
+			},
+			"password": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The member's password for first access" + descParameterIgnoredForGuest,
+			},
+			"first_name": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The member's first name" + descParameterIgnoredForGuest,
+			},
+			"last_name": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The member's last name" + descParameterIgnoredForGuest,
+			},
+			"phone_number": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The member's phone number" + descParameterIgnoredForGuest,
+			},
+			"locale": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				Description: "The member's locale" + descParameterIgnoredForGuest,
+			},
+			// Computed data
 			"created_at": {
 				Type:        schema.TypeString,
 				Computed:    true,
@@ -65,7 +110,7 @@ func ResourceUser() *schema.Resource {
 			"status": {
 				Type:        schema.TypeString,
 				Computed:    true,
-				Description: "The status of user invitation.",
+				Description: "The status of user invitation",
 			},
 			"mfa": {
 				Type:        schema.TypeBool,
@@ -75,22 +120,60 @@ func ResourceUser() *schema.Resource {
 			"account_root_user_id": {
 				Type:        schema.TypeString,
 				Computed:    true,
-				Description: "The ID of the account root user associated with the iam user.",
+				Description: "The ID of the account root user associated with the iam user",
 			},
-			"organization_id": account.OrganizationIDOptionalSchema(),
+			"locked": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Defines whether the user is locked",
+			},
 		},
+	}
+}
+
+func createUserRequestBody(d *schema.ResourceData, isMember bool) *iam.CreateUserRequest {
+	if isMember {
+		// Create and return a member.
+		return &iam.CreateUserRequest{
+			OrganizationID: d.Get("organization_id").(string),
+			Tags:           types.ExpandStrings(d.Get("tags")),
+			Member: &iam.CreateUserRequestMember{
+				Email:             d.Get("email").(string),
+				SendPasswordEmail: d.Get("send_password_email").(bool),
+				SendWelcomeEmail:  d.Get("send_welcome_email").(bool),
+				Username:          d.Get("username").(string),
+				Password:          d.Get("password").(string),
+				FirstName:         d.Get("first_name").(string),
+				LastName:          d.Get("last_name").(string),
+				PhoneNumber:       d.Get("phone_number").(string),
+				Locale:            d.Get("locale").(string),
+			},
+		}
+	} else {
+		// Create and return a guest.
+		return &iam.CreateUserRequest{
+			OrganizationID: d.Get("organization_id").(string),
+			Email:          scw.StringPtr(d.Get("email").(string)),
+			Tags:           types.ExpandStrings(d.Get("tags")),
+		}
 	}
 }
 
 func resourceIamUserCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	api := NewAPI(m)
-	email := d.Get("email").(string)
 
-	user, err := api.CreateUser(&iam.CreateUserRequest{
-		OrganizationID: d.Get("organization_id").(string),
-		Email:          &email,
-		Tags:           types.ExpandStrings(d.Get("tags")),
-	}, scw.WithContext(ctx))
+	var user *iam.User
+
+	var err error
+
+	if d.Get("username").(string) != "" {
+		// Create a member.
+		user, err = api.CreateUser(createUserRequestBody(d, true), scw.WithContext(ctx))
+	} else {
+		// Create a guest.
+		user, err = api.CreateUser(createUserRequestBody(d, false), scw.WithContext(ctx))
+	}
+
 	if err != nil {
 		return diag.FromErr(err)
 	}
@@ -116,16 +199,25 @@ func resourceIamUserRead(ctx context.Context, d *schema.ResourceData, m interfac
 		return diag.FromErr(err)
 	}
 
+	_ = d.Set("organization_id", user.OrganizationID)
+	// User input data
 	_ = d.Set("email", user.Email)
+	_ = d.Set("tags", types.FlattenSliceString(user.Tags))
+	_ = d.Set("username", user.Username)
+	_ = d.Set("first_name", user.FirstName)
+	_ = d.Set("last_name", user.LastName)
+	_ = d.Set("phone_number", user.PhoneNumber)
+	_ = d.Set("locale", user.Locale)
+	// Computed data
 	_ = d.Set("created_at", types.FlattenTime(user.CreatedAt))
 	_ = d.Set("updated_at", types.FlattenTime(user.UpdatedAt))
-	_ = d.Set("organization_id", user.OrganizationID)
 	_ = d.Set("deletable", user.Deletable)
-	_ = d.Set("tags", types.FlattenSliceString(user.Tags))
 	_ = d.Set("last_login_at", types.FlattenTime(user.LastLoginAt))
 	_ = d.Set("type", user.Type)
 	_ = d.Set("status", user.Status)
 	_ = d.Set("mfa", user.Mfa)
+	_ = d.Set("account_root_user_id", user.AccountRootUserID)
+	_ = d.Set("locked", user.Locked)
 
 	return nil
 }
@@ -140,13 +232,49 @@ func resourceIamUserUpdate(ctx context.Context, d *schema.ResourceData, m interf
 		return diag.FromErr(err)
 	}
 
-	if d.HasChanges("tags") {
-		_, err = api.UpdateUser(&iam.UpdateUserRequest{
-			UserID: user.ID,
-			Tags:   types.ExpandUpdatedStringsPtr(d.Get("tags")),
-		}, scw.WithContext(ctx))
+	if user.Type == "guest" {
+		// Users of type "guest" only support the update of tags. The update of the email is not supported.
+		if d.HasChanges("tags") {
+			_, err = api.UpdateUser(&iam.UpdateUserRequest{
+				UserID: user.ID,
+				Tags:   types.ExpandUpdatedStringsPtr(d.Get("tags")),
+			}, scw.WithContext(ctx))
+			if err != nil {
+				return diag.FromErr(err)
+			}
+		}
+
+		if d.HasChange("email") {
+			return diag.FromErr(errors.New("the email of a guest user cannot be updated, you need to create a new user"))
+		}
+	} else {
+		req := &iam.UpdateUserRequest{UserID: user.ID}
+
+		if d.HasChanges("tags", "email", "first_name") {
+			req.Tags = types.ExpandUpdatedStringsPtr(d.Get("tags"))
+			req.Email = scw.StringPtr(d.Get("email").(string))
+			req.FirstName = scw.StringPtr(d.Get("first_name").(string))
+		}
+
+		if d.HasChanges("last_name", "phone_number", "locale") {
+			req.LastName = scw.StringPtr(d.Get("last_name").(string))
+			req.PhoneNumber = scw.StringPtr(d.Get("phone_number").(string))
+			req.Locale = scw.StringPtr(d.Get("locale").(string))
+		}
+
+		_, err = api.UpdateUser(req, scw.WithContext(ctx))
 		if err != nil {
 			return diag.FromErr(err)
+		}
+		// The update of the 'username' field is made through a different endpoint and payload.
+		if d.HasChange("username") {
+			_, err = api.UpdateUserUsername(&iam.UpdateUserUsernameRequest{
+				UserID:   user.ID,
+				Username: d.Get("username").(string),
+			}, scw.WithContext(ctx))
+			if err != nil {
+				return diag.FromErr(err)
+			}
 		}
 	}
 
