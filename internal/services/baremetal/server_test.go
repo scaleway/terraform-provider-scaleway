a5f8ef7b062ff9499d9c624979819e1cc2bfc5ef
Rémy Léone <rleone@scaleway.com>
2025-05-23 16:15:13 +0200

    fix baremetal

--- internal/services/baremetal/server_test.go
+++ internal/services/baremetal/server_test.go
@@ -1074,6 +1074,76 @@ func TestAccServer_WithIPAMPrivateNetwork(t *testing.T) {
 							  ipam_ip_ids = [scaleway_ipam_ip.ip01.id, scaleway_ipam_ip.ip02.id]
 							}
 						}
+					`, Zone, Zone, OfferName, Zone, SSHKeyName, SSHKeyBaremetal, name, Zone),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckBaremetalServerExists(tt, "scaleway_baremetal_server.base"),
+					testAccCheckBaremetalServerHasPrivateNetwork(tt, "scaleway_baremetal_server.base"),
+				),
+			},
+			{
+				Config: fmt.Sprintf(`
+						resource "scaleway_vpc" "vpc01" {
+						  name = "TestAccScalewayBaremetalIPAM"
+						}
+			
+						resource "scaleway_vpc_private_network" "pn01" {
+						  name = "TestAccScalewayBaremetalIPAM"
+						  ipv4_subnet {
+							subnet = "172.16.64.0/22"
+						  }
+						  vpc_id = scaleway_vpc.vpc01.id
+						}
+			
+						resource "scaleway_ipam_ip" "ip01" {
+						  address = "172.16.64.7"
+						  source {
+							private_network_id = scaleway_vpc_private_network.pn01.id
+						  }
+						}
+			
+						resource "scaleway_ipam_ip" "ip02" {
+						  address = "172.16.64.9"
+						  source {
+							private_network_id = scaleway_vpc_private_network.pn01.id
+						  }
+						}
+			
+						data "scaleway_baremetal_os" "my_os" {
+							zone = "%s"
+							name = "Ubuntu"
+							version = "22.04 LTS (Jammy Jellyfish)"
+						}
+			
+						data "scaleway_baremetal_offer" "my_offer" {
+							zone = "%s"
+							name = "%s"
+						}
+			
+						data "scaleway_baremetal_option" "private_network" {
+							zone = "%s"
+							name = "Private Network"
+						}
+			
+						resource "scaleway_iam_ssh_key" "base" {
+							name 	   = "%s"
+							public_key = "%s"
+						}
+			
+						resource "scaleway_baremetal_server" "base" {
+							name        = "%s"
+							zone        = "%s"
+							offer       = data.scaleway_baremetal_offer.my_offer.offer_id
+							os          = data.scaleway_baremetal_os.my_os.os_id
+			
+							ssh_key_ids = [ scaleway_iam_ssh_key.base.id ]
+							options {
+							  id = data.scaleway_baremetal_option.private_network.option_id
+							}
+							private_network {
+							  id = scaleway_vpc_private_network.pn01.id
+							  ipam_ip_ids = [scaleway_ipam_ip.ip01.id, scaleway_ipam_ip.ip02.id]
+							}
+						}
 			
 						data "scaleway_ipam_ips" "base" {
 						  resource {
