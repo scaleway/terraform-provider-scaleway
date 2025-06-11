75f202405afdb03018d6328b7ecec784cff83ec9
Laure-di <62625835+Laure-di@users.noreply.github.com>
2025-06-11 11:37:57 +0200

    feat(file): add support v1beta1 (#3141)
    * feat(file): add support v1beta1
    
    * Create, Read and Update function
    
    * Delete Methode
    
    * add tests
    
    * fix date issue and register cassettes
    
    * doc: create and update documentation
    
    * doc: create and update documentation
    
    * chore: tidy Go modules and update go.sum
    
    * test: fix filesystem size assertion by using strconv.FormatInt
    
    * doc: error checking file contents fix
    
    * test: add validation for filesystem size granularity
    
    * fix: convertion
    
    * chore: add file to github action
    
    * test: add testing files to exceptionsCassettesCases
    
    * test: add testing files to exceptionsCassettesCases
    
    * Update docs/resources/file_filesystem.md
    
    Co-authored-by: Rémy Léone <remy.leone@gmail.com>
    
    ---------
    
    Co-authored-by: Rémy Léone <remy.leone@gmail.com>

---
+++ internal/services/file/waiter.go
@@ -0,0 +1,26 @@
+package file
+
+import (
+	"context"
+	"time"
+
+	file "github.com/scaleway/scaleway-sdk-go/api/file/v1alpha1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/transport"
+)
+
+func waitForFileSystem(ctx context.Context, fileAPI *file.API, region scw.Region, id string, timeout time.Duration) (*file.FileSystem, error) {
+	retryInterval := defaultFileSystemRetryInterval
+	if transport.DefaultWaitRetryInterval != nil {
+		retryInterval = *transport.DefaultWaitRetryInterval
+	}
+
+	fileSystem, err := fileAPI.WaitForFileSystem(&file.WaitForFileSystemRequest{
+		FileSystemID:  id,
+		Region:        region,
+		Timeout:       scw.TimeDurationPtr(timeout),
+		RetryInterval: &retryInterval,
+	}, scw.WithContext(ctx))
+
+	return fileSystem, err
+}
