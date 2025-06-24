30c6e15b42ba20d54df28df19c4a6cd8dbf112d9
Nicolas Le Gall <nicolas.legall@synoptik-labs.com>
2025-06-24 14:36:23 +0900

    fix(baremetal_server): missing service information on creation

--- internal/services/baremetal/server.go
+++ internal/services/baremetal/server.go
@@ -382,6 +382,8 @@ func ResourceServerCreate(ctx context.Context, d *schema.ResourceData, m any) di
 			User:               types.ExpandStringPtr(d.Get("user")),
 			Password:           types.ExpandStringPtr(d.Get("password")),
 			PartitioningSchema: &partitioningSchema,
+			ServiceUser:        types.ExpandStringPtr(d.Get("service_user")),
+			ServicePassword:    types.ExpandStringPtr(d.Get("service_password")),
 		}
 	}
 
