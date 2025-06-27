577093da6c629fd6d942c085254692fd91dd18ed
Yacine Fodil <105779815+yfodil@users.noreply.github.com>
2025-06-27 17:16:50 +0200

    feat(edge-services): add support for include_cookies on cache stage (#3184)
    * feat(edge-services): add support for include_cookies on cache stage
    
    * fix GetEndOfServiceDate test

--- internal/services/edgeservices/cache_stage_test.go
+++ internal/services/edgeservices/cache_stage_test.go
@@ -33,6 +33,31 @@ func TestAccEdgeServicesCache_Basic(t *testing.T) {
 						"scaleway_edge_services_pipeline.main", "id",
 						"scaleway_edge_services_cache_stage.main", "pipeline_id"),
 					resource.TestCheckResourceAttr("scaleway_edge_services_cache_stage.main", "fallback_ttl", "3600"),
+					resource.TestCheckResourceAttr("scaleway_edge_services_cache_stage.main", "include_cookies", "false"),
+					resource.TestCheckResourceAttrSet("scaleway_edge_services_cache_stage.main", "created_at"),
+					resource.TestCheckResourceAttrSet("scaleway_edge_services_cache_stage.main", "updated_at"),
+				),
+			},
+			{
+				Config: `
+					resource "scaleway_edge_services_pipeline" "main" {
+					  name        = "my-edge_services-pipeline"
+					  description = "pipeline description"
+					}
+
+					resource "scaleway_edge_services_cache_stage" "main" {
+                      pipeline_id     = scaleway_edge_services_pipeline.main.id
+					  include_cookies = true
+                      fallback_ttl    = 7200
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					edgeservicestestfuncs.CheckEdgeServicesCacheExists(tt, "scaleway_edge_services_cache_stage.main"),
+					resource.TestCheckResourceAttrPair(
+						"scaleway_edge_services_pipeline.main", "id",
+						"scaleway_edge_services_cache_stage.main", "pipeline_id"),
+					resource.TestCheckResourceAttr("scaleway_edge_services_cache_stage.main", "fallback_ttl", "7200"),
+					resource.TestCheckResourceAttr("scaleway_edge_services_cache_stage.main", "include_cookies", "true"),
 					resource.TestCheckResourceAttrSet("scaleway_edge_services_cache_stage.main", "created_at"),
 					resource.TestCheckResourceAttrSet("scaleway_edge_services_cache_stage.main", "updated_at"),
 				),
