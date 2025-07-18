29f26e8868baae943c914f47c6f7cbb73a76978f
Jonathan R. <jremy@scaleway.com>
2025-07-18 09:59:51 +0200

    fix(tem): refactor reputation test to create and validate domain (#3210)

--- internal/services/tem/domain_data_source_test.go
+++ internal/services/tem/domain_data_source_test.go
@@ -50,21 +50,40 @@ func TestAccDataSourceDomain_Reputation(t *testing.T) {
 	tt := acctest.NewTestTools(t)
 	defer tt.Cleanup()
 
-	domainName := "test.scaleway-terraform.com"
+	subDomainName := "test-reputation"
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { acctest.PreCheck(t) },
 		ProviderFactories: tt.ProviderFactories,
+		CheckDestroy:      isDomainDestroyed(tt),
 		Steps: []resource.TestStep{
 			{
 				Config: fmt.Sprintf(`
-					data "scaleway_tem_domain" "test" {
-						name = "%s"
+					resource "scaleway_domain_zone" "test" {
+						domain    = "%s"
+						subdomain = "%s"
 					}
-				`, domainName),
+
+					resource "scaleway_tem_domain" "main" {
+						name       = scaleway_domain_zone.test.id
+						accept_tos = true
+						autoconfig = true
+					}
+
+					resource "scaleway_tem_domain_validation" "valid" {
+						domain_id = scaleway_tem_domain.main.id
+						region    = scaleway_tem_domain.main.region
+						timeout   = 3600
+					}
+
+					data "scaleway_tem_domain" "test" {
+						name = scaleway_tem_domain.main.name
+					}
+				`, domainNameValidation, subDomainName),
 				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("scaleway_tem_domain_validation.valid", "validated", "true"),
 					isDomainPresent(tt, "data.scaleway_tem_domain.test"),
-					resource.TestCheckResourceAttr("data.scaleway_tem_domain.test", "name", domainName),
+					resource.TestCheckResourceAttr("data.scaleway_tem_domain.test", "name", subDomainName+"."+domainNameValidation),
 					resource.TestCheckResourceAttrSet("data.scaleway_tem_domain.test", "reputation.0.status"),
 					resource.TestCheckResourceAttrSet("data.scaleway_tem_domain.test", "reputation.0.score"),
 					resource.TestCheckResourceAttrSet("data.scaleway_tem_domain.test", "reputation.0.scored_at"),
