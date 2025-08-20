78d74305be89a958bbdff73d4f3b3ca9d4b29ba4
Rémy Léone <rleone@scaleway.com>
2025-08-20 18:27:52 +0200

    docs: add support for description in lb schema

--- internal/services/lb/lbs_data_source.go
+++ internal/services/lb/lbs_data_source.go
@@ -2,6 +2,7 @@ package lb
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -30,86 +31,104 @@ func DataSourceLbs() *schema.Resource {
 				Description: "LBs with these exact tags are listed",
 			},
 			"lbs": {
-				Type:     schema.TypeList,
-				Computed: true,
+				Type:        schema.TypeList,
+				Description: "List of LBs",
+				Computed:    true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"id": {
-							Computed: true,
-							Type:     schema.TypeString,
+							Computed:    true,
+							Description: "UUID of the load balancer",
+							Type:        schema.TypeString,
 						},
 						"description": {
-							Computed: true,
-							Type:     schema.TypeString,
+							Computed:    true,
+							Description: "Description of the load balancer",
+							Type:        schema.TypeString,
 						},
 						"status": {
-							Computed: true,
-							Type:     schema.TypeString,
+							Computed:    true,
+							Description: "Status of the load balancer",
+							Type:        schema.TypeString,
 						},
 						"name": {
-							Computed: true,
-							Type:     schema.TypeString,
+							Computed:    true,
+							Description: "Name of the load balancer",
+							Type:        schema.TypeString,
 						},
 						"type": {
-							Computed: true,
-							Type:     schema.TypeString,
+							Computed:    true,
+							Description: "Type of the load balancer",
+							Type:        schema.TypeString,
 						},
 						"tags": {
-							Computed: true,
-							Type:     schema.TypeList,
+							Computed:    true,
+							Description: "List of tags assigned to the load balancer",
+							Type:        schema.TypeList,
 							Elem: &schema.Schema{
 								Type: schema.TypeString,
 							},
 						},
 						"instances": {
-							Type:     schema.TypeList,
-							Computed: true,
+							Type:        schema.TypeList,
+							Description: "List of instances assigned to the load balancer",
+							Computed:    true,
 							Elem: &schema.Resource{
 								Schema: map[string]*schema.Schema{
 									"id": {
-										Type:     schema.TypeString,
-										Computed: true,
+										Type:        schema.TypeString,
+										Description: "UUID of the instance",
+										Computed:    true,
 									},
 									"status": {
-										Type:     schema.TypeString,
-										Computed: true,
+										Type:        schema.TypeString,
+										Description: "Status of the instance",
+										Computed:    true,
 									},
 									"ip_address": {
-										Type:     schema.TypeString,
-										Computed: true,
+										Type:        schema.TypeString,
+										Description: "IP address of the instance",
+										Computed:    true,
 									},
 									"created_at": {
-										Computed: true,
-										Type:     schema.TypeString,
+										Computed:    true,
+										Description: "Date and time when the instance was created (RFC3339)",
+										Type:        schema.TypeString,
 									},
 									"updated_at": {
-										Computed: true,
-										Type:     schema.TypeString,
+										Computed:    true,
+										Description: "Date and time when the instance was updated (RFC3339)",
+										Type:        schema.TypeString,
 									},
 									"zone": zonal.Schema(),
 								},
 							},
 						},
 						"ips": {
-							Type:     schema.TypeList,
-							Computed: true,
+							Type:        schema.TypeList,
+							Description: "List of IPs assigned to the load balancer",
+							Computed:    true,
 							Elem: &schema.Resource{
 								Schema: map[string]*schema.Schema{
 									"id": {
-										Type:     schema.TypeString,
-										Computed: true,
+										Type:        schema.TypeString,
+										Description: "UUID of the IP",
+										Computed:    true,
 									},
 									"ip_address": {
-										Type:     schema.TypeString,
-										Computed: true,
+										Type:        schema.TypeString,
+										Description: "IP address",
+										Computed:    true,
 									},
 									"reverse": {
-										Type:     schema.TypeString,
-										Computed: true,
+										Type:        schema.TypeString,
+										Description: "Reverse DNS attached to the IP",
+										Computed:    true,
 									},
 									"lb_id": {
-										Computed: true,
-										Type:     schema.TypeString,
+										Computed:    true,
+										Description: "UUID of the load balancer attached to the IP",
+										Type:        schema.TypeString,
 									},
 									"project_id":      account.ProjectIDSchema(),
 									"organization_id": account.OrganizationIDSchema(),
@@ -118,36 +137,48 @@ func DataSourceLbs() *schema.Resource {
 							},
 						},
 						"frontend_count": {
-							Computed: true,
-							Type:     schema.TypeInt,
+							Computed:    true,
+							Description: "number of frontends the Load Balancer has.",
+							Type:        schema.TypeInt,
 						},
 						"backend_count": {
-							Computed: true,
-							Type:     schema.TypeInt,
+							Computed:    true,
+							Description: "number of backends the Load Balancer has.",
+							Type:        schema.TypeInt,
 						},
 						"private_network_count": {
-							Computed: true,
-							Type:     schema.TypeInt,
+							Computed:    true,
+							Description: "number of Private Networks attached to the Load Balancer.",
+							Type:        schema.TypeInt,
 						},
 						"route_count": {
-							Computed: true,
-							Type:     schema.TypeInt,
+							Computed:    true,
+							Description: "number of routes configured on the Load Balancer.",
+							Type:        schema.TypeInt,
 						},
 						"subscriber": {
-							Computed: true,
-							Type:     schema.TypeString,
+							Computed:    true,
+							Description: "Subscriber information.",
+							Type:        schema.TypeString,
 						},
 						"ssl_compatibility_level": {
 							Computed: true,
-							Type:     schema.TypeString,
+							Description: func() string {
+								var t lb.SSLCompatibilityLevel
+								values := t.Values()
+								return fmt.Sprintf("SSL compatibility level possible values are %s", values)
+							}(),
+							Type: schema.TypeString,
 						},
 						"created_at": {
-							Computed: true,
-							Type:     schema.TypeString,
+							Computed:    true,
+							Description: "Date and time when the load balancer was created (RFC3339)",
+							Type:        schema.TypeString,
 						},
 						"updated_at": {
-							Computed: true,
-							Type:     schema.TypeString,
+							Computed:    true,
+							Description: "Date and time when the load balancer was created (RFC3339)",
+							Type:        schema.TypeString,
 						},
 						"zone":            zonal.Schema(),
 						"organization_id": account.OrganizationIDSchema(),
