d79b9f301f73b9bed79296cf9c6432b01449c3dd
Laure-di <62625835+Laure-di@users.noreply.github.com>
2025-11-04 13:55:30 +0100

    feat(object): add support datasource object (#3444)

---
+++ internal/services/object/data_source_object_test.go
@@ -0,0 +1,77 @@
+package object_test
+
+import (
+	"fmt"
+	"testing"
+
+	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/acctest"
+	objectchecks "github.com/scaleway/terraform-provider-scaleway/v2/internal/services/object/testfuncs"
+)
+
+func TestAccDataSourceObject_Basic(t *testing.T) {
+	tt := acctest.NewTestTools(t)
+	defer tt.Cleanup()
+
+	bucketName := sdkacctest.RandomWithPrefix("test-acc-scaleway-data-source-object-basic")
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: tt.ProviderFactories,
+		CheckDestroy: resource.ComposeTestCheckFunc(
+			objectchecks.IsObjectDestroyed(tt),
+			objectchecks.IsBucketDestroyed(tt),
+		),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					resource "scaleway_object_bucket" "base-01" {
+						name = "%s"
+						region= "%s"
+						tags = {
+							foo = "bar"
+						}
+					}
+
+					resource scaleway_object "file" {
+						bucket = scaleway_object_bucket.base-01.id
+						key = "myfile"
+						file   = "testfixture/empty.qcow2"
+					}
+
+				`, bucketName, objectTestsMainRegion),
+				Check: resource.ComposeTestCheckFunc(
+					objectchecks.CheckBucketExists(tt, "scaleway_object_bucket.base-01", true),
+					objectchecks.IsObjectExists(tt, "scaleway_object.file"),
+				),
+			},
+			{
+				Config: fmt.Sprintf(`
+					resource "scaleway_object_bucket" "base-01" {
+						name = "%s"
+						region= "%s"
+						tags = {
+							foo = "bar"
+						}
+					}
+
+					resource scaleway_object "file" {
+						bucket = scaleway_object_bucket.base-01.id
+						key = "myfile"
+						file   = "testfixture/empty.qcow2"
+					}
+
+					data scaleway_object "by-key" {
+						key = "myfile"
+						bucket = scaleway_object_bucket.base-01.id
+					}
+				`, bucketName, objectTestsMainRegion),
+				Check: resource.ComposeTestCheckFunc(
+					objectchecks.CheckBucketExists(tt, "scaleway_object_bucket.base-01", true),
+					resource.TestCheckResourceAttr("data.scaleway_object.by-key", "key", "myfile"),
+					resource.TestCheckResourceAttrSet("data.scaleway_object.by-key", "id"),
+				),
+			},
+		},
+	})
+}
