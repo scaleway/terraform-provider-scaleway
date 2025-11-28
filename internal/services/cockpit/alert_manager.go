6116881f6a7ef6085edc1e84eef93874fe4e2391
Jonathan Remy <jremy@scaleway.com>
2025-11-28 07:20:43 +0100

    chore: fix lint whitespace in alert_manager

--- internal/services/cockpit/alert_manager.go
+++ internal/services/cockpit/alert_manager.go
@@ -181,8 +181,10 @@ func ResourceCockpitAlertManagerRead(ctx context.Context, d *schema.ResourceData
 	if err != nil {
 		if httperrors.Is404(err) {
 			d.SetId("")
+
 			return nil
 		}
+
 		return diag.FromErr(err)
 	}
 
