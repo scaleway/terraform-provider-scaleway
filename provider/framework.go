8432199fdf27e93f9b940d31a3df75dd1030ccb3
Jonathan Remy <jremy@scaleway.com>
2025-12-03 09:52:13 +0100

    feat: add mongodb instance snapshot action

--- provider/framework.go
+++ provider/framework.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/meta"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/services/instance"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/services/mongodb"
 )
 
 var (
@@ -138,6 +139,7 @@ func (p *ScalewayProvider) Actions(_ context.Context) []func() action.Action {
 	var res []func() action.Action
 
 	res = append(res, instance.NewServerAction)
+	res = append(res, mongodb.NewInstanceSnapshotAction)
 
 	return res
 }
