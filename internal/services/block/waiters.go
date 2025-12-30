f7e9f88dee62b480845c5a1faffccef3f1caca40
Leïla Marabese <lmarabese@scaleway.com>
2025-12-30 10:42:00 +0100

    chore(block): bump API from v1alpha1 to v1

--- internal/services/block/waiters.go
+++ internal/services/block/waiters.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"time"
 
-	block "github.com/scaleway/scaleway-sdk-go/api/block/v1alpha1"
+	"github.com/scaleway/scaleway-sdk-go/api/block/v1"
 	"github.com/scaleway/scaleway-sdk-go/scw"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/transport"
 )
