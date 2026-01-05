94c690b193073959c0e5174b873965a3bcd9dcf8
Yacine FODIL <yfodil@scaleway.com>
2026-01-05 09:34:50 +0100

    chore(s2s_vpn): migrate to SchemaFunc

--- internal/services/s2svpn/vpn_gateway.go
+++ internal/services/s2svpn/vpn_gateway.go
@@ -32,92 +32,96 @@ func ResourceVPNGateway() *schema.Resource {
 			StateContext: schema.ImportStatePassthroughContext,
 		},
 		SchemaVersion: 0,
-		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Optional:    true,
-				Description: "The name of the VPN gateway",
+		SchemaFunc:    vpnGatewaySchema,
+	}
+}
+
+func vpnGatewaySchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"name": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Optional:    true,
+			Description: "The name of the VPN gateway",
+		},
+		"tags": {
+			Type:        schema.TypeList,
+			Optional:    true,
+			Computed:    true,
+			Description: "The list of tags to apply to the VPN gateway",
+			Elem: &schema.Schema{
+				Type: schema.TypeString,
 			},
-			"tags": {
-				Type:        schema.TypeList,
-				Optional:    true,
-				Computed:    true,
-				Description: "The list of tags to apply to the VPN gateway",
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
-				},
-			},
-			"gateway_type": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "The VPN gateway type (commercial offer type)",
-			},
-			"public_config": {
-				Type:        schema.TypeList,
-				Computed:    true,
-				Optional:    true,
-				Description: "The public endpoint configuration of the VPN gateway",
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"ipam_ipv4_id": {
-							Type:        schema.TypeString,
-							Description: " The ID of the IPAM IPv4 address to use as the public IP for the VPN gateway",
-							Optional:    true,
-						},
-						"ipam_ipv6_id": {
-							Type:        schema.TypeString,
-							Description: " The ID of the IPAM IPv6 address to use as the public IP for the VPN gateway",
-							Optional:    true,
-						},
+		},
+		"gateway_type": {
+			Type:        schema.TypeString,
+			Required:    true,
+			Description: "The VPN gateway type (commercial offer type)",
+		},
+		"public_config": {
+			Type:        schema.TypeList,
+			Computed:    true,
+			Optional:    true,
+			Description: "The public endpoint configuration of the VPN gateway",
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"ipam_ipv4_id": {
+						Type:        schema.TypeString,
+						Description: " The ID of the IPAM IPv4 address to use as the public IP for the VPN gateway",
+						Optional:    true,
+					},
+					"ipam_ipv6_id": {
+						Type:        schema.TypeString,
+						Description: " The ID of the IPAM IPv6 address to use as the public IP for the VPN gateway",
+						Optional:    true,
 					},
 				},
 			},
-			"private_network_id": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "The ID of the Private Network to attach to the VPN gateway",
-			},
-			"ipam_private_ipv4_id": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-				Description: "The ID of the IPAM private IPv4 address to attach to the VPN gateway",
-			},
-			"ipam_private_ipv6_id": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Optional:    true,
-				Description: "The ID of the IPAM private IPv6 address to attach to the VPN gateway",
-			},
-			"asn": {
-				Type:        schema.TypeInt,
-				Computed:    true,
-				Description: "The AS Number of the vpn gateway",
-			},
-			"status": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The status of the VPN gateway",
-			},
-			"created_at": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The date and time of the creation of the VPN gateway",
-			},
-			"updated_at": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The date and time of the last update of the VPN gateway",
-			},
-			"zone":       zonal.OptionalSchema(),
-			"region":     regional.Schema(),
-			"project_id": account.ProjectIDSchema(),
-			"organization_id": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Organization ID of the Project",
-			},
+		},
+		"private_network_id": {
+			Type:        schema.TypeString,
+			Required:    true,
+			Description: "The ID of the Private Network to attach to the VPN gateway",
+		},
+		"ipam_private_ipv4_id": {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Computed:    true,
+			Description: "The ID of the IPAM private IPv4 address to attach to the VPN gateway",
+		},
+		"ipam_private_ipv6_id": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Optional:    true,
+			Description: "The ID of the IPAM private IPv6 address to attach to the VPN gateway",
+		},
+		"asn": {
+			Type:        schema.TypeInt,
+			Computed:    true,
+			Description: "The AS Number of the vpn gateway",
+		},
+		"status": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "The status of the VPN gateway",
+		},
+		"created_at": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "The date and time of the creation of the VPN gateway",
+		},
+		"updated_at": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "The date and time of the last update of the VPN gateway",
+		},
+		"zone":       zonal.OptionalSchema(),
+		"region":     regional.Schema(),
+		"project_id": account.ProjectIDSchema(),
+		"organization_id": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "Organization ID of the Project",
 		},
 	}
 }
