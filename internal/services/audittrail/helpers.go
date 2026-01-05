8e90392c08416aef843e5e09291cc0ed19a56a2e
Laure-di <62625835+Laure-di@users.noreply.github.com>
2026-01-05 10:22:08 +0100

    feat(applesilicon): add support server action (#3567)
    * feat(applesilicon): add support server action
    
    * doc(applesilicon): add support server action
    
    * doc(applesilicon): add support server action

--- internal/services/audittrail/helpers.go
+++ internal/services/audittrail/helpers.go
@@ -1,7 +1,13 @@
 package audittrail
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	audittrailSDK "github.com/scaleway/scaleway-sdk-go/api/audit_trail/v1alpha1"
 	"github.com/scaleway/scaleway-sdk-go/scw"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/meta"
@@ -17,4 +23,26 @@ func newAPIWithRegion(d *schema.ResourceData, m any) (*audittrailSDK.API, scw.Re
 	}
 
 	return api, region, nil
+}
+
+func CheckEventsOccurrence(resourceName string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		rs, ok := state.RootModule().Resources[resourceName]
+		if !ok {
+			return errors.New("not found: " + resourceName)
+		}
+
+		countStr := rs.Primary.Attributes["events.#"]
+
+		count, err := strconv.Atoi(countStr)
+		if err != nil {
+			return fmt.Errorf("could not parse events.# as integer: %w", err)
+		}
+
+		if count != 1 {
+			return fmt.Errorf("expected exactly 1 event, got %d", count)
+		}
+
+		return nil
+	}
 }
