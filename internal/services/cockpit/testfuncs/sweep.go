194363b162a78b612e32f6447cc20febb51dc29c
Estelle Soulard <esoulard@scaleway.com>
2026-01-06 17:36:49 +0100

    chore(cockpit): refacto

--- internal/services/cockpit/testfuncs/sweep.go
+++ internal/services/cockpit/testfuncs/sweep.go
@@ -10,13 +10,12 @@ import (
 	"github.com/scaleway/scaleway-sdk-go/scw"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/acctest"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/httperrors"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/logging"
 )
 
+// Cockpit resource doesn't require explicit deactivation.
+// Sources, tokens, and other resources are cleaned up by their respective sweepers.
 func AddTestSweepers() {
-	resource.AddTestSweepers("scaleway_cockpit", &resource.Sweeper{
-		Name: "scaleway_cockpit",
-		F:    testSweepCockpit,
-	})
 	resource.AddTestSweepers("scaleway_cockpit_grafana_user", &resource.Sweeper{
 		Name: "scaleway_cockpit_grafana_user",
 		F:    testSweepCockpitGrafanaUser,
@@ -27,7 +26,7 @@ func AddTestSweepers() {
 	})
 	resource.AddTestSweepers("scaleway_cockpit_source", &resource.Sweeper{
 		Name: "scaleway_cockpit_source",
-		F:    testSweepCockpitSource,
+		F:    testSweepCockpitDataSource,
 	})
 }
 
@@ -116,13 +115,7 @@ func testSweepCockpitGrafanaUser(_ string) error {
 	})
 }
 
-func testSweepCockpit(_ string) error {
-	// Cockpit resource doesn't require explicit deactivation.
-	// Sources, tokens, and other resources are cleaned up by their respective sweepers.
-	return nil
-}
-
-func testSweepCockpitSource(_ string) error {
+func testSweepCockpitDataSource(_ string) error {
 	return acctest.SweepRegions(scw.AllRegions, func(scwClient *scw.Client, region scw.Region) error {
 		accountAPI := accountSDK.NewProjectAPI(scwClient)
 		cockpitAPI := cockpit.NewRegionalAPI(scwClient)
@@ -137,11 +130,10 @@ func testSweepCockpitSource(_ string) error {
 				continue
 			}
 
-			// Collect all sources by trying without filter and with all possible origins
-			// Some sources may only appear when filtering by origin
-			allDataSources := make(map[string]*cockpit.DataSource)
+			// Some datasources may only appear when filtering by origin. We query them first
+			// with no origin filter, then by origin, to ensure all datasources are retrieved
+			dataSources := make(map[string]*cockpit.DataSource)
 
-			// List of possible origins to try
 			origins := []cockpit.DataSourceOrigin{
 				cockpit.DataSourceOriginUnknownOrigin,
 				cockpit.DataSourceOriginCustom,
@@ -149,7 +141,6 @@ func testSweepCockpitSource(_ string) error {
 				cockpit.DataSourceOriginExternal,
 			}
 
-			// First, try without any origin filter
 			listDatasources, err := cockpitAPI.ListDataSources(&cockpit.RegionalAPIListDataSourcesRequest{
 				ProjectID: project.ID,
 				Region:    region,
@@ -159,14 +150,11 @@ func testSweepCockpitSource(_ string) error {
 					return fmt.Errorf("failed to list sources: %w", err)
 				}
 			} else {
-				// Collect all sources from the unfiltered list
 				for _, datasource := range listDatasources.DataSources {
-					allDataSources[datasource.ID] = datasource
+					dataSources[datasource.ID] = datasource
 				}
 			}
 
-			// Always try with each origin to ensure we catch all sources
-			// Some sources may only appear when filtering by specific origin
 			for _, origin := range origins {
 				listDatasources, err := cockpitAPI.ListDataSources(&cockpit.RegionalAPIListDataSourcesRequest{
 					ProjectID: project.ID,
@@ -174,22 +162,16 @@ func testSweepCockpitSource(_ string) error {
 					Origin:    origin,
 				}, scw.WithAllPages())
 				if err != nil {
-					if httperrors.Is404(err) {
-						continue
-					}
-
-					// Don't return error here, just continue with next origin
+					logging.L.Debugf("sweeper: failed to list cockpit datasource by origin %s", origin)
 					continue
 				}
 
-				// Collect all unique sources
 				for _, datasource := range listDatasources.DataSources {
-					allDataSources[datasource.ID] = datasource
+					dataSources[datasource.ID] = datasource
 				}
 			}
 
-			// Delete all collected sources
-			for _, datasource := range allDataSources {
+			for _, datasource := range dataSources {
 				err = cockpitAPI.DeleteDataSource(&cockpit.RegionalAPIDeleteDataSourceRequest{
 					DataSourceID: datasource.ID,
 					Region:       region,
