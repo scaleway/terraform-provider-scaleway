c12b85e39ee8b86282c2a0b752220d144139d144
Rémy Léone <rleone@scaleway.com>
2026-01-12 12:51:06 +0100

    Fix linters

--- internal/functions/region_from_id_test.go
+++ internal/functions/region_from_id_test.go
@@ -17,8 +17,8 @@ func TestRegionFromIDFunctionRun(t *testing.T) {
 	t.Parallel()
 
 	testCases := map[string]struct {
-		request  function.RunRequest
 		expected function.RunResponse
+		request  function.RunRequest
 	}{
 		// The example implementation uses the Go built-in string type, however
 		// if AllowNullValue was enabled and *string or types.String was used,
@@ -99,8 +99,6 @@ func TestRegionFromIDFunctionRun(t *testing.T) {
 	}
 
 	for name, testCase := range testCases {
-		name, testCase := name, testCase
-
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
