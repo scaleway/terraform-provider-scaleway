6209930ec64c0306121aa985bac246d54ff9ab01
Laure-di <62625835+Laure-di@users.noreply.github.com>
2026-01-15 17:15:23 +0100

    feat(apple-silicon): add support runner and os datasource (#3497)
    * feat(apple-silicon): add support runner
    
    * doc(apple-silicon): add runner ressource
    
    * add skip test security
    
    * feat(apple-silicon): manage runner on server
    
    * feat(apple-silicon): add runner to server
    
    * feat(apple-silicon): add runner to server
    
    * feat(apple-silicon): add support identity for runner
    
    * doc(apple-silicon): fix tfplugindoc
    
    * doc(apple-silicon): fix tfplugindoc
    
    * doc(apple-silicon): fix tfplugindoc
    
    * feat(apple-silicon): add support os datasource
    
    ---------
    
    Co-authored-by: Rémy Léone <rleone@scaleway.com>

---
+++ internal/services/applesilicon/os_data_source_test.go
@@ -0,0 +1,73 @@
+package applesilicon_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	applesilicon "github.com/scaleway/scaleway-sdk-go/api/applesilicon/v1alpha1"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/acctest"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/locality/zonal"
+)
+
+func TestAccDataSourceOS_Basic(t *testing.T) {
+	tt := acctest.NewTestTools(t)
+	defer tt.Cleanup()
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: tt.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					data "scaleway_apple_silicon_os" "by_name" {
+						name = "macos-ventura-13.6"
+					}
+					
+					data "scaleway_apple_silicon_os" "by_id" {
+						os_id = "cafecafe-5018-4dcd-bd08-35f031b0ac3e"
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAppleSiliconOsExists(tt, "data.scaleway_apple_silicon_os.by_id"),
+					testAccCheckAppleSiliconOsExists(tt, "data.scaleway_apple_silicon_os.by_name"),
+
+					resource.TestCheckResourceAttrSet("data.scaleway_apple_silicon_os.by_name", "name"),
+					resource.TestCheckResourceAttrSet("data.scaleway_apple_silicon_os.by_name", "version"),
+					resource.TestCheckResourceAttrSet("data.scaleway_apple_silicon_os.by_name", "os_id"),
+
+					resource.TestCheckResourceAttrSet("data.scaleway_apple_silicon_os.by_id", "name"),
+					resource.TestCheckResourceAttrSet("data.scaleway_apple_silicon_os.by_id", "version"),
+					resource.TestCheckResourceAttrSet("data.scaleway_apple_silicon_os.by_id", "os_id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAppleSiliconOsExists(tt *acctest.TestTools, n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+
+		if !ok {
+			return fmt.Errorf("not found: %s", n)
+		}
+
+		zone, ID, err := zonal.ParseID(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		applesiliconAPI := applesilicon.NewAPI(tt.Meta.ScwClient())
+
+		_, err = applesiliconAPI.GetOS(&applesilicon.GetOSRequest{
+			OsID: ID,
+			Zone: zone,
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
