766621665422c6ca2556769b8cef9796d00b85b2
esoulard <esoulard@scaleway.com>
2026-02-05 10:54:27 +0100

    feat(mongodb): add instance password_wo (#3624)
    * feat(mongodb): add instance password_wo
    
    * docs(mongodb): fix instance examples
    
    * docs(mongodb): add instance password_wo rollback example

---
+++ examples/resources/scaleway_mongodb_instance/resource-private-network.tf
@@ -0,0 +1,20 @@
+### MongoDB instance with Private Network
+
+resource "scaleway_vpc_private_network" "pn01" {
+  name   = "my_private_network"
+  region = "fr-par"
+}
+
+resource "scaleway_mongodb_instance" "main" {
+  name              = "test-mongodb-basic1"
+  version           = "7.0.12"
+  node_type         = "MGDB-PLAY2-NANO"
+  node_number       = 1
+  user_name         = "my_initial_user"
+  password          = "thiZ_is_v&ry_s3cret"
+  volume_size_in_gb = 5
+
+  private_network {
+    pn_id = scaleway_vpc_private_network.pn01.id
+  }
+}
