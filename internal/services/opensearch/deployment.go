f9912ea92170d8c35a5a975873b04727b4403332
Jonathan Remy <jremy@scaleway.com>
2026-02-09 15:26:55 +0100

    Fix gci formatting by removing blank line between scaleway SDK imports

--- internal/services/opensearch/deployment.go
+++ internal/services/opensearch/deployment.go
@@ -7,7 +7,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	searchdbapi "github.com/scaleway/scaleway-sdk-go/api/searchdb/v1alpha1"
-
 	"github.com/scaleway/scaleway-sdk-go/scw"
 
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/httperrors"
