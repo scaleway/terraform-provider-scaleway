c5b09f30dd9b25737ff7af8a4bf52d864b1c5a8b
Jonathan Remy <jremy@scaleway.com>
2026-02-10 16:20:50 +0100

    Fix GCI import formatting for opensearch deployment

--- internal/services/opensearch/deployment.go
+++ internal/services/opensearch/deployment.go
@@ -8,7 +8,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	searchdbapi "github.com/scaleway/scaleway-sdk-go/api/searchdb/v1alpha1"
 	"github.com/scaleway/scaleway-sdk-go/scw"
-
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/httperrors"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/identity"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/locality"
