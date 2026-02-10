6a640a3de64fe39196b3f53678331c573a06e525
Jonathan Remy <jremy@scaleway.com>
2026-02-10 11:32:18 +0100

    Fix import formatting in opensearch deployment

--- internal/services/opensearch/deployment.go
+++ internal/services/opensearch/deployment.go
@@ -6,7 +6,8 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	searchdbapi "github.com/scaleway/scaleway-sdk-go/api/searchdb/v1alpha1"	"github.com/scaleway/scaleway-sdk-go/scw"
+	searchdbapi "github.com/scaleway/scaleway-sdk-go/api/searchdb/v1alpha1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
 
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/httperrors"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/locality"
