908ee346dab0e5a26f463df0d62008c85c7f98b8
Leïla Marabese <lmarabese@scaleway.com>
2026-02-20 17:22:51 +0100

    move consts to dedicated file

--- internal/services/instance/helpers_instance.go
+++ internal/services/instance/helpers_instance.go
@@ -25,28 +25,6 @@ import (
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/types"
 )
 
-const (
-	// InstanceServerStateStopped transient state of the instance event stop
-	InstanceServerStateStopped = "stopped"
-	// InstanceServerStateStarted transient state of the instance event start
-	InstanceServerStateStarted = "started"
-	// InstanceServerStateStandby transient state of the instance event waiting third action or rescue mode
-	InstanceServerStateStandby = "standby"
-
-	DefaultInstanceServerWaitTimeout        = 20 * time.Minute
-	defaultInstancePrivateNICWaitTimeout    = 10 * time.Minute
-	defaultInstanceVolumeDeleteTimeout      = 10 * time.Minute
-	defaultInstanceSecurityGroupTimeout     = 1 * time.Minute
-	defaultInstanceSecurityGroupRuleTimeout = 1 * time.Minute
-	defaultInstancePlacementGroupTimeout    = 1 * time.Minute
-	defaultInstanceIPTimeout                = 1 * time.Minute
-	defaultInstanceIPReverseDNSTimeout      = 10 * time.Minute
-
-	defaultInstanceSnapshotWaitTimeout = 1 * time.Hour
-
-	defaultInstanceImageTimeout = 1 * time.Hour
-)
-
 // newAPIWithZone returns a new instance API and the zone for a Create request
 func newAPIWithZone(d *schema.ResourceData, m any) (*instance.API, scw.Zone, error) {
 	instanceAPI := instance.NewAPI(meta.ExtractScwClient(m))
