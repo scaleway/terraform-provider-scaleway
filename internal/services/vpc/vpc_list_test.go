44f2d9b4589ac283cee0ecb256b21c0cd0059db6
Rémy Léone <rleone@scaleway.com>
2026-02-25 15:45:38 +0100

    Fix

--- internal/services/vpc/vpc_list_test.go
+++ internal/services/vpc/vpc_list_test.go
@@ -3,6 +3,7 @@ package vpc_test
 import (
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-testing/config"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/querycheck"
@@ -17,16 +18,21 @@ func TestAccListVPCs_Basic(t *testing.T) {
 		ProtoV6ProviderFactories: tt.ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Query: true,
-				Config: `
-				list "scaleway_vpc" "fr-par" {
-					config {
-						region = "fr-par"
- 					}
-				}
-				`,
+				// Create VPCs in different regions with different tags
+				ConfigDirectory: config.StaticDirectory("testdata/vpc_list_basic/"),
+			},
+			{
+				Query:           true,
+				ConfigDirectory: config.StaticDirectory("testdata/vpc_list_basic/"),
 				QueryResultChecks: []querycheck.QueryResultCheck{
-					querycheck.ExpectIdentity("scaleway_vpc.fr-par", map[string]knownvalue.Check{
+					// Check that we can list all VPCs
+					querycheck.ExpectLength("list.scaleway_vpc.all", 2),
+					// Check that we can filter by region and tag
+					querycheck.ExpectLength("list.scaleway_vpc.fr-par", 1),
+					// Check that we can filter by name pattern
+					querycheck.ExpectLength("list.scaleway_vpc.by_name", 2),
+					// Verify specific VPC attributes in the results
+					querycheck.ExpectIdentity("list.scaleway_vpc.fr-par", map[string]knownvalue.Check{
 						"region": knownvalue.StringExact("fr-par"),
 					}),
 				},
