af35210e2eafa282e6b30ce96c30a80f67cd1005
Yacine Fodil <105779815+yfodil@users.noreply.github.com>
2026-03-11 14:00:25 +0100

    feat(vpcgw): add support for ip identity (#3777)
    * feat(vpcgw): add support for ip identity
    
    * lint
    
    * Fix
    
    ---------
    
    Co-authored-by: Rémy Léone <rleone@scaleway.com>

--- internal/services/vpcgw/ip.go
+++ internal/services/vpcgw/ip.go
@@ -9,6 +9,7 @@ import (
 	"github.com/scaleway/scaleway-sdk-go/api/vpcgw/v2"
 	"github.com/scaleway/scaleway-sdk-go/scw"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/httperrors"
+	"github.com/scaleway/terraform-provider-scaleway/v2/internal/identity"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/locality/zonal"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/services/account"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/types"
@@ -23,6 +24,7 @@ func ResourceIP() *schema.Resource {
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
 		},
+		Identity:      identity.DefaultZonal(),
 		SchemaVersion: 0,
 		SchemaFunc:    ipSchema,
 	}
@@ -83,7 +85,12 @@ func ResourceIPCreate(ctx context.Context, d *schema.ResourceData, m any) diag.D
 		return diag.FromErr(err)
 	}
 
-	d.SetId(zonal.NewIDString(zone, res.ID))
+	d.SetId(zonal.NewIDString(res.Zone, res.ID))
+
+	err = identity.SetZonalIdentity(d, res.Zone, res.ID)
+	if err != nil {
+		return diag.FromErr(err)
+	}
 
 	reverse := d.Get("reverse")
 	if len(reverse.(string)) > 0 {
@@ -123,12 +130,23 @@ func ResourceIPRead(ctx context.Context, d *schema.ResourceData, m any) diag.Dia
 		return diag.FromErr(err)
 	}
 
+	diags := setIPState(d, ip)
+
+	err = identity.SetZonalIdentity(d, ip.Zone, ip.ID)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return diags
+}
+
+func setIPState(d *schema.ResourceData, ip *vpcgw.IP) diag.Diagnostics {
 	_ = d.Set("organization_id", ip.OrganizationID)
 	_ = d.Set("address", ip.Address.String())
 	_ = d.Set("project_id", ip.ProjectID)
 	_ = d.Set("created_at", ip.CreatedAt.Format(time.RFC3339))
 	_ = d.Set("updated_at", ip.UpdatedAt.Format(time.RFC3339))
-	_ = d.Set("zone", zone)
+	_ = d.Set("zone", ip.Zone)
 	_ = d.Set("tags", ip.Tags)
 	_ = d.Set("reverse", ip.Reverse)
 
