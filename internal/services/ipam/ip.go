86bd132110de35cde4841562e8c782c277889dcc
Yacine FODIL <yfodil@scaleway.com>
2026-03-12 11:04:10 +0100

    fix

--- internal/services/ipam/ip.go
+++ internal/services/ipam/ip.go
@@ -221,9 +221,7 @@ func ResourceIPAMIPCreate(ctx context.Context, d *schema.ResourceData, m any) di
 		return diag.FromErr(err)
 	}
 
-	d.SetId(regional.NewIDString(region, res.ID))
-
-	err = identity.SetRegionalIdentity(d, region, res.ID)
+	err = identity.SetRegionalIdentity(d, res.Region, res.ID)
 	if err != nil {
 		return diag.FromErr(err)
 	}
