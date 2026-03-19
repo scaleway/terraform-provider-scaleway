3b38c3572ff2ed7cc87a5c2865408b63ab8dd688
Yacine Fodil <105779815+yfodil@users.noreply.github.com>
2026-03-19 11:24:27 +0100

    feat(edge_services): add cache datasource (#3818)
    * feat(edge_services): add cache datasource
    
    * fix
    
    ---------
    
    Co-authored-by: Rémy Léone <rleone@scaleway.com>

--- provider/sdkv2.go
+++ provider/sdkv2.go
@@ -294,6 +294,7 @@ func SDKProvider(config *Config) plugin.ProviderFunc {
 				"scaleway_container_namespace":                 container.DataSourceNamespace(),
 				"scaleway_domain_record":                       domain.DataSourceRecord(),
 				"scaleway_domain_zone":                         domain.DataSourceZone(),
+				"scaleway_edge_services_cache_stage":           edgeservices.DataSourceCacheStage(),
 				"scaleway_edge_services_backend_stage":         edgeservices.DataSourceBackendStage(),
 				"scaleway_flexible_ip":                         flexibleip.DataSourceFlexibleIP(),
 				"scaleway_flexible_ips":                        flexibleip.DataSourceFlexibleIPs(),
