61c233dd56c036565a739d0106c96830d7d80d90
Geoffrey JOUNT <gjount@scaleway.com>
2026-06-15 17:37:11 +0200

    test(block): update cassettes

--- internal/services/block/snapshot_data_source.go
+++ internal/services/block/snapshot_data_source.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
 	"github.com/scaleway/scaleway-sdk-go/api/block/v1"
 	"github.com/scaleway/scaleway-sdk-go/scw"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/datasource"
