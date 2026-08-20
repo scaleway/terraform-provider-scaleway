80ccbb76d1b3611ba9edbec44ac5122cf9480448
Estelle Soulard <esoulard@scaleway.com>
2026-08-20 15:08:57 +0200

    chore(rdb): sweep snapshots

--- internal/services/rdb/testfuncs/sweep.go
+++ internal/services/rdb/testfuncs/sweep.go
@@ -13,6 +13,10 @@ func AddTestSweepers() {
 		Name: "scaleway_rdb_instance",
 		F:    testSweepInstance,
 	})
+	resource.AddTestSweepers("scaleway_rdb_snapshot", &resource.Sweeper{
+		Name: "scaleway_rdb_snapshot",
+		F:    testSweepInstanceSnapshots,
+	})
 }
 
 func testSweepInstance(_ string) error {
@@ -37,6 +41,35 @@ func testSweepInstance(_ string) error {
 			})
 			if err != nil {
 				logging.L.Warningf("error deleting rdb instance in sweeper: %s", err)
+			}
+		}
+
+		return nil
+	})
+}
+
+func testSweepInstanceSnapshots(_ string) error {
+	return acctest.SweepRegions(scw.AllRegions, func(scwClient *scw.Client, region scw.Region) error {
+		rdbAPI := rdbSDK.NewAPI(scwClient)
+
+		logging.L.Debugf("sweeper: destroying the rdb snapshots in (%s)", region)
+
+		listSnapshots, err := rdbAPI.ListSnapshots(&rdbSDK.ListSnapshotsRequest{
+			Region: region,
+		}, scw.WithAllPages())
+		if err != nil {
+			logging.L.Warningf("error listing rdb snapshots in (%s) in sweeper: %s", region, err)
+
+			return nil
+		}
+
+		for _, snapshot := range listSnapshots.Snapshots {
+			_, err := rdbAPI.DeleteSnapshot(&rdbSDK.DeleteSnapshotRequest{
+				Region:     region,
+				SnapshotID: snapshot.ID,
+			})
+			if err != nil {
+				logging.L.Warningf("Failed to delete rdb snapshot: %s", err.Error())
 			}
 		}
 
