69141f384505304cddd84dff9fef04b1e24b6965
Geoffrey JOUNT <gjount@scaleway.com>
2026-09-04 12:15:58 +0200

    rm project_ids=["*"] from tests

--- internal/services/file/filesystem_list_test.go
+++ internal/services/file/filesystem_list_test.go
@@ -81,7 +81,7 @@ func TestAccListFileSystems_Basic(t *testing.T) {
 					  provider = scaleway
 
 					  config {
-						project_ids = ["*"]
+						project_ids = [scaleway_file_system.fs1.project_id]
 						regions = ["fr-par"]
 						name = "test-fs"
 					  }
@@ -91,7 +91,7 @@ func TestAccListFileSystems_Basic(t *testing.T) {
 					  provider = scaleway
 
 					  config {
-						project_ids = ["*"]
+						project_ids = [scaleway_file_system.fs1.project_id]
 						regions = ["fr-par"]
 						name = "test-fs-01"
 					  }
@@ -114,7 +114,7 @@ func TestAccListFileSystems_Basic(t *testing.T) {
 					  provider = scaleway
 
 					  config {
-						project_ids = ["*"]
+						project_ids = [scaleway_file_system.fs1.project_id]
 						regions = ["fr-par"]
 						tags = ["foo"]
 					  }
@@ -124,7 +124,7 @@ func TestAccListFileSystems_Basic(t *testing.T) {
 					  provider = scaleway
 
 					  config {
-						project_ids = ["*"]
+						project_ids = [scaleway_file_system.fs1.project_id]
 						regions = ["fr-par"]
 						tags = ["bar"]
 					  }
@@ -134,7 +134,7 @@ func TestAccListFileSystems_Basic(t *testing.T) {
 					  provider = scaleway
 
 					  config {
-						project_ids = ["*"]
+						project_ids = [scaleway_file_system.fs1.project_id]
 						regions = ["fr-par"]
 						tags = ["foobar"]
 					  }
@@ -144,7 +144,7 @@ func TestAccListFileSystems_Basic(t *testing.T) {
 					  provider = scaleway
 
 					  config {
-						project_ids = ["*"]
+						project_ids = [scaleway_file_system.fs1.project_id]
 						regions = ["fr-par"]
 						tags = ["foo", "bar"]
 					  }
