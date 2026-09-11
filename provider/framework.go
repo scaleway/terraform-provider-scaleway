ae63b61077b21233fdbf54de0fcfb435654e4a9b
Estelle Soulard <esoulard@scaleway.com>
2026-09-11 15:43:11 +0200

    feat(partner): add list resources

--- provider/framework.go
+++ provider/framework.go
@@ -318,6 +318,7 @@ func (p *ScalewayProvider) ListResources(_ context.Context) []func() list.ListRe
 		mongodb.NewInstanceListResource,
 		object.NewBucketListResource,
 		opensearch.NewDeploymentListResource,
+		partner.NewPartnerOrganizationListResource,
 		rdb.NewDatabaseBackupListResource,
 		rdb.NewDatabaseListResource,
 		rdb.NewInstanceListResource,
