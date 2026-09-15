b9f7f3d721d4957361c2b0324cf19ec8f0d4d1c9
Jonathan Remy <jremy@scaleway.com>
2026-09-15 09:43:19 +0200

    fix(tem): sweep leftover TEM test DNS zones after revoking domains

--- internal/services/tem/testfuncs/sweep.go
+++ internal/services/tem/testfuncs/sweep.go
@@ -1,12 +1,17 @@
 package temtestfuncs
 
 import (
+	"strings"
+
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	domainSDK "github.com/scaleway/scaleway-sdk-go/api/domain/v2beta1"
 	temSDK "github.com/scaleway/scaleway-sdk-go/api/tem/v1alpha1"
 	"github.com/scaleway/scaleway-sdk-go/scw"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/acctest"
 	"github.com/scaleway/terraform-provider-scaleway/v2/internal/logging"
 )
+
+const temTestDNSDomain = "scaleway-terraform.com"
 
 func AddTestSweepers() {
 	resource.AddTestSweepers("scaleway_tem_domain", &resource.Sweeper{
@@ -16,7 +21,7 @@ func AddTestSweepers() {
 }
 
 func testSweepDomain(_ string) error {
-	return acctest.SweepRegions([]scw.Region{scw.RegionFrPar, scw.RegionNlAms}, func(scwClient *scw.Client, region scw.Region) error {
+	err := acctest.SweepRegions([]scw.Region{scw.RegionFrPar, scw.RegionNlAms}, func(scwClient *scw.Client, region scw.Region) error {
 		temAPI := temSDK.NewAPI(scwClient)
 
 		logging.L.Debugf("sweeper: revoking the tem domains in (%s)", region)
@@ -46,4 +51,69 @@ func testSweepDomain(_ string) error {
 
 		return nil
 	})
+	if err != nil {
+		return err
+	}
+
+	return sweepTEMTestDNSZones()
+}
+
+func sweepTEMTestDNSZones() error {
+	return acctest.Sweep(func(scwClient *scw.Client) error {
+		domainAPI := domainSDK.NewAPI(scwClient)
+
+		logging.L.Debugf("sweeper: deleting leftover TEM test DNS zones on %s", temTestDNSDomain)
+
+		zones, err := domainAPI.ListDNSZones(&domainSDK.ListDNSZonesRequest{
+			Domain: temTestDNSDomain,
+		}, scw.WithAllPages())
+		if err != nil {
+			logging.L.Warningf("error listing DNS zones in sweeper: %s", err)
+
+			return nil
+		}
+
+		for _, zone := range zones.DNSZones {
+			if !isTEMTestDNSZone(zone.Subdomain) {
+				continue
+			}
+
+			dnsZone := zone.Subdomain + "." + zone.Domain
+
+			logging.L.Debugf("sweeper: deleting DNS zone %s", dnsZone)
+
+			_, err := domainAPI.DeleteDNSZone(&domainSDK.DeleteDNSZoneRequest{
+				DNSZone:   dnsZone,
+				ProjectID: zone.ProjectID,
+			})
+			if err != nil {
+				logging.L.Warningf("error deleting DNS zone %s in sweeper: %s", dnsZone, err)
+			}
+		}
+
+		return nil
+	})
+}
+
+func isTEMTestDNSZone(subdomain string) bool {
+	if subdomain == "" {
+		return false
+	}
+
+	prefixes := []string{
+		"tf-tem-",
+		"test-blockedlist",
+		"test-autoconfig",
+		"webhook-test",
+		"test-reputation",
+		"validation-",
+	}
+
+	for _, prefix := range prefixes {
+		if strings.HasPrefix(subdomain, prefix) {
+			return true
+		}
+	}
+
+	return false
 }
